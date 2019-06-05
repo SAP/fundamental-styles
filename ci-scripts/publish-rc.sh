4c3a7988335d1a9a3b22b53b3c1374ed5142cebd
jbadan <jenna.badanowski@gmail.com>
2019-06-05 13:45:20 -0700

    testing travis

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -7,10 +7,11 @@ git config --global user.name "fundamental-bot"
 
 npm run std-version -- --prerelease rc --no-verify
 
-echo $TRAVIS_REPO_SLUG
-echo $TRAVIS_BRANCH
+git status
 
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
+echo "Pushing changes to master"
+
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
 
 #build dist and less folders
 npm run build:prod
