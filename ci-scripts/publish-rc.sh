c97c294d42d755064aeaae89f4522f89518bf9d2
jbadan <jenna.badanowski@gmail.com>
2019-06-05 15:17:19 -0700

    chore: revert to standard version 4.2.0

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -5,11 +5,7 @@ set -e
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
-npm run std-version -- --prerelease rc --no-verify --skip.bump false --skip.changelog false
-
-git status
-
-echo "Pushing changes to master"
+npm run std-version -- --prerelease rc --no-verify
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
 
