8024ab0b7d9d7b9f6d8a803eb05a8706c6c8749c
Inna Atanasova <i.atanasova@sap.com>
2019-09-19 13:43:16 -0400

    use of only css variables

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -24,7 +24,7 @@ module.exports = {
     sourcesContent: true,
     plugins: [
         postcssImport(),
-        postcssAddFallback({importFrom: 'dist/root.css'}), //replace with theme-designer variables
+        postcssAddFallback(), //adds backup css var: var(--providedVar, postCssAddFallbackVar)
         autoprefixer({
             cascade: true
         }),
@@ -33,8 +33,7 @@ module.exports = {
             level: 1
         }),
         postcssCustomProperties({
-            preserve: true,
-            importFrom: 'dist/root.css' //replace with theme-designer variables
+            preserve: true //adds ie11 backup for css variables
           }),
         minify,
         postcssBanner({
