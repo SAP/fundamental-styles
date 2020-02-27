2d5341fc742416a3206d6f8379d87402778a647e
jbadan <jenna.badanowski@gmail.com>
2020-02-27 08:42:03 -0800

    add bold and light to docs site

--- scripts/create-library-config.js
+++ scripts/create-library-config.js
@@ -11,13 +11,20 @@ const data = {
 yaml.sync('docs/_config-library.yml', data);
 
 const fontPath = require.resolve('@sap-theming/theming-base-content/content/Base/baseLib/sap_base_fiori/fonts/72-Regular.woff');
+const fontPathLight = require.resolve('@sap-theming/theming-base-content/content/Base/baseLib/sap_base_fiori/fonts/72-Light.woff');
+const fontPathBold = require.resolve('@sap-theming/theming-base-content/content/Base/baseLib/sap_base_fiori/fonts/72-Bold.woff');
 const iconPath = require.resolve('@sap-theming/theming-base-content/content/Base/baseLib/sap_fiori_3/fonts/SAP-icons.woff');
 
 // copy fonts and icons for docs site
 fs.copyFile(fontPath, 'docs/css/72-Regular.woff', (err) => {
     if (err) throw err;
 });
-
+fs.copyFile(fontPathLight, 'docs/css/72-Light.woff', (err) => {
+    if (err) throw err;
+});
+fs.copyFile(fontPathBold, 'docs/css/72-Bold.woff', (err) => {
+    if (err) throw err;
+});
 fs.copyFile(iconPath, 'docs/css/fonts/SAP-icons.woff', (err) => {
     if (err) throw err;
 });
