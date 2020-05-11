046e0dcba30b253929446133bfe93651adba3a80
InnaAtanasova <i.atanasova@sap.com>
2020-05-11 16:46:42 -0400

    add storybook for tile

---
+++ stories/numeric-content/numerci-content.stories.js
@@ -0,0 +1,17 @@
+
+import "../base.scss";
+import "./numeric-content-playground.scss";
+
+import "../../dist/numeric-content.css";
+import "../../dist/icon.css";
+
+import NumericContent from "./numeric-content.html";
+
+export default {
+  title: "Numeric Content",
+  component: NumericContent,
+};
+
+export const AllCases = () => ({
+  Component: NumericContent,
+});
---
+++ stories/tile/tile.stories.js
@@ -0,0 +1,15 @@
+
+import "../base.scss";
+
+import "../../dist/tile.css";
+
+import Tile from "./tile.html";
+
+export default {
+  title: "Tile",
+  component: Tile,
+};
+
+export const AllCases = () => ({
+  Component: Tile,
+});
