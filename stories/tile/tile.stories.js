c98040d2027d669884dafbeda5f46a963dd47d4b
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2020-05-22 14:28:36 -0400

    feat: migrate Tile component to Fiori3 (#990)
    * initial commit for Tiles, keeping and moving the old Tile code for documentation purposes
    
    * add numeric content, feed and launch tile
    
    * add tests
    
    * update visual tests
    
    * update visual tests
    
    * minor changes, combine tests
    
    * add storybook for tile
    
    * add truncation for long text and doc improvements
    
    * add Badge Tile
    
    * add rtl for badge
    
    * add action mode and slide tile
    
    * bring back badge
    
    * IE11 fixes
    
    * address PR review comments
    
    * update tests
    
    * add more info for the docs
    
    * some a11y improvements, use fd-button for indicator and close btns
    
    * docs improvements

---
+++ stories/tile/tile.stories.js
@@ -0,0 +1,16 @@
+
+import "../base.scss";
+
+import "../../dist/tile.css";
+import "../../dist/badge.css";
+
+import Tile from "./tile.html";
+
+export default {
+  title: "Tile",
+  component: Tile,
+};
+
+export const AllCases = () => ({
+  Component: Tile,
+});
