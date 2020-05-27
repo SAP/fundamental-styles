8c74636e59f16cea0203a877c87a274adfe52f58
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2020-05-27 21:19:41 +0200

    fix: introduce toolbar component (#1022)
    * fix: bringing fiori3 tooblar
    
    * fix: fd-reset added
    
    * fix: docs updated
    
    * fix: button replaced by btn-playground, storybook docker
    
    * fix: theme ugly params added
    
    * fix: working on overflow
    
    * fix: documentation update
    
    * fix: separator width
    
    * fix: documentation updaated
    
    * Update toolbar.md
    
    * Update toolbar.md
    
    * Update toolbar.md
    
    * Update package.json
    
    new line was added
    
    * fix: text overflow inside toolbaroverflow fixed
    
    Co-authored-by: Vanessa-Cusmich <54723167+Vanessa-Cusmich@users.noreply.github.com>

---
+++ stories/toolbar/toolbar.stories.js
@@ -0,0 +1,62 @@
+import { action } from "@storybook/addon-actions";
+
+import "../base.scss";
+
+import "../../dist/toolbar.css";
+import "../../dist/button.css";
+import "../../dist/list.css";
+import "../../dist/menu.css";
+import "../../dist/popover.css";
+import "../../test/resources/layout.css";
+
+import Toolbar from "./toolbar.html";
+
+const actionsData = {
+  onPopoverMenuClicked: action("onPopoverMenuClicked"),
+};
+
+const toolbarData = {
+  hideOverflowMenu: true,
+  dir: "ltr",
+};
+
+export default {
+  title: "Toolbar",
+  component: Toolbar,
+  props: {
+    toolbar: toolbarData,
+  },
+  on: {
+    ...actionsData,
+  },
+};
+
+export const Default = () => ({
+  Component: Toolbar,
+  props: {
+    toolbar: toolbarData,
+  },
+  on: {
+    ...actionsData,
+  },
+});
+
+export const OverflowMenuOpened = () => ({
+  Component: Toolbar,
+  props: {
+    toolbar: { ...toolbarData, hideOverflowMenu: false },
+  },
+  on: {
+    ...actionsData,
+  },
+});
+
+export const Rtl = () => ({
+  Component: Toolbar,
+  props: {
+    toolbar: { ...toolbarData, dir: "rtl" },
+  },
+  on: {
+    ...actionsData,
+  },
+});
