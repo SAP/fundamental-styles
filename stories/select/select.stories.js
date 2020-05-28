ad8cd7c0b638427b03520c7ae23a64c07f683fff
jbadan <jenna.badanowski@gmail.com>
2020-05-28 08:34:25 -0700

    select-readonly

---
+++ stories/select/select.stories.js
@@ -0,0 +1,19 @@
+
+import "../base.scss";
+
+import "../../dist/select.css";
+import "../../dist/icon.css";
+import "../../dist/popover.css";
+import "../../dist/list.css";
+import "../../dist/form-message.css";
+
+import Select from "./select.html";
+
+export default {
+  title: "Select",
+  component: Select,
+};
+
+export const AllCases = () => ({
+  Component: Select,
+});
