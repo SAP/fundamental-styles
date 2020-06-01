47c6d1ba4085f75e11f8234f44827e4feeabe5e7
jbadan <jenna.badanowski@gmail.com>
2020-06-01 15:24:05 -0700

    break out button

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -6,11 +6,41 @@ import "../../dist/icon.css";
 
 import Button from "./button.html";
 
+
+const buttonData = {
+  compact: false,
+  dir: "ltr",
+  disabled: false,
+  text: 'Button'
+}
+
 export default {
   title: "Button",
   component: Button,
+  props: {
+    select: buttonData,
+  },
 };
 
-export const Text = () => ({
+export const primary = () => ({
   Component: Button,
+  props: {
+    button: buttonData,
+  },
 });
+
+export const icon = () => ({
+  Component: Button,
+  props: {
+    button: {...buttonData, icon: 'sap-icon--cart'},
+  },
+});
+
+export const emphasized = () => ({
+  Component: Button,
+  props: {
+    button: {...buttonData, emphasized: true },
+  },
+});
+
+
