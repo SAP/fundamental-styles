85f72b7f430a0aafb4098e81f278d2399aeab599
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2020-06-09 13:45:20 -0400

    chore: add Action Bar to Storybook (#1121)

---
+++ stories/action-bar/action-bar.visual.js
@@ -0,0 +1,17 @@
+import '../../dist/button.css';
+import '../../dist/icon.css';
+import '../../dist/action-bar.css';
+import * as stories from './action-bar.stories.js';
+
+export default {
+    title: 'Visual'
+};
+
+export const ActionBar = () => {
+    const storyNames = Object.keys(stories).filter(story => story !== 'default');
+    const div = document.createElement('div');
+    div.innerHTML = storyNames.map(function(item) {
+        return '<div>' + stories[item]() + '</div>';
+    }).join('');
+    return div;
+};
