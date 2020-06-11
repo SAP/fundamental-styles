f80e7aea83f02e36c894bb10a7d26aefa4e1197a
droshev <mladen.droshev@sap.com>
2020-06-10 23:10:01 -0400

    docs: add pagination storybook and remove njk

---
+++ stories/pagination/pagination.visual.js
@@ -0,0 +1,15 @@
+import '../../dist/pagination.css';
+import * as stories from './pagination.stories.js';
+
+export default {
+    title: 'Visual'
+};
+
+export const Pagination = () => {
+    const storyNames = Object.keys(stories).filter(story => story !== 'default');
+    const div = document.createElement('div');
+    div.innerHTML = storyNames.map(function(item) {
+        return '<div>' + stories[item]() + '</div>';
+    }).join('');
+    return div;
+};
