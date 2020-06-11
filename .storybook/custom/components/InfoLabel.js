223bfce9160930a43bfb1482a5c3be03eb8153f0
deno <mladen.droshev@sap.com>
2020-06-11 10:29:10 -0400

    chore: Add checkbox storybook (#1143)
    * docs: migrate checkbox docs and test page over storybook
    
    * docs: add tags for checkbox page

--- .storybook/custom/components/InfoLabel.js
+++ .storybook/custom/components/InfoLabel.js
@@ -4,7 +4,7 @@ import tagData from './tags.json';
 
 const InfoLabel = ({ tag }) => {
     return (
-        tagData[tag] && <span title={tagData[tag]?.title} className={`fd-info-label fd-info-label--accent-color-${tagData[tag]?.color}`}>{tagData[tag]?.name}</span>
+        tagData[tag] && <span title={tagData[tag]?.title} className={`fddocs fd-info-label fd-info-label--accent-color-${tagData[tag]?.color}`}>{tagData[tag]?.name}</span>
     )
 }
 
