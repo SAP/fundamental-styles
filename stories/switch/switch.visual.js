6592484d17b1bc4d1ca32a789814d85afe6f1a5a
jmarkowski <jedrzej.markowski@sap.com>
2020-06-22 15:08:08 +0200

    chore: add Switch to Storybook (#1167)
    * docs: Add Switch To Storybook
    
    * update storybook tests, remove njk tests for switch
    
    * remove switch from tests list

---
+++ stories/switch/switch.visual.js
@@ -0,0 +1,16 @@
+import '../../dist/form-label.css';
+import '../../dist/switch.css';
+import * as stories from './switch.stories.js';
+
+export default {
+    title: 'Visual'
+};
+
+export const Switch = () => {
+    const storyNames = Object.keys(stories).filter(story => story !== 'default');
+    const div = document.createElement('div');
+    div.innerHTML = storyNames.map(function(item) {
+        return '<div>' + stories[item]() + '</div>';
+    }).join('');
+    return div;
+};
