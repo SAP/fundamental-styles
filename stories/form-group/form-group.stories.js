94879eadf8af40bff47a84f62d00ceb014fffcd4
Darren Matis <darren.matis@gmail.com>
2020-06-24 08:12:45 -0700

    chore: Add form elements to storybook (#1139)
    * Migrated docs to storybook
    
    * Update tests
    
    * Add tags
    
    * Updated tests
    
    * Remove completed stories from index.njk
    
    * Updated tests
    
    * Update visual tests
    
    * Update additional visual tests
    
    * Revert test/visual-regresssion-tests changes

---
+++ stories/form-group/form-group.stories.js
@@ -0,0 +1,42 @@
+import '../../dist/form-group.css';
+
+export default {
+    title: 'Components/Forms/FormGroup',
+    parameters: {
+        description: `
+        Form groups are used to assemble form elements with labels, messages, and help containers.
+
+        These components can be used alone. For example, the 'form__item' element with the label and control could  
+        be used without the full form group since not every form field will need error messages.`,
+        tags: ['f3', 'a11y', 'theme']
+    }
+};
+
+export const primary = () => `
+    <div class="fd-form-group">
+        <div class="fd-form-item">
+            <label class="fd-form-label" for="input-1">Default input:</label>
+            <input class="fd-input" type="text" id="input-1" placeholder="Field placeholder text">
+        </div>
+    </div>
+`;
+
+primary.storyName = 'Default';
+
+export const compact = () => `
+    <div class="fd-form-group">
+        <div class="fd-form-item">
+            <label class="fd-form-label" for="input-1b">Compact Input:</label>
+            <input class="fd-input fd-input--compact" type="text" id="input-1b" placeholder="Field placeholder text">
+        </div>
+    </div>
+`;
+
+export const required = () => `
+    <div class="fd-form-group">
+        <div class="fd-form-item">
+            <label class="fd-form-label fd-form-label--required" for="input-1c">Required Input:</label>
+            <input class="fd-input" type="text" id="input-1c" placeholder="Field placeholder text">
+        </div>
+    </div>
+`;
---
+++ stories/form-message/form-message.stories.js
@@ -0,0 +1,81 @@
+import '../../dist/form-message.css';
+import '../../dist/form-item.css';
+import '../../dist/form-label.css';
+import '../../dist/input.css';
+import '../../dist/popover.css';
+
+export default {
+    title: 'Components/Forms/FormMessage',
+    parameters: {
+        description: `
+Input States
+
+The state of the input field can reflect the validity of the data entered, whether the input data is editable or disabled.
+
+* **Default**: The field is editable but no validation has occurred
+* **Success**: The data format entered has been validated and it's correct, such as an email address.
+* **Error**: The data entered is not valid and must be corrected.
+* **Warning**: The data entered is formatted correctly but there are other issues are problematic but will not stop the user from moving forward.
+* **Disabled**: Indicates the field is not editable, commonly used when the field is dependent on a previous entry/selection within the form.
+* **Read Only**: Used to display static information in the context of a form.
+
+Along with Error and Warning, error messages should be displayed below the field so the user can correct the error and move forward.`,
+        tags: ['f3', 'a11y', 'theme']
+    }
+};
+
+export const success = () => `
+    <div class="fd-form-item">
+        <label class="fd-form-label" for="input-1bb">Success input:</label>
+        <div class="fd-form-input-message-group fd-popover fd-popover--input-message-group">
+            <div class="fd-popover__control" aria-controls="popoverB2" aria-expanded="false" aria-haspopup="true">
+                <input class="fd-input is-success" type="text" id="input-1bb" placeholder="Field placeholder text" aria-label="Image label">
+            </div>
+            <div class="fd-popover__body fd-popover__body--no-arrow" aria-hidden="true" id="popoverB2">
+                <div class="fd-form-message fd-form-message--success">Success message</div>
+            </div>
+        </div>
+    </div>
+`;
+
+export const error = () => `
+    <div class="fd-form-item">
+        <label class="fd-form-label" for="input-1cc">Error input:</label>
+        <div class="fd-form-input-message-group fd-popover fd-popover--input-message-group">
+            <div class="fd-popover__control" aria-controls="popoverB3" aria-expanded="false" aria-haspopup="true">
+                <input class="fd-input is-error" type="text" id="input-1cc" placeholder="Field placeholder text" aria-label="Image label">
+            </div>
+            <div class="fd-popover__body fd-popover__body--no-arrow" aria-hidden="true" id="popoverB3">
+                <div class="fd-form-message fd-form-message--error" >Error message</div>
+            </div>
+        </div>
+    </div>
+`;
+
+export const warning = () => `
+    <div class="fd-form-item">
+        <label class="fd-form-label" for="input-1cc">Warning input:</label>
+        <div class="fd-form-input-message-group fd-popover fd-popover--input-message-group">
+            <div class="fd-popover__control" aria-controls="popoverB3" aria-expanded="false" aria-haspopup="true">
+                <input class="fd-input is-warning" type="text" id="input-1cc" placeholder="Field placeholder text" aria-label="Image label">
+            </div>
+            <div class="fd-popover__body fd-popover__body--no-arrow" aria-hidden="true" id="popoverB3">
+            <div class="fd-form-message fd-form-message--warning" >Warning message</div>
+            </div>
+        </div>
+    </div>
+`;
+
+export const information = () => `
+    <div class="fd-form-item">
+        <label class="fd-form-label" for="input-1ee">Information input:</label>
+        <div class="fd-form-input-message-group fd-popover fd-popover--input-message-group">
+            <div class="fd-popover__control" aria-controls="popoverB5" aria-expanded="false" aria-haspopup="true">
+                <input class="fd-input fd-input--compact is-information" type="text" id="input-1ee" placeholder="Field placeholder text" aria-label="Image label">
+            </div>
+            <div class="fd-popover__body fd-popover__body--no-arrow" aria-hidden="true" id="popoverB5">
+                <div class="fd-form-message fd-form-message--information">Information message</div>
+            </div>
+        </div>
+    </div>
+`;
