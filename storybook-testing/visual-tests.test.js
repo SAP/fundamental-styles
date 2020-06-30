83647c4f241f69cd82790765428b6d7e029bdc43
jbadan <jenna.badanowski@gmail.com>
2020-06-30 09:00:19 -0700

    failure threshold

--- storybook-testing/visual-tests.test.js
+++ storybook-testing/visual-tests.test.js
@@ -10,7 +10,7 @@ global.window = { ...global };
 
 const getMatchOptions = ({ context }) => {
     return {
-        failureThreshold: 0.2,
+        failureThreshold: 2,
         failureThresholdType: 'percent',
         customSnapshotIdentifier: () => context.name.replace(/\s/g, '')
     };
