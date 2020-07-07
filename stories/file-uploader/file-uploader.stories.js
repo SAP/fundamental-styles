3029e099452cc758ff62c7bf040f5786b82923a6
deno <mladen.droshev@sap.com>
2020-07-07 11:33:25 -0400

    fix: remove button.scss from file uploader components (#1213)
    * fix: remove button.scss from file uploader components
    
    * more cleanup, align storybook and test update
    
    Co-authored-by: InnaAtanasova <i.atanasova@sap.com>

--- stories/file-uploader/file-uploader.stories.js
+++ stories/file-uploader/file-uploader.stories.js
@@ -1,5 +1,4 @@
 import '../../dist/button.css';
-import '../../dist/icon.css';
 import '../../dist/form-item.css';
 import '../../dist/file-uploader.css';
 import '../../dist/form-label.css';
@@ -11,7 +10,7 @@ export default {
         description: `
 The File Uploader component allows the user to select the files from the system to upload.
 The File Uploader component is an opinionated composition of the input and button components.
-Please see the input and button components documentation for the complete list of modifiers.
+Please refer to the input and button components documentation for the complete list of modifiers.
 
 Use the File Uploader if:
 
@@ -22,59 +21,143 @@ Use the File Uploader if:
     }
 };
 
-/** On smartphones and tablets, the file uploader is shown in cozy mode (default). */
-
 export const primary = () => `
 <div class="fd-form-item">
     <div class="fd-file-uploader">
-        <input class="fd-input fd-file-uploader__input" 
-        title="Select a file for uploading" 
-        aria-label="Select a file for uploading" 
-        type="text" 
-        id="browse_input1" 
-        placeholder=" Choose a file for upload to localserver " 
-        readonly>
-        <button class="fd-button" 
-        id="fileuploader-button1" 
-        aria-label="Select a file for uploading" >
-        Browse...
+        <input 
+          class="fd-input fd-file-uploader__input" 
+          onclick="browseFile('input1');" 
+          title="Select a file for uploading" 
+          aria-label="Select a file for uploading"
+          aria-live="polite"
+          autocomplete="off"
+          type="text" 
+          id="browse_input1" 
+          placeholder=" Choose a file for upload" 
+          tabindex="-1"
+          readonly>
+        <button 
+          class="fd-button"
+          onclick="browseFile('input1');" 
+          tabindex="0"
+          id="file-uploader-button-1" 
+          aria-label="Select a file for uploading">Browse...
         </button>
     </div>
+    <input
+      id="input1"
+      class="fd-file-uploader__hidden"
+      type="file"
+      onchange="selectFile(this,'browse_input1')">
+</div>
+
+<br/>
+<br/>
+<br/>
+
+<div dir="rtl">
+  <h3>RTL Support</h3>
+  <div class="fd-form-item">
+      <div class="fd-file-uploader">
+          <input 
+            class="fd-input fd-file-uploader__input" 
+            onclick="browseFile('input1');" 
+            title="Select a file for uploading" 
+            aria-label="Select a file for uploading"
+            aria-live="polite"
+            autocomplete="off"
+            type="text" 
+            id="browse_input1-rtl" 
+            placeholder=" Choose a file for upload" 
+            tabindex="-1"
+            readonly>
+          <button 
+            class="fd-button"
+            onclick="browseFile('input1');" 
+            tabindex="0"
+            id="file-uploader-button-2" 
+            aria-label="Select a file for uploading">Browse...
+          </button>
+      </div>
       <input
-        id="input1"
+        id="input1-rtl"
         class="fd-file-uploader__hidden"
         type="file"
-      />
-    </div>
+        onchange="selectFile(this,'browse_input1-rtl')">
+  </div>
+</div>
 `;
 
 primary.storyName = 'Default';
-
-/**
- * The File Uploader should be used in compact mode when using a desktop of devices with large screens.
-It can be achieved by adding the `--compact` modifier to the button and input elements.
- */
+primary.parameters = {
+    docs: {
+        iframeHeight: 250
+    }
+};
 
 export const compact = () => `
 <div class="fd-form-item">
+  <div class="fd-file-uploader">
+      <input 
+        class="fd-input fd-input--compact fd-file-uploader__input" 
+        onclick="browseFile('input2');" 
+        id="browse_input2" 
+        type="text"
+        aria-label="Select a file for uploading" 
+        title="Select a file for uploading"  
+        placeholder="Choose a file for upload"
+        tabindex="-1"
+        readonly>
+      <button
+        class="fd-button fd-button--compact fd-file-uploader__button"  
+        onclick="browseFile('input2');"
+        id="file-uploader-button-3" 
+        aria-label="Select a file for uploading">Browse...
+      </button>
+  </div>
+  <input
+    id="input2"
+    class="fd-file-uploader__hidden"
+    type="file"
+    onchange="selectFile(this,'browse_input2')">
+</div>
+
+<br/>
+<br/>
+<br/>
+
+<div dir="rtl">
+  <h3>RTL Support</h3>
+  <div class="fd-form-item">
     <div class="fd-file-uploader">
-      <input class="fd-input fd-input--compact fd-file-uploader__input" 
+      <input 
+      class="fd-input fd-input--compact fd-file-uploader__input" 
+      onclick="browseFile('input2-rtl');" 
       id="browse_input2-rtl" 
       type="text" 
-      aria-label="Select a file for uploading" 
+      aria-label="Select a file for uploading"
+      tabindex="-1"
       title="Select a file for uploading"  
       placeholder="Choose a file for upload to localserver" 
       readonly>
-      <button class="fd-button fd-button--compact "  
-      id="fileuplader-button2" 
-      aria-label="Select a file for uploading">Browse...
+      <button 
+        class="fd-button fd-button--compact fd-file-uploader__button"  
+        onclick="browseFile('input2-rtl');" 
+        id="file-uploader-button-4" 
+        aria-label="Select a file for uploading">Browse...
       </button>
     </div>
       <input
         id="input2-rtl"
         class="fd-file-uploader__hidden"
         type="file"
-      />
+        onchange="selectFile(this,'browse_input2-rtl')">
   </div>
+</div>
 `;
-
+compact.parameters = {
+    docs: {
+        iframeHeight: 250,
+        storyDescription: 'For compact File Uploader apply the `--compact` modifier to the button and input elements.'
+    }
+};
