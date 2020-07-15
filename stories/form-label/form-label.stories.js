de29fa4a5d9e67ca19c865a02a4d9a896747a7ad
jmarkowski <jedrzej.markowski@sap.com>
2020-07-15 16:12:51 +0200

    fix: label asterix and colon (#1181)
    * fix: label asterix and colon
    
    * Update documentation and storybook examples
    
    * remove leftover import, change documentation
    
    * add rtl example, change storyshot
    
    * change storyshot
    
    * fix ie11 label expand width
    
    * Revert "fix ie11 label expand width" - It causes bugs on other components
    
    * add workaround for IE11 for label width

--- stories/form-label/form-label.stories.js
+++ stories/form-label/form-label.stories.js
@@ -12,7 +12,7 @@ export default {
 
 export const primary = () => `
     <div class="fd-form-item">
-        <label class="fd-form-label" for="input-1">Default input:</label>
+        <label class="fd-form-label" for="input-1">Default input</label>
         <input class="fd-input" type="text" id="input-1" placeholder="Field placeholder text">
     </div>
 `;
@@ -21,14 +21,39 @@ primary.storyName = 'Default';
 
 export const required = () => `
     <div class="fd-form-item">
-        <label class="fd-form-label fd-form-label--required" for="input-1c">Required Input:</label>
+        <label class="fd-form-label fd-form-label--required" for="input-1c">Required Input</label>
         <input class="fd-input" type="text" id="input-1c" placeholder="Field placeholder text">
     </div>
 `;
 
+export const colon = () => `
+    <div class="fd-form-item">
+        <label class="fd-form-label fd-form-label--colon" for="input-1d">Input With Colon</label>
+        <input class="fd-input" type="text" id="input-1d" placeholder="Field placeholder text">
+    </div>
+    <div dir="rtl">
+        <div class="fd-form-item">
+            <label class="fd-form-label fd-form-label--colon" for="input-2d">Input With Colon</label>
+            <input class="fd-input" type="text" id="input-2d" placeholder="Field placeholder text">
+        </div>
+    </div>
+`;
+
+colon.storyName = 'Colon';
+
+colon.parameters = {
+    docs: {
+        storyDescription: `
+            To achieve overflow proof colon icon there should be 'fd-form-label--colon' included. The ":" character
+            will be added at the end of a label as pseudo element. 
+        `
+    }
+};
+
+
 export const disabled = () => `
     <div class="fd-form-item">
-        <label class="fd-form-label" for="input-05">Disabled Input:</label>
+        <label class="fd-form-label" for="input-05">Disabled Input</label>
         <input class="fd-input" type="text" id="input-05" disabled placeholder="Field placeholder text">
     </div>
 `;
