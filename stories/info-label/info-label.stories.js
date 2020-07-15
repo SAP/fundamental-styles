a44f0b5f65cbcecc0d233d64f1f83450e6773b15
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2020-07-15 15:57:28 -0400

    feat: refactor Info Label (#1253)
    * feat: refactor Info Label
    
    * address PR review comments

--- stories/info-label/info-label.stories.js
+++ stories/info-label/info-label.stories.js
@@ -9,21 +9,20 @@ Its primary use is to add user-defined characteristic to an object.
 Use the Info Label base class with following modifiers:
 
 - \`fd-info-label\`
+    - \`fd-info-label--accent-color-*\`
     - \`fd-info-label--icon\`
     - \`fd-info-label--numeric\`
-    - \`fd-info-label--accent-color-*\`
       `,
         tags: ['f3', 'a11y', 'theme']
     }
 };
 
-
-/**
- * Info Label with color accents currently supports 10 semantic colors, starting from `fd-info-label--accent-color-1` to `fd-info-label--accent-color-10`.
- * */
+/** Use the `fd-info-label--accent-color-*` modifier class to apply semantic color to Info Label.
+ * Options include numbers from 1 to 10.
+ */
 
 export const colors = () => `
-<div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+<div class="fddocs-container">
     <span class="fd-info-label fd-info-label--accent-color-1">Info Label</span>
     <br><br>
     <span class="fd-info-label fd-info-label--accent-color-2">Info Label</span>
@@ -45,30 +44,23 @@ export const colors = () => `
     <span class="fd-info-label fd-info-label--accent-color-10">Info Label</span>
 </div>
 `;
+colors.storyName = 'Color Flavors';
 
-colors.storyName = 'Info Label colors';
-
-/**
- * Use `fd-info-label--icon` and icon class to create Info Label with an icon.
- * */
+/** Use the `fd-info-label--icon` modifier class and icon type to create Info Label with Icon. */
 
 export const icons = () => `
-<div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+<div class="fddocs-container">
     <span class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon sap-icon--future">Info Label</span>
     <br><br>
-    <span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--only-icon sap-icon--upload-to-cloud"></span>
+    <span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon sap-icon--upload-to-cloud"></span>
 </div>
 `;
+icons.storyName = 'Info Label with Icon';
 
-icons.storyName = 'Info Label icons';
-
-
-/**
- * Use `fd-info-label--numeric` modifier to create Info Label with numeric content.
- * */
+/** For Numeric Info Label use the `fd-info-label--numeric` modifier class. */
 
 export const numeric = () => `
-<div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+<div class="fddocs-container">
     <span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-1">6</span>
     <br><br>
     <span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-2">6.2</span>
@@ -76,20 +68,16 @@ export const numeric = () => `
     <span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-3">42k</span>
 </div>
 `;
+numeric.storyName = 'Numeric Info Label';
 
-numeric.storyName = 'Info Label numeric';
-
-/**
- * Info Label in RTL mode.
- * */
+/** Info Label in RTL mode. */
 
 export const rtl = () => `
-<div style="display: flex; justify-content: space-around; flex-wrap: wrap;" dir="rtl">
+<div class="fddocs-container" dir="rtl">
     <span class="fd-info-label fd-info-label--accent-color-1">Info Label</span>
     <span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-1">6</span>
     <span class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon sap-icon--future">Info Label</span>
-    <span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--only-icon sap-icon--upload-to-cloud"></span>
+    <span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon sap-icon--upload-to-cloud"></span>
 </div>
 `;
-
 rtl.storyName = 'Info Label RTL';
