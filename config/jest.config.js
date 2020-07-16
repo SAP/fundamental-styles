62e4dfe1edea97c3f7fcad4d629a486be74eb699
droshev <mladen.droshev@sap.com>
2020-07-15 22:17:21 -0400

    test docker in travis

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,10 +1,11 @@
+const process = require('process');
 const path = require('path');
 
 module.exports = {
     rootDir: path.resolve(__dirname, '../storybook-testing/'),
     verbose: true,
     testEnvironment: 'node',
-    preset: 'jest-puppeteer',
+    preset: process.env.DOCKER ? 'jest-puppeteer-docker' : 'jest-puppeteer',
     setupFilesAfterEnv: [path.resolve(__dirname, 'storyshots-setup.js')],
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': 'babel-jest'
