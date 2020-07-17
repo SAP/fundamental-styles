9c04686b973e98c9fa1d8473c4c9e46e4592be5a
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-17 16:05:19 +0200

    chore: docker works on linux need to test on mack

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+process.env.PUPPETEER_CHROMIUM_REVISION = 768783;
 
 module.exports = {
     rootDir: path.resolve(__dirname, '../storybook-testing/'),
-    verbose: true,
+    // verbose: true,
     testEnvironment: 'node',
-    preset: 'jest-puppeteer',
+    preset: 'jest-puppeteer-docker',
     setupFilesAfterEnv: [path.resolve(__dirname, 'storyshots-setup.js')],
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': 'babel-jest'
