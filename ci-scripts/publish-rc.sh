b361b0323f68c9b74341bad73f9d2cdc2373f779
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-23 11:25:47 +0200

    chore: hotfix added till std-version is fixed

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -7,7 +7,12 @@ git config --global user.name "fundamental-bot"
 
 git status
 
-npm run std-version -- --prerelease rc --no-verify --debug
+std_ver=$(npm run std-version -- --prerelease rc --no-verify)
+release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
+
+echo "$release_tag"
+npx node ci-scripts/bump_package.js $release_tag
+cat package.json | grep version
 
 git status
 
