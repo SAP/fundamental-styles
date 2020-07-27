876f9765b8841e26b3338ce138ac115330d5fbeb
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2020-07-27 14:42:09 -0400

    Update avatar.stories.js

--- stories/avatar/avatar.stories.js
+++ stories/avatar/avatar.stories.js
@@ -6,19 +6,20 @@ export default {
     parameters: {
         description: `The avatar component displays an image, icon or user initials, and is used for user profiles, placeholder images, icons or business-related images i.e. product photos.
 
-####Sizes
+Sizes
 
 Size   |  rem  | Use for images in…	| Modifier |
 :-----:|:-----:|--------------------|--------------------------|
-
 XS | 2 rem | Table list items, Card list items | --xs modifier|
 S | 3 rem | Card headers, Card list items | --s modifier|
 M | 4 rem | App headers for small screen sizes | --m modifier|
 L | 5 rem | App headers for normal screen sizes | --l modifier|
 XL | 7 rem | App headers for large screen sizes | --xl modifier|
 
-####Usage
+Usage
+
 Use avatar to display:
+
 * A user’s image or initials
 * Standardized images for business-related content (such as products, company logos, ad campaign images etc.)
 * Icons
@@ -26,6 +27,7 @@ Use avatar to display:
 * Placeholder images
 
 Do not use avatar if:
+
 * You want to display an image for any other use case. Instead, use the Image component.
 * You want to display images in a carousel. Instead, use the Carousel component.
 * You want to display an interactive icon. Instead, use the Button component with an icon inside.
