d5922645e4acb2a35200c38600d8e0d300edb7ec
Jedrzej <jedrzej.markowski@sap.com>
2020-07-27 12:36:08 +0200

    add checkbox and radio as semantic elements

--- stories/semantic-elements/semantic.stories.js
+++ stories/semantic-elements/semantic.stories.js
@@ -1,9 +1,9 @@
-import "../../dist/semantic-elements.css";
+import '../../dist/semantic-elements.css';
 
 export default {
-    title: "Components/Semantic Elements",
+    title: 'Components/Semantic Elements',
     parameters: {
-        description: ``
+        description: ''
     }
 };
 
@@ -39,7 +39,7 @@ export const input = () => `
 input.parameters = {
     docs: {
         iframeHeight: 400,
-        storyDescription: "There are presented inputs with associated labels"
+        storyDescription: 'There are presented inputs with associated labels'
     }
 };
 
@@ -98,6 +98,46 @@ export const form = () => `
         <label for="input-form-3">Input inside form</label>
         <input type="text" id="input-form-3" aria-label="Input inside form" placeholder="Write Something...">    
     </div>
+    <div>
+        <label for="input-form-3">Input inside form</label>
+        <input type="text" id="input-form-3" aria-label="Input inside form" placeholder="Write Something...">    
+    </div>
+    <div>
+        <label for="pDidh76F">
+            <input type="radio" id="pDidh76F" name="radio1" checked>
+            Radio Option 1
+        </label>
+    </div>
+    <div>
+        <label for="pDidJ7612">
+            <input type="radio" id="pDidJ7612" name="radio1">
+            Radio Option 2
+        </label>
+    </div>
+    <div>
+        <label for="pDidhd76F">
+            <input type="checkbox" id="pDidhd76F" name="checkbox1" checked>
+            Checkbox Option 1
+        </label>
+    </div>
+    <div>
+        <label for="pDiHJJ7612">
+            <input type="checkbox" id="pDiHJJ7612" name="checkbox2">
+            Checkbox Option 2
+        </label>
+    </div>
+    <div>
+        <input type="checkbox" id="pDidhd7GD" name="checkbox3" checked>
+        <label for="pDidhd7GD">
+            Checkbox Option 3
+        </label>
+    </div>
+    <div>
+        <input type="checkbox" id="pDiHJJVF2" name="checkbox4">
+        <label for="pDiHJJVF2">
+            Checkbox Option 4
+        </label>
+    </div>
 </form>
 `;
 
@@ -106,3 +146,75 @@ form.parameters = {
 };
 
 
+export const checkbox = () => `
+<form>
+    <div>
+        <input type="checkbox" id="pDidhdBDD" name="checkbox5" checked>
+        <label for="pDidhdBDD">
+            Styled Checkbox 1
+        </label>
+    </div>
+    <div>
+        <input type="checkbox" id="pDidhFDBDD" name="checkbox6">
+        <label for="pDidhFDBDD">
+            Styled Checkbox 2
+        </label>
+    </div>
+    <div>
+        <label for="FGd76F">
+            <input type="checkbox" id="FGd76F" name="checkbox7" checked>
+            Default Checkbox 1
+        </label>
+    </div>
+    <div>
+        <label for="pDHTTLJ2">
+            <input type="checkbox" id="pDHTTLJ2" name="checkbox8">
+            Default Checkbox 2
+        </label>
+    </div>
+</form>
+`;
+
+checkbox.parameters = {
+    docs: {
+        iframeHeight: 300,
+        storyDescription: 'Styled checkbox require a specific construction. Otherwise the styles will not be applied.'
+    }
+};
+
+
+export const radio = () => `
+<form>
+    <div>
+        <input type="radio" id="pDidhdBDC" name="radio3" checked>
+        <label for="pDidhdBDC">
+            Styled Radio 1
+        </label>
+    </div>
+    <div>
+        <input type="radio" id="pDidhFDBDC" name="radio3">
+        <label for="pDidhFDBDC">
+            Styled Radio 2
+        </label>
+    </div>
+    <div>
+        <label for="FGd76H">
+            <input type="radio" id="FGd76H" name="radio4" checked>
+            Default Radio 1
+        </label>
+    </div>
+    <div>
+        <label for="pDHTTBJ2">
+            <input type="radio" id="pDHTTBJ2" name="radio4">
+            Default Radio 2
+        </label>
+    </div>
+</form>
+`;
+
+radio.parameters = {
+    docs: {
+        iframeHeight: 300,
+        storyDescription: 'Styled radio button require a specific construction. Otherwise the styles will not be applied.'
+    }
+};
--- stories/semantic-elements/semantic.visual.js
+++ stories/semantic-elements/semantic.visual.js
@@ -5,9 +5,8 @@ export default {
 };
 
 export const Semantic = () => {
-    const storyNames = Object.keys(stories).filter(story => story !== 'default' && story !== 'mobileMode');
     const div = document.createElement('div');
-    div.innerHTML = storyNames.map(function(item) {
+    div.innerHTML = stories.map(function(item) {
         return '<div>' + stories[item]() + '</div>';
     }).join('');
     return div;
