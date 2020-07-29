33e6fa99870da7b8dcc5d12c194d05caa731a8d4
Jedrzej <jedrzej.markowski@sap.com>
2020-07-29 11:45:15 +0200

    test-remove-debug

--- storybook-testing/visual-tests.test.js
+++ storybook-testing/visual-tests.test.js
@@ -15,7 +15,7 @@ const getMatchOptions = ({ context }) => {
         customSnapshotIdentifier: () => context.name.replace(/\s/g, ''),
         // Will output base64 string of a diff image to console in case of failed tests (in addition to creating a diff image).
         // This string can be copy-pasted to a browser address string to preview the diff for a failed test.
-        dumpDiffToConsole: true,
+        dumpDiffToConsole: false,
         allowSizeMismatch: true
     };
 };
