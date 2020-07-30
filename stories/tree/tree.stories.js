764dbb6860b8cc79fd5f38e58efa1ac9776a9b87
InnaAtanasova <i.atanasova@sap.com>
2020-07-30 11:43:39 -0400

    more code

--- stories/tree/tree.stories.js
+++ stories/tree/tree.stories.js
@@ -17,19 +17,22 @@ If available, a single topmost node is called a root node. Apart from the hierar
 };
 
 export const primaryTree = () => `
-<ul role="tree" aria-label="Root Tree" id="TREE1L1" class="fd-tree level-1 expanded-level-1">
+<ul role="tree" aria-label="Root Tree" id="TREE1L1" class="fd-tree fd-tree--active expanded-level-1">
     <li role="treeitem" aria-level="1" aria-expanded="true" class="fd-tree__item">
-        <div tabindex="0" class="fd-tree__item-container">
+        <div onclick="event.stopPropagation();" tabindex="0" class="fd-tree__item-container is-navigated">
             <button class="fd-tree__expander is-expanded" aria-controls="TREE1L2" aria-haspopup="true" aria-label="Expand level 2" aria-expanded="true"></button>
-            <div class="fd-tree__content">
-                <span class="fd-tree__text">Level 1</span>
+            <div class="fd-form-item fd-tree__form-item">
+                <input type="checkbox" class="fd-checkbox" id="Ai4ez1">
+                <label tabindex="-1" class="fd-checkbox__label" for="Ai4ez1"></label>
             </div>
-            <span class="fd-tree__navigation-indication"></span>
+            <a href="https://www.google.com/" class="fd-tree__content has-navigation-indicator">
+                <span class="fd-tree__text">Level 1</span>
+            </a>
             <button class="fd-button fd-button--transparent sap-icon--decline"></button>
         </div>
-        <ul role="group" class="fd-tree level-2" id="TREE1L2" aria-hidden="false">
+        <ul role="group" class="fd-tree" id="TREE1L2" aria-hidden="false">
             <li role="treeitem" aria-level="2" class="fd-tree__item">
-                <div tabindex="0" class="fd-tree__item-container">
+                <div tabindex="0" class="fd-tree__item-container has-highlight-indicator--warning">
                     <button class="fd-tree__expander" aria-label="Expander"></button>
                     <div class="fd-tree__content">
                         <span class="fd-tree__text">Level 2</span>
@@ -64,18 +67,18 @@ export const primaryTree = () => `
         </div>
     </li>
     <li role="treeitem" aria-level="1" class="fd-tree__item">
-        <div tabindex="0" class="fd-tree__item-container">
-            <div class="fd-tree__content">
+        <div tabindex="0" class="fd-tree__item-container is-navigated has-highlight-indicator--error">
+            <a href="https://www.google.com/" class="fd-tree__content has-navigation-indicator">
                 <span class="fd-tree__text">Very long text that does not wrap ... perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</span>
-            </div>
-            <span class="fd-tree__navigation-indication"></span>
+            </a>
         </div>
     </li>
     <li role="treeitem" aria-level="1" class="fd-tree__item">
-        <div tabindex="0" class="fd-tree__item-container">
+        <div tabindex="0" class="fd-tree__item-container has-highlight-indicator">
             <div class="fd-tree__content fd-tree__content--wrap">
                 <span class="fd-tree__text">Very long text with wrapping behaviour ... perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</span>
             </div>
+            <button class="fd-button fd-button--transparent sap-icon--edit"></button>
             <button class="fd-button fd-button--transparent sap-icon--decline"></button>
         </div>
     </li>
