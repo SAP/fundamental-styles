7a88b62db358d462a248e9b11cc11fbe0a7199b8
Jedrzej <jedrzej.markowski@sap.com>
2020-07-31 11:44:38 +0200

    increaste timeout to 30000ms

--- storybook-testing/visual-tests.test.js
+++ storybook-testing/visual-tests.test.js
@@ -15,7 +15,7 @@ const getMatchOptions = ({ context }) => {
         customSnapshotIdentifier: () => context.name.replace(/\s/g, ''),
         // Will output base64 string of a diff image to console in case of failed tests (in addition to creating a diff image).
         // This string can be copy-pasted to a browser address string to preview the diff for a failed test.
-        dumpDiffToConsole: false,
+        dumpDiffToConsole: true,
         allowSizeMismatch: true
     };
 };
@@ -42,6 +42,6 @@ initStoryshots({
         customizePage: customizePage,
         getMatchOptions: getMatchOptions,
         beforeScreenshot: beforeScreenshot,
-        setupTimeout: 15000
+        setupTimeout: 30000
     })
 });
