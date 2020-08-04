1ab26ad2b93ae1ad3e0d9dcab39496479d6f6d6a
Dmitriy Tkachenko <dimamarksman@gmail.com>
2020-08-04 16:10:42 +0300

    improve styles

--- stories/feed-input/feed-input.stories.js
+++ stories/feed-input/feed-input.stories.js
@@ -12,10 +12,10 @@ export default {
 };
 
 export const empty = () => `
-    <div class="fd-feed-input" role="region" aria-label="Feed input">
+    <div class="fd-feed-input" role="region" aria-label="Feed input empty">
         <div class="fd-feed-input__thumb" style="background-image: url(http://lorempixel.com/460/620/people/7/)" role="img" title="Thumbnail"></div>
         <textarea class="fd-textarea fd-feed-input__textarea" id="textarea-1" rows="1" placeholder="Post something here" aria-label="Feed"></textarea>
-        <button class="fd-button sap-icon--navigation-right-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="true" disabled></button>
+        <button class="fd-button sap-icon--feeder-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="true" disabled></button>
     </div>
 `;
 
@@ -28,10 +28,10 @@ empty.parameters = {
 
 
 export const notEmpty = () => `
-    <div class="fd-feed-input" role="region" aria-label="Feed input">
+    <div class="fd-feed-input" role="region" aria-label="Feed input with one row text">
         <div class="fd-feed-input__thumb" style="background-image: url(http://lorempixel.com/460/620/people/7/)" role="img" title="Thumbnail"></div>
         <textarea class="fd-textarea fd-feed-input__textarea" id="textarea-2" aria-label="Feed" rows="1" placeholder="Post something here">Textarea with a text makes submit button active</textarea>
-        <button class="fd-button sap-icon--navigation-right-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
+        <button class="fd-button sap-icon--feeder-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
     </div>
 `;
 
@@ -43,10 +43,10 @@ notEmpty.parameters = {
 };
 
 export const inputGrowth = () => `
-    <div class="fd-feed-input" role="region" aria-label="Feed input">
+    <div class="fd-feed-input" role="region" aria-label="Feed input height growth">
         <div class="fd-feed-input__thumb" style="background-image: url(http://lorempixel.com/460/620/people/7/)" role="img" title="Thumbnail"></div>
         <textarea class="fd-textarea fd-feed-input__textarea" id="textarea-3" aria-label="Feed" rows="3" placeholder="Post something here">With every line\ntextarea growths up\nuntil it reaches the max height</textarea>
-        <button class="fd-button sap-icon--navigation-right-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
+        <button class="fd-button sap-icon--feeder-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
     </div>
 `;
 
@@ -58,10 +58,10 @@ inputGrowth.parameters = {
 };
 
 export const overMaxHeight = () => `
-    <div class="fd-feed-input" role="region" aria-label="Feed input">
+    <div class="fd-feed-input" role="region" aria-label="Feed input over max height">
         <div class="fd-feed-input__thumb" style="background-image: url(http://lorempixel.com/460/620/people/7/)" role="img" title="Thumbnail"></div>
         <textarea class="fd-textarea fd-feed-input__textarea" id="textarea-4" rows="18" aria-label="Feed" placeholder="Post something here">Over max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height\nOver max height</textarea>
-        <button class="fd-button sap-icon--navigation-right-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
+        <button class="fd-button sap-icon--feeder-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
     </div>
 `;
 
@@ -74,10 +74,10 @@ overMaxHeight.parameters = {
 
 export const rtl = () => `
 <div dir="rtl">
-    <div class="fd-feed-input" role="region" aria-label="Feed input">
+    <div class="fd-feed-input" role="region" aria-label="Feed input with RTL support">
         <div class="fd-feed-input__thumb" style="background-image: url(http://lorempixel.com/460/620/people/7/)" role="img" title="Thumbnail"></div>
         <textarea class="fd-textarea fd-feed-input__textarea" id="textarea-5" rows="1" aria-label="Feed" placeholder="Post something here">POst message here</textarea>
-        <button class="fd-button sap-icon--navigation-left-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
+        <button class="fd-button sap-icon--feeder-arrow fd-feed-input__submit-button" aria-label="Submit" title="Submit" aria-disabled="false"></button>
     </div>
 </div>
 `;
