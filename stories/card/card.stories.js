50c343ef296a939cf64357a9152984be29a8f96a
InnaAtanasova <i.atanasova@sap.com>
2020-08-06 16:54:51 -0400

    finish card anatomy

--- stories/card/card.stories.js
+++ stories/card/card.stories.js
@@ -1,5 +1,7 @@
-import '../../dist/card.css';
+import '../../dist/avatar.css';
+import '../../dist/badge.css';
 import '../../dist/button.css';
+import '../../dist/card.css';
 
 export default {
     title: 'Components/Card',
@@ -23,11 +25,101 @@ Several cards can reference the same applicaton but the information should not b
 
 
 export const cardAnatomy = () => `
-<div class="fd-card">Test</div>
+<div style="display:flex; justify-content:space-around; flex-wrap: wrap">
+    <div style="width: 300px; height: 400px; margin-bottom: 1rem;">
+        <div class="fd-card">
+            <a class="fd-card__header">
+                <span class="fd-avatar fd-avatar--s fd-avatar--circle fd-avatar--thumbnail" style="background-image: url('http://lorempixel.com/400/400/nature/4/')" role="presentation" aria-label="John Doe"></span>
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title</div>
+                        <span class="fd-object-status fd-card__counter">Counter</span>
+                    </div>
+                    <div class="fd-card__subtitle">Card Subtitle</div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+    <div style="width: 300px; height: 400px">
+        <div class="fd-card">
+            <a class="fd-card__header">
+                <span class="fd-avatar fd-avatar--s fd-avatar--circle fd-avatar--thumbnail" style="background-image: url('http://lorempixel.com/400/400/nature/4/')" role="presentation" aria-label="John Doe"></span>
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title With a Very Long Text ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                        <span class="fd-object-status fd-object-status--informative fd-card__counter">1 of 15</span>
+                    </div>
+                    <div class="fd-card__subtitle">Card Subtitle With a Very Long Text ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+    <div style="width: 300px; height: 400px">
+        <div class="fd-card">
+            <a class="fd-card__header">
+                <span class="fd-avatar fd-avatar--s fd-avatar--circle fd-avatar--thumbnail" style="background-image: url('http://lorempixel.com/400/400/nature/4/')" role="presentation" aria-label="John Doe"></span>
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title</div>
+                        <span class="fd-object-status fd-object-status--positive fd-card__counter">6 of 15</span>
+                    </div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+    <div style="width: 300px; height: 400px">
+        <div class="fd-card">
+            <div class="fd-badge">Badge</div>
+            <a class="fd-card__header">
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title</div>
+                        <span class="fd-object-status fd-object-status--critical fd-card__counter">3 of 15</span>
+                    </div>
+                    <div class="fd-card__subtitle">Card Subtitle</div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+    <div style="width: 300px; height: 400px">
+        <div class="fd-card">
+            <div class="fd-badge">New</div>
+            <a class="fd-card__header">
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title With a Very Long Text ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                        <span class="fd-object-status fd-card__counter">1 of 15</span>
+                    </div>
+                    <div class="fd-card__subtitle">Card Subtitle With a Very Long Text ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+    <div style="width: 300px; height: 400px">
+        <div class="fd-card">
+            <div class="fd-badge">Updated Content</div>
+            <a class="fd-card__header">
+                <div class="fd-card__header-text">
+                    <div class="fd-card__title-wrapper">
+                        <div class="fd-card__title">Card Title</div>
+                        <span class="fd-object-status fd-card__counter">1 of 15</span>
+                    </div>
+                </div>
+            </a>
+            <div class="fd-card__content"></div>
+        </div>
+    </div>
+</div>
 `;
 
 cardAnatomy.parameters = {
     docs: {
+        iframeHeight: 450,
         storyDescription: `Card consists of two mandatory components: a clickable header area and a content area. A footer are is only used for actions in the quick view card.
 
 Header:
