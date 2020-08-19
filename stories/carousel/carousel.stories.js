3186f75afb6fb7598401faf511c25d50657033a5
InnaAtanasova <i.atanasova@sap.com>
2020-08-19 10:55:55 -0400

    initial commit

---
+++ stories/carousel/carousel.stories.js
@@ -0,0 +1,23 @@
+import '../../dist/carousel.css';
+
+export default {
+    title: 'Components/Carousel',
+    parameters: {
+        description: `
+`,
+        tags: ['f3', 'a11y', 'theme', 'development']
+    }
+};
+
+
+export const carousel = () => `
+`;
+
+carousel.storyName = '';
+carousel.parameters = {
+    docs: {
+        iframeHeight: 900,
+        storyDescription: `
+`
+    }
+};
