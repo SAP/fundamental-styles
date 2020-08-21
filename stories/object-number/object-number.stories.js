ab05b1d614f97f0a7d6133324e0018eb61eefd30
olshansky <olshansky0@gmail.com>
2020-08-21 14:53:03 +0300

    fix: add bold text and fixes

--- stories/object-number/object-number.stories.js
+++ stories/object-number/object-number.stories.js
@@ -7,7 +7,7 @@ export default {
         description: `The Object Number displays number and number unit properties for an object.
         The number can be displayed using semantic colors to provide additional meaning about the object to the user.`
     },
-    tags: ['f3', 'a11y', 'theme']
+    tags: ['f3', 'theme']
 };
 
 /**
@@ -18,19 +18,19 @@ export default {
 
 export const primary = () => `
 <span class="fd-object-number fd-object-number--negative">
-    <span class="fd-object-number__text">-2,000.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">-2,000.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--critical">
-    <span class="fd-object-number__text">0.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">0.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--positive">
-    <span class="fd-object-number__text">1,000.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">1,000.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--informative">
-    <span class="fd-object-number__text">100.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">100.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number">
-    <span class="fd-object-number__text">999.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">999.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 `;
 
@@ -40,18 +40,40 @@ export const primary = () => `
 
 export const large = () => `
 <span class="fd-object-number fd-object-number--large fd-object-number--negative">
-    <span class="fd-object-number__text">-2,000.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">-2,000.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--large fd-object-number--critical">
-    <span class="fd-object-number__text">0.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">0.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--large fd-object-number--positive">
-    <span class="fd-object-number__text">1,000.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">1,000.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--large fd-object-number--informative">
-    <span class="fd-object-number__text">100.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">100.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 <span class="fd-object-number fd-object-number--large">
-    <span class="fd-object-number__text">999.00</span>&nbsp;<span class="fd-object-number__unit">EUR</span>
+    <span class="fd-object-number__text">999.00</span><span class="fd-object-number__unit">EUR</span>
+</span>
+`;
+
+/**
+ * Object Number text part could be bold (class: `fd-object-number__text--bold`)
+ */
+
+export const bold = () => `
+<span class="fd-object-number fd-object-number--negative">
+    <span class="fd-object-number__text fd-object-number__text--bold">-2,000.00</span><span class="fd-object-number__unit">EUR</span>
+</span>
+<span class="fd-object-number fd-object-number--critical">
+    <span class="fd-object-number__text fd-object-number__text--bold">0.00</span><span class="fd-object-number__unit">EUR</span>
+</span>
+<span class="fd-object-number fd-object-number--positive">
+    <span class="fd-object-number__text fd-object-number__text--bold">1,000.00</span><span class="fd-object-number__unit">EUR</span>
+</span>
+<span class="fd-object-number fd-object-number--informative">
+    <span class="fd-object-number__text fd-object-number__text--bold">100.00</span><span class="fd-object-number__unit">EUR</span>
+</span>
+<span class="fd-object-number">
+    <span class="fd-object-number__text fd-object-number__text--bold">999.00</span><span class="fd-object-number__unit">EUR</span>
 </span>
 `;
