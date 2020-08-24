f067030f61b7056406cd19b838f3d5914b84875b
InnaAtanasova <i.atanasova@sap.com>
2020-08-24 16:26:24 -0400

    documentation and examples

--- stories/carousel/carousel.stories.js
+++ stories/carousel/carousel.stories.js
@@ -34,10 +34,10 @@ The numeral format is the following: **1 of 9**
     }
 };
 
-
-export const carousel = () => `
-<div style="display: flex; justify-content: space-between;">
-    <div class="fd-carousel" data-ride="carousel" style="margin: 3rem;">
+export const carouselBottom = () => `
+<div style="display: flex; flex-direction: column; align-items: center; background: #CCD1D1;">
+    <h4>Navigation Buttons in Page Indicator</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
         <div class="fd-carousel__content" style="min-height: 15.5rem;"></div>
         <div class="fd-carousel__page-indicator-container">
             <button 
@@ -80,77 +80,70 @@ export const carousel = () => `
             </button>
         </div>
     </div>
-
-    <div class="fd-carousel" data-ride="carousel" style="margin: 3rem;">
-        <div class="fd-carousel__content" style="min-height: 15.5rem;"></div>
-        <div class="fd-carousel__page-indicator-container">
-            <button 
-                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
-                data-slide="prev">
-            </button>
-            <div class="fd-carousel__page-indicators">
-                <div class="fd-carousel__text">1 of 4</div>
-            </div>
-            <button 
-                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
-                data-slide="next">
-            </button>
-        </div>
-    </div>
-</div>
-
-<div style="display: flex; justify-content: space-between;">
-    <div class="fd-carousel" data-ride="carousel" style="margin: 3rem;">
-        <div class="fd-carousel__page-indicator-container">
-            <ol class="fd-carousel__page-indicators">
-                <li 
-                    data-slide-to="1" 
-                    aria-label="Displaying item 1 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-                <li 
-                    data-slide-to="2" 
-                    aria-label="Displaying item 2 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-                <li 
-                    data-slide-to="3" 
-                    aria-label="Displaying item 3 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-                <li 
-                    data-slide-to="4" 
-                    aria-label="Displaying item 4 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-                <li 
-                    data-slide-to="5" 
-                    aria-label="Displaying item 5 of 7" 
-                    class="fd-carousel__page-indicator fd-carousel__page-indicator--active"></li>
-                <li 
-                    data-slide-to="6" 
-                    aria-label="Displaying item 6 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-                <li 
-                    data-slide-to="7" 
-                    aria-label="Displaying item 7 of 7" 
-                    class="fd-carousel__page-indicator"></li>
-            </ol>
-        </div>
+    <h4>Navigation Buttons in Content</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
         <div class="fd-carousel__content" style="min-height: 15.5rem;">
-           <button 
+            <button 
                 class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
                 data-slide="prev">
             </button>
-           <button 
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
+                data-slide="next">
+            </button>
+        </div>
+        <div class="fd-carousel__page-indicator-container">
+            <ol class="fd-carousel__page-indicators">
+                <li 
+                    data-slide-to="1" 
+                    aria-label="Displaying item 1 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="2" 
+                    aria-label="Displaying item 2 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="3" 
+                    aria-label="Displaying item 3 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="4" 
+                    aria-label="Displaying item 4 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="5" 
+                    aria-label="Displaying item 5 of 7" 
+                    class="fd-carousel__page-indicator fd-carousel__page-indicator--active"></li>
+                <li 
+                    data-slide-to="6" 
+                    aria-label="Displaying item 6 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="7" 
+                    aria-label="Displaying item 7 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+            </ol>
+        </div>
+    </div>
+    <h4>Numeral Format for Page Indicator</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
+        <div class="fd-carousel__content" style="min-height: 15.5rem;"></div>
+        <div class="fd-carousel__page-indicator-container">
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
+                data-slide="prev">
+            </button>
+            <div class="fd-carousel__page-indicators">
+                <div class="fd-carousel__text">1 of 4</div>
+            </div>
+            <button 
                 class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
                 data-slide="next">
             </button>
         </div>
     </div>
-
-    <div class="fd-carousel" data-ride="carousel" style="margin: 3rem;">
-        <div class="fd-carousel__page-indicator-container">
-            <div class="fd-carousel__page-indicators">
-                <div class="fd-carousel__text">1 of 4</div>
-            </div>
-        </div>
+    <h4>No Page Indicator</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
         <div class="fd-carousel__content" style="min-height: 15.5rem;">
             <button 
                 class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
@@ -165,8 +158,157 @@ export const carousel = () => `
 </div>
 `;
 
-carousel.storyName = '';
-carousel.parameters = {
+carouselBottom.storyName = 'Carousel with Page Indicator on Bottom';
+carouselBottom.parameters = {
+    docs: {
+        iframeHeight: 900,
+        storyDescription: `
+`
+    }
+};
+
+export const carouselTop = () => `
+<div style="display: flex; flex-direction: column; align-items: center; background: #CCD1D1;">
+    <h4>Navigation Buttons in Page Indicator</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
+        <div class="fd-carousel__page-indicator-container">
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
+                data-slide="prev">
+            </button>
+            <ol class="fd-carousel__page-indicators">
+                <li 
+                    data-slide-to="1" 
+                    aria-label="Displaying item 1 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="2" 
+                    aria-label="Displaying item 2 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="3" 
+                    aria-label="Displaying item 3 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="4" 
+                    aria-label="Displaying item 4 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="5" 
+                    aria-label="Displaying item 5 of 7" 
+                    class="fd-carousel__page-indicator fd-carousel__page-indicator--active"></li>
+                <li 
+                    data-slide-to="6" 
+                    aria-label="Displaying item 6 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="7" 
+                    aria-label="Displaying item 7 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+            </ol>
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
+                data-slide="next">
+            </button>
+        </div>
+        <div class="fd-carousel__content" style="min-height: 15.5rem;"></div>
+    </div>
+    <h4>Navigation Buttons in Content</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
+        <div class="fd-carousel__page-indicator-container">
+            <ol class="fd-carousel__page-indicators">
+                <li 
+                    data-slide-to="1" 
+                    aria-label="Displaying item 1 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="2" 
+                    aria-label="Displaying item 2 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="3" 
+                    aria-label="Displaying item 3 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="4" 
+                    aria-label="Displaying item 4 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="5" 
+                    aria-label="Displaying item 5 of 7" 
+                    class="fd-carousel__page-indicator fd-carousel__page-indicator--active"></li>
+                <li 
+                    data-slide-to="6" 
+                    aria-label="Displaying item 6 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+                <li 
+                    data-slide-to="7" 
+                    aria-label="Displaying item 7 of 7" 
+                    class="fd-carousel__page-indicator"></li>
+            </ol>
+        </div>
+        <div class="fd-carousel__content" style="min-height: 15.5rem;">
+           <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
+                data-slide="prev">
+            </button>
+           <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
+                data-slide="next">
+            </button>
+        </div>
+    </div>
+    <h4>Numeral Format for Page Indicator</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
+        <div class="fd-carousel__page-indicator-container">
+            <div class="fd-carousel__page-indicators">
+                <div class="fd-carousel__text">1 of 4</div>
+            </div>
+        </div>
+        <div class="fd-carousel__content" style="min-height: 15.5rem;">
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
+                data-slide="prev">
+            </button>
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
+                data-slide="next">
+            </button>
+        </div>
+    </div>
+    <h4>No Page Indicators</h4>
+    <div class="fd-carousel" data-ride="carousel" style="margin-bottom: 3rem; max-width: 30rem;">
+        <div class="fd-carousel__page-indicator-container">
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-left"
+                data-slide="prev">
+            </button>
+            <ol class="fd-carousel__page-indicators"></ol>
+            <button 
+                class="fd-button fd-carousel__button sap-icon--slim-arrow-right"
+                data-slide="next">
+            </button>
+        </div>
+        <div class="fd-carousel__content" style="text-align: center; padding: 1rem; min-height: 15.5rem;">
+            <div class="fd-carousel__item">
+                <img src="http://lorempixel.com/400/400/nature/1/" alt="Carousel Image 1" />
+            </div>
+            <div class="fd-carousel__item">
+                <img src="http://lorempixel.com/400/400/nature/2/" alt="Carousel Image 2" />
+            </div>
+            <div class="fd-carousel__item">
+                <img src="http://lorempixel.com/400/400/nature/3/" alt="Carousel Image 3" />
+            </div>
+            <div class="fd-carousel__item fd-carousel__item--active">
+                <img src="http://lorempixel.com/400/400/nature/4/" alt="Carousel Image 4" />
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+carouselTop.storyName = 'Carousel with Page Indicator on Top';
+carouselTop.parameters = {
     docs: {
         iframeHeight: 900,
         storyDescription: `
