86bb71908f574da6e1b2036bbdc916b0c29d9e24
jbadan <jenna.badanowski@gmail.com>
2020-08-27 09:45:10 -0700

    updates - remove docker again

--- ci-scripts/update-test.sh
+++ ci-scripts/update-test.sh
@@ -7,7 +7,7 @@ git config --global user.name "fundamental-bot"
 
 if [[ `git status --porcelain` ]]; then
   git add .
-  git commit -a -n -m "chore: update visual regression images"
+  git commit -a -n -m "chore: update visual regression images [CI SKIP]"
   git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_BRANCH"
   exit 1
 fi
