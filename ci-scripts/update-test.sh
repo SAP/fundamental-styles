8f37ba3380da127851d142a3f93156f34ca351fd
jbadan <jenna.badanowski@gmail.com>
2020-08-27 09:51:03 -0700

    test pr comment + fail card tests

--- ci-scripts/update-test.sh
+++ ci-scripts/update-test.sh
@@ -9,5 +9,10 @@ if [[ `git status --porcelain` ]]; then
   git add .
   git commit -a -n -m "chore: update visual regression images [CI SKIP]"
   git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_BRANCH"
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"CI updated visual tests in this pull request. Please review changes and trigger a rebuild.\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  fi
   exit 1
 fi
