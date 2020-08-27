1c46ee16fae0d0321bf95bd66f471f74cf1f113a
jbadan <jenna.badanowski@gmail.com>
2020-08-27 10:28:32 -0700

    dont push to master

---
+++ ci-scripts/update-test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -e
+
+git config --global user.email "fundamental@sap.com"
+git config --global user.name "fundamental-bot"
+
+# if [[ `git status --porcelain` ]]; then
+  # git add .
+  # git commit -a -n -m "chore: update visual regression images [CI SKIP]"
+  echo "$TRAVIS_BRANCH"
+  echo "$TRAVIS_REPO_SLUG"
+  echo "$TRAVIS_PULL_REQUEST"
+  echo "TRAVIS_PULL_REQUEST_BRANCH"
+  # git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_BRANCH"
+  # if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+  #  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  #   -d "{\"body\": \"fundamental-bot updated visual test files in this pull request. Please review these changes and trigger a rebuild.\"}" \
+  #   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  # fi
+  exit 1
+# fi
