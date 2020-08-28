4e455a57f27154fc75e9c155b26f0b87f370fcd2
jbadan <jenna.badanowski@gmail.com>
2020-08-28 14:10:12 -0700

    try it again

--- ci-scripts/update-test.sh
+++ ci-scripts/update-test.sh
@@ -5,18 +5,18 @@ set -e
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
-# if [[ `git status --porcelain` ]]; then
-  # git add .
-  # git commit -a -n -m "chore: update visual regression images [CI SKIP]"
-  echo "$TRAVIS_BRANCH"
-  echo "$TRAVIS_REPO_SLUG"
-  echo "$TRAVIS_PULL_REQUEST"
+if [[ `git status --porcelain` ]]; then
+
   echo "$TRAVIS_PULL_REQUEST_BRANCH"
-  # git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_BRANCH"
-  # if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+     git add .
+     git commit -a -n -m "chore: update visual regression images [CI SKIP]"
+     git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_PULL_REQUEST_BRANCH"
+   fi
+    
+    exit 1
+ fi  
+
   #  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
   #   -d "{\"body\": \"fundamental-bot updated visual test files in this pull request. Please review these changes and trigger a rebuild.\"}" \
   #   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-  # fi
-  exit 1
-# fi
