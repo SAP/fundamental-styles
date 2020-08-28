bb4bc111e0d97339f8c7336d633b2ac14d4d30c1
jbadan <jenna.badanowski@gmail.com>
2020-08-28 14:51:32 -0700

    use the right token

--- ci-scripts/update-test.sh
+++ ci-scripts/update-test.sh
@@ -7,20 +7,21 @@ git config --global user.name "fundamental-bot"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
 
-  npm run test:update
-
-  if [[ `git status --porcelain` ]]; then
-    git branch
-    git checkout $TRAVIS_PULL_REQUEST_BRANCH
-    git add .
-    git commit -a -n -m "chore: update visual regression images [CI SKIP]"
-    git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1;
-
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    curl -H "Authorization: token ${GH_TOKEN}" -X POST \
     -d "{\"body\": \"fundamental-bot updated visual test files in this pull request. Please review these changes and trigger a rebuild.\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
+  #npm run test:update
+
+  # if [[ `git status --porcelain` ]]; then
+  #   git branch
+  #   git checkout $TRAVIS_PULL_REQUEST_BRANCH
+  #   git add .
+  #   git commit -a -n -m "chore: update visual regression images [CI SKIP]"
+  #   git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1;
+
+
     exit 1
-  fi
+  # fi
 fi  
 
