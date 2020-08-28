4f5bcae1e2943f9821da6b894507c1aa93096998
jbadan <jenna.badanowski@gmail.com>
2020-08-28 15:29:33 -0700

    fix

--- ci-scripts/update-test.sh
+++ ci-scripts/update-test.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1;
 
     curl -H "Authorization: token ${GH_TOKEN}" -X POST \
-    -d "{\"body\": \"fundamental-bot updated visual test files in this pull request. Please review these changes for accuracy and trigger a rebuild.\"` }" \
+    -d "{\"body\": \"fundamental-bot updated visual test files in this pull request. Please review these changes for accuracy and trigger a rebuild.\" }" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
     exit 1
