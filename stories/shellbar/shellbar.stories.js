989d6b95e8e5b48bf3042fef66bfafb310065e61
Jedrzej <jedrzej.markowski@sap.com>
2020-09-09 15:30:45 +0200

    fix descriptions

--- stories/shellbar/shellbar.stories.js
+++ stories/shellbar/shellbar.stories.js
@@ -298,8 +298,7 @@ export const linksWithCollapsibleMenuXlSize = () => `
 `;
 linksWithCollapsibleMenuXlSize.parameters = {
     docs: {
-        storyDescription: `
-            This example includes XL shellbar. All actions except \`--mobile\` should be displayed. 
+        storyDescription: `This example includes XL shellbar. All actions except \`--mobile\` should be displayed. 
             This type of shellbar can be achieved by adding \`fd-shellbar--xl\` modifier, it also increases padding of
             shellbar container to \`3rem\`
         `
@@ -527,8 +526,7 @@ export const linksWithCollapsibleMenuSSize = () => `
 `;
 linksWithCollapsibleMenuSSize.parameters = {
     docs: {
-        storyDescription: `
-            When a product has multiple links, the product links should collapse into an overflow menu on mobile screens. 
+        storyDescription: `When a product has multiple links, the product links should collapse into an overflow menu on mobile screens. 
             All actions, except for the user menu, should be collapsed. 
             See the placement of the \`.fd-shellbar__action--mobile\` container below.
             This type of shellbar can be achieved by adding \`fd-shellbar--s\` modifier
