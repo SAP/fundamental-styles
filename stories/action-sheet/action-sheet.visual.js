c4adce8ea0b56f069926b0447fd082320bf0080c
jmarkowski <jedrzej.markowski@sap.com>
2020-09-11 16:10:10 +0200

    feat: new component Action Sheet (#1578)
    * feat: create action sheet component
    
    * Add descriptions, add title
    
    * fix descriptions
    
    * wrap mobile action sheet inside popover
    
    * add static modifier to popper/popover body
    
    * IE11 fix - remove initial values
    
    * Add PR comments
    
    * Add delta styling for overlay opacity
    
    * Add --mobile modifier, remove popover__body from action sheet mobile
    
    * remove leftovers
    
    * bring center for cancel button, add truncation for text
    
    * fix docs, add responsive tag
    
    * update storyshots
    
    * capitalize names of external components
    
    * capitalize name of external components
    
    * fix documentation

---
+++ stories/action-sheet/action-sheet.visual.js
@@ -0,0 +1,16 @@
+import '../../dist/avatar.css';
+import '../../dist/icon.css';
+import * as stories from './action-sheet.stories.js';
+
+export default {
+    title: 'Visual/Avatar'
+};
+
+export const Avatar = () => {
+    const storyNames = Object.keys(stories).filter(story => story !== 'default');
+    const div = document.createElement('div');
+    div.innerHTML = storyNames.map(function(item) {
+        return '<div>' + stories[item]() + '</div>';
+    }).join('');
+    return div;
+};
