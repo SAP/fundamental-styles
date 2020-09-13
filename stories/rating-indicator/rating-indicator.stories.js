62d715e082ff4f6916b26e220d2cd45d7a4a058e
Yurii Betrozov <betrozov91@gmail.com>
2020-09-13 18:13:35 +0300

    fix: fix after review

--- stories/rating-indicator/rating-indicator.stories.js
+++ stories/rating-indicator/rating-indicator.stories.js
@@ -32,7 +32,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-3" name="rating" value="3">
                 <label class="fd-rating-indicator__label" for="rating-3"></label>            
     
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-4" name="rating" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-4" name="rating" value="4">
                 <label class="fd-rating-indicator__label" for="rating-4"></label>
     
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-5" name="rating" value="5">
@@ -55,7 +55,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-xs-3" name="rating-xs" value="3">
                 <label class="fd-rating-indicator__label" for="rating-xs-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-xs-4" name="rating-xs" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-xs-4" name="rating-xs" value="4">
                 <label class="fd-rating-indicator__label" for="rating-xs-4"></label>
     
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-xs-5" name="rating-xs" value="5">
@@ -79,7 +79,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-s-3" name="rating-s" value="3">
                 <label class="fd-rating-indicator__label" for="rating-s-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-s-4" name="rating-s" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-s-4" name="rating-s" value="4">
                 <label class="fd-rating-indicator__label" for="rating-s-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-s-5" name="rating-s" value="5">
@@ -103,7 +103,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-m-3" name="rating-m" value="3">
                 <label class="fd-rating-indicator__label" for="rating-m-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-m-4" name="rating-m" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-m-4" name="rating-m" value="4">
                 <label class="fd-rating-indicator__label" for="rating-m-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-m-5" name="rating-m" value="5">
@@ -126,7 +126,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-l-3" name="rating-l" value="3">
                 <label class="fd-rating-indicator__label" for="rating-l-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-l-4" name="rating-l" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-l-4" name="rating-l" value="4">
                 <label class="fd-rating-indicator__label" for="rating-l-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-l-5" name="rating-l" value="5">
@@ -149,7 +149,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-cozy-3" name="rating-cozy" value="3">
                 <label class="fd-rating-indicator__label" for="rating-cozy-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-cozy-4" name="rating-cozy" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-cozy-4" name="rating-cozy" value="4">
                 <label class="fd-rating-indicator__label" for="rating-cozy-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-cozy-5" name="rating-cozy" value="5">
@@ -172,7 +172,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-compact-3" name="rating-compact" value="3">
                 <label class="fd-rating-indicator__label" for="rating-compact-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-compact-4" name="rating-compact" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-compact-4" name="rating-compact" value="4">
                 <label class="fd-rating-indicator__label" for="rating-compact-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-compact-5" name="rating-compact" value="5">
@@ -195,7 +195,7 @@ export const Sizes = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-condensed-3" name="rating-condensed" value="3">
                 <label class="fd-rating-indicator__label" for="rating-condensed-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-condensed-4" name="rating-condensed" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-condensed-4" name="rating-condensed" value="4">
                 <label class="fd-rating-indicator__label" for="rating-condensed-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-condensed-5" name="rating-condensed" value="5">
@@ -311,19 +311,19 @@ export const ReadOnly = () => `
 <div class="example-container">
     <div class="fd-rating-indicator" aria-readonly="true">
         <div class="fd-rating-indicator__container" aria-label="Star Rating (out of 5)">
-            <input aria-label="1 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-1" name="rating-readonly" value="1" disabled>  
+            <input aria-label="1 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-1" name="rating-readonly" value="1" readonly>  
             <label class="fd-rating-indicator__label" for="rating-readonly-1"></label>
             
-            <input aria-label="2 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-2" name="rating-readonly" value="2" disabled checked>
+            <input aria-label="2 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-2" name="rating-readonly" value="2" readonly checked>
             <label class="fd-rating-indicator__label" for="rating-readonly-2"></label>
             
-            <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-3" name="rating-readonly" value="3" disabled>
+            <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-3" name="rating-readonly" value="3" readonly>
             <label class="fd-rating-indicator__label" for="rating-readonly-3"></label>
             
-            <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-4" name="rating-readonly" value=4" disabled>
+            <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-4" name="rating-readonly" value="4" readonly>
             <label class="fd-rating-indicator__label" for="rating-readonly-4"></label>
             
-            <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-5" name="rating-readonly" value="5" disabled>
+            <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-readonly-5" name="rating-readonly" value="5" readonly>
             <label class="fd-rating-indicator__label" for="rating-readonly-5"></label>
         </div>
         <span class="fd-rating-indicator__dynamic-text">(2 of 5)</span>
@@ -358,7 +358,7 @@ export const Disabled = () => `
             <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-disabled-3" name="rating-disabled" value="3" disabled>
             <label class="fd-rating-indicator__label" for="rating-disabled-3"></label>
             
-            <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-disabled-4" name="rating-disabled" value=4" disabled>
+            <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-disabled-4" name="rating-disabled" value="4" disabled>
             <label class="fd-rating-indicator__label" for="rating-disabled-4"></label>
             
             <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-disabled-5" name="rating-disabled" value="5" disabled>
@@ -395,7 +395,7 @@ export const DisplayMode = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-display-mode-3" name="rating-display-mode" value="3" disabled>
                 <label class="fd-rating-indicator__label" for="rating-display-mode-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-display-mode-4" name="rating-display-mode" value=4" disabled>
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-display-mode-4" name="rating-display-mode" value="4" disabled>
                 <label class="fd-rating-indicator__label" for="rating-display-mode-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-display-mode-5" name="rating-display-mode" value="5" disabled>
@@ -430,7 +430,7 @@ export const DifferentValues = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-5-3" name="rating-max-value-5" value="3">
                 <label class="fd-rating-indicator__label" for="rating-max-value-5-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-5-4" name="rating-max-value-5" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-5-4" name="rating-max-value-5" value="4">
                 <label class="fd-rating-indicator__label" for="rating-max-value-5-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-5-5" name="rating-max-value-5" value="5">
@@ -452,7 +452,7 @@ export const DifferentValues = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-6-3" name="rating-max-value-6" value="3">
                 <label class="fd-rating-indicator__label" for="rating-max-value-6-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-6-4" name="rating-max-value-6" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-6-4" name="rating-max-value-6" value="4">
                 <label class="fd-rating-indicator__label" for="rating-max-value-6-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-6-5" name="rating-max-value-6" value="5">
@@ -477,7 +477,7 @@ export const DifferentValues = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-7-3" name="rating-max-value-7" value="3">
                 <label class="fd-rating-indicator__label" for="rating-max-value-7-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-7-4" name="rating-max-value-7" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-7-4" name="rating-max-value-7" value="4">
                 <label class="fd-rating-indicator__label" for="rating-max-value-7-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-max-value-7-5" name="rating-max-value-7" value="5">
@@ -518,7 +518,7 @@ export const RTL = () => `
                 <input aria-label="3 star" type="radio" class="fd-rating-indicator__input" id="rating-rtl-3" name="rating-rtl" value="3">
                 <label class="fd-rating-indicator__label" for="rating-rtl-3"></label>
                 
-                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-rtl-4" name="rating-rtl" value=4">
+                <input aria-label="4 star" type="radio" class="fd-rating-indicator__input" id="rating-rtl-4" name="rating-rtl" value="4">
                 <label class="fd-rating-indicator__label" for="rating-rtl-4"></label>
                 
                 <input aria-label="5 star" type="radio" class="fd-rating-indicator__input" id="rating-rtl-5" name="rating-rtl" value="5">
