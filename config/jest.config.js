28f7b859a67f0d16f005564f86ae216d3059b3b4
Prasad Thakur <prasad.thakur@sap.com>
2020-10-13 16:56:15 -0700

    remove visual snapshot testing from suite

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,19 +1,14 @@
 const path = require('path');
-const puppeteer = require('puppeteer');
-
-process.env.PUPPETEER_CHROMIUM_REVISION = puppeteer._preferredRevision;
 
 module.exports = {
     rootDir: path.resolve(__dirname, '../storybook-testing/'),
     testEnvironment: 'node',
-    preset: 'jest-puppeteer-docker',
     setupFilesAfterEnv: [path.resolve(__dirname, 'storyshots-setup.js')],
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': 'babel-jest'
     },
     transform: {
         '^.+\\.stories\\.js?$': '@storybook/addon-storyshots/injectFileName',
-        '^.+\\.?visual\\.js?$': '@storybook/addon-storyshots/injectFileName',
         '^.+\\.js?$': 'babel-jest',
         '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx'
     }
