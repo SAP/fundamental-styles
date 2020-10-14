c504243aa8e43bc634d98eddb2ea6f3c51d7542f
skudum <sushma.kudum@sap.com>
2020-10-14 16:02:42 +0530

    enable docs in dev
    rebase with master
    
    removed visuals as suggested by Jeban
    
    addressing jacob reviee comments on acccesiblity

--- stories/object-attribute/object-attribute.stories.js
+++ stories/object-attribute/object-attribute.stories.js
@@ -5,28 +5,28 @@ export default {
 
 export const Text = () => `
 
-<div class="fd-object-attribute">Standard Object Attribute Text</div>
+<div class="fd-object-attribute" id="standardtext">Standard Object Attribute Text</div>
 `;
 
 Text.parameters = { docs: { disable: true } };
 
 
 export const TextWithLink = () => `
-<div class="fd-object-attribute">Object Attribute :
-<span class="fd-object-attribute--link" role="link" tabindex="0">Text With Link</span></div>
+<div id="firsttext" class="fd-object-attribute">Object Attribute :
+<span id="textwithlink" class="fd-object-attribute--link" role="link" tabindex="0">Text With Link</span></div>
 `;
 
 TextWithLink.parameters = { docs: { disable: true } };
 
 export const DisabledLink = () => `
-    <span class="fd-object-attribute fd-object-attribute--link" role="link" tabindex="0" disabled="true" aria-disabled="true">Disabled Object Attribute linked Text</span>
+    <span id="disabledlink" class="fd-object-attribute fd-object-attribute--link" role="link" tabindex="0" aria-disabled=true>Disabled Object Attribute linked Text</span>
 `;
 
 DisabledLink.parameters = { docs: { disable: true } };
 
 export const TruncatedText = () => `
 <div style="width: 120px;">
-    <span class="fd-object-attribute" tabindex="0" title="Truncate Very long Object Attribute Text">Truncate Very long Object Attribute Text</span>
+    <span id="truncated" class="fd-object-attribute" tabindex="0" title="Truncate Very long Object Attribute Text">Truncate Very long Object Attribute Text</span>
     </div>
 `;
 
