df5605dcce9a8b4251df2cda79e868ed775254a9
skudum <sushma.kudum@sap.com>
2020-10-20 12:51:53 +0530

    addressing jcob secoond pahse review comments
    addressing prasad's comments
    
    addressing prasad's comments
    
    removing new line
    
    made hyperlink from span prasad suggested
    
    rebase with master
    
    role link removed

--- stories/object-attribute/object-attribute.stories.js
+++ stories/object-attribute/object-attribute.stories.js
@@ -3,31 +3,24 @@ export default {
     components: ['object-attribute']
 };
 
-export const Text = () => `
-
-<div class="fd-object-attribute" id="standardtext">Standard Object Attribute Text</div>
+export const Text = () => `<div class="fd-object-attribute" id="standardtext">Standard Object Attribute Text</div>
 `;
 
 Text.parameters = { docs: { disable: true } };
 
-
-export const TextWithLink = () => `
-<div id="firsttext" class="fd-object-attribute">Object Attribute :
-<span id="textwithlink" class="fd-object-attribute--link" role="link" tabindex="0">Text With Link</span></div>
+export const TextWithLink = () => `<a id="textwithlink" href="#" class="fd-object-attribute--link" tabindex="0">Text With Link</a>
 `;
 
 TextWithLink.parameters = { docs: { disable: true } };
 
-export const DisabledLink = () => `
-    <span id="disabledlink" class="fd-object-attribute fd-object-attribute--link" role="link" tabindex="0" aria-disabled=true>Disabled Object Attribute linked Text</span>
+export const DisabledLink = () => `<a id="disabledlink" class="fd-object-attribute fd-object-attribute--link" aria-disabled="true">Disabled Object Attribute linked Text</a>
 `;
 
 DisabledLink.parameters = { docs: { disable: true } };
 
-export const TruncatedText = () => `
-<div style="width: 120px;">
-    <span id="truncated" class="fd-object-attribute" tabindex="0" title="Truncate Very long Object Attribute Text">Truncate Very long Object Attribute Text</span>
-    </div>
+export const TruncatedText = () => `<div style="width: 120px;">
+<span id="truncated" class="fd-object-attribute" title="Truncate Very long Object Attribute Text">Truncate Very long Object Attribute Text</span>
+</div>
 `;
 
 TruncatedText.parameters = { docs: { disable: true } };
