c156c2322296acd1e657054aea266b93009b2f20
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2020-10-26 15:20:39 +0200

    fix: added classes and changed examples according to PR review

--- stories/text/text.stories.js
+++ stories/text/text.stories.js
@@ -8,8 +8,8 @@ export default {
     }
 };
 
-export const defaultExample = () => `
-    <p class="fd-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+export const defaultExample = () => `<p class="fd-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
     eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
@@ -26,41 +26,35 @@ defaultExample.parameters = {
 
 export const wrapping = () => `
     <h3>Whitespace normal</h3>
-    <p class="fd-text" style="white-space: normal;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    <p class="fd-text"">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
     eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
     deserunt mollit anim id est laborum.</p>
     
     <h3>Whitespace pre-wrap</h3>
-    <p class="fd-text" style="white-space: pre-wrap;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+    <p class="fd-text fd-text--pre-wrap">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+aliquip ex ea commodo consequat.
+ 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
 eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
 deserunt mollit anim id est laborum.</p>
-
-    <h3>Whitespace nowrap</h3>
-    <div style="overflow: auto;">
-        <p class="fd-text" style="white-space: nowrap;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.</p>
-    </div>
 `;
 
 wrapping.storyName = 'Wrapping';
 wrapping.parameters = {
     docs: {
         iframeHeight: 300,
-        storyDescription: `You can define whether the text should wrap or truncate directly
-                           by setting <code class="docs-code">white-space</code> style`
+        storyDescription: `The Text component has a property allowing browsers to render whitespace and tabs. 
+To enable whitespace rendering add a <code class="docs-code">fd-text--pre-wrap</code> class`
     }
 };
 
 export const maxLines = () => `
     <h3>No max lines rule</h3>
-    <p className="fd-text" style="white-space: normal;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    <p className="fd-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum. Ut etiam sit
     amet nisl. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Tellus in metus vulputate eu
     scelerisque felis imperdiet proin fermentum. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra
@@ -77,7 +71,7 @@ export const maxLines = () => `
     Augue neque gravida in fermentum et. Proin fermentum leo vel orci porta non pulvinar.</p>
     
     <h3>2 max lines</h3>
-    <p class="fd-text" style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2; overflow: hidden;">
+    <p class="fd-text fd-text--max-lines" style="-webkit-line-clamp: 2;">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum. Ut etiam sit
     amet nisl. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Tellus in metus vulputate eu
@@ -95,7 +89,7 @@ export const maxLines = () => `
     Augue neque gravida in fermentum et. Proin fermentum leo vel orci porta non pulvinar.</p>
 
     <h3>3 max lines</h3>
-    <p class="fd-text" style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden;">
+    <p class="fd-text fd-text--max-lines" style="-webkit-line-clamp: 3;">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum. Ut etiam sit
     amet nisl. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Tellus in metus vulputate eu
@@ -118,40 +112,31 @@ maxLines.parameters = {
     docs: {
         iframeHeight: 300,
         storyDescription: `You can define maximum number of lines for text, after reaching the limit text will be
-        truncated and ellipsis will be added. To set maximum numbers you should use these styles:
-        <code class="docs-code">
-        display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: [number of lines]; overflow: hidden;
-        </code>`
+        truncated and ellipsis will be added. To set maximum numbers you should add
+        <code class="docs-code">fd-text--max-lines</code> class and inline style rule
+        <code class="docs-code">-webkit-line-clamp</code> with number of visible lines. For example:
+        <code class="docs-code">style="-webkit-line-clamp: 3;"</code>`
     }
 };
 
-export const hyphenation = () => `
+export const hyphenation = () => `<div class="example-container">
+        <div class="mw-300">
+            <h3>Without hyphens</h3>
+            <p class="fd-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.</p>
+        </div>
 
-    <h3>Without hyphens</h3>
-    <div style="width: 300px">
-        <p class="fd-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.</p>
-    </div>
-    
-    <h3>Hyphen</h3>
-    <div style="width: 300px">
-        <p class="fd-text">Lorem ipsum dolor sit amet, consectetur adip-iscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad mi-nim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.</p>
-    </div>
-    
-    <h3>Shy</h3>
-    <div style="width: 300px">
-        <p class="fd-text">Lorem ipsum dolor sit amet, con&shy;sectetur adip&shy;iscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad mi&shy;nim veniam, quis nostrud exer&shy;citation ullamco laboris nisi ut
-        aliquip ex ea commodo conse&shy;quat. Duis aute irure dolor in reprehenderit in vo&shy;luptate velit esse cillum dolore
-        eu fu&shy;giat nulla pari&shy;atur. Excepteur sint occaecat cupidatat non pro&shy;ident, sunt in culpa qui officia
-        deserunt mollit anim id est la&shy;bo&shy;rum.</p>
+        <div class="mw-300">
+            <h3>Hyphenation</h3>
+            <p class="fd-text fd-text--hyphenation">Lorem ipsum dolor sit amet, con&shy;sectetur adip&shy;iscing el&shy;it, sed do eiusmod tempor
+            incididunt ut labore et do&shy;lore magna aliqua. Ut enim ad mi&shy;nim veniam, quis nostrud exer&shy;citation ullamco laboris nisi ut
+            aliquip ex ea commodo conse&shy;quat. Duis aute irure dolor in repre&shy;henderit in vo&shy;luptate velit esse cillum dolore
+            eu fu&shy;giat nulla pari&shy;atur. Excepteur sint occaecat cupidatat non pro&shy;ident, sunt in culpa qui officia
+            deserunt mollit anim id est la&shy;bo&shy;rum.</p>
+        </div>
     </div>
 `;
 
@@ -159,8 +144,10 @@ hyphenation.storyName = 'Hyphenation';
 hyphenation.parameters = {
     docs: {
         iframeHeight: 200,
-        storyDescription: `The text control supports hyphenation. There are two characters used to manually
-specify potential line break points within text:
+        storyDescription: `To allow browser to automatically break words at appropriate hyphenation points, 
+        following whatever rules it chooses you can add <code class="docs-code">fd-text--hyphenation</code> class.
+        Also you can suggest line break opportunities.
+        There are two Unicode characters used to manually specify potential line break points within text:
 
 - **HYPHEN:** The "hard" hyphen character indicates a visible line break opportunity. Even if the line
 is not actually broken at that point, the hyphen is still rendered.
