6d479c7bdcebb43d9b9cab2d650f518b4a89e62e
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2020-10-30 11:39:51 +0200

    fix: added chagnes according to PR review

--- stories/text/text.stories.js
+++ stories/text/text.stories.js
@@ -26,7 +26,7 @@ defaultExample.parameters = {
 };
 
 export const whitespace = () => `
-    <h3>No indents and whitespace</h3>
+    <h3>No indents or whitespace</h3>
     <p class="fd-text"">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
