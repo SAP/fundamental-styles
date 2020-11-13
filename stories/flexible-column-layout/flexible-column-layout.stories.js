9eda0b3a5e5892b3f6ef023e0c1dd2c5ba48df08
Jedrzej <jedrzej.markowski@sap.com>
2020-11-13 13:42:23 +0100

    Add examples / delta theming

--- stories/flexible-column-layout/flexible-column-layout.stories.js
+++ stories/flexible-column-layout/flexible-column-layout.stories.js
@@ -10,7 +10,7 @@ export default {
 export const threeColumnsLayout = () => `
 <div class="fd-flexible-column-layout">
     <div class="fd-flexible-column-layout__column" style="width: 25%">
-        <div style="height: 600px; background: blue;"></div>
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-7"></div>
     </div>
     <div class="fd-flexible-column-layout__separator">
         <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
@@ -19,7 +19,7 @@ export const threeColumnsLayout = () => `
         </button>
     </div>
     <div class="fd-flexible-column-layout__column" style="width: 50%">
-        <div style="height: 600px; background: green;"></div>
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-8"></div>
     </div>
     <div class="fd-flexible-column-layout__separator">
         <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button"
@@ -28,32 +28,63 @@ export const threeColumnsLayout = () => `
         </button>       
     </div>
     <div class="fd-flexible-column-layout__column" style="width: 25%">
-        <div style="height: 600px; background: red;"></div>
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-9"></div>
     </div>
 </div>`;
 
 
 
 export const twoColumnsLayout = () => `
-<div class="fd-flexible-column-layout">
-    <div class="fd-flexible-column-layout__column" style="width: 33%">
-        <div style="height: 600px; background: blue;"></div>
-    </div>
-    <div class="fd-flexible-column-layout__separator">
-        <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
-                aria-label="Expand">
-            <i class="sap-icon--slim-arrow-right" role="presentation"></i>
-        </button>
-    </div>
-    <div class="fd-flexible-column-layout__column" style="width: 67%">
-        <div style="height: 600px; background: green;"></div>
+<div style="max-width: 1200px">
+    <div class="fd-flexible-column-layout">
+        <div class="fd-flexible-column-layout__column" style="width: 33%">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-9"></div>
+        </div>
+        <div class="fd-flexible-column-layout__separator">
+            <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
+                    aria-label="Expand">
+                <i class="sap-icon--slim-arrow-right" role="presentation"></i>
+            </button>
+        </div>
+        <div class="fd-flexible-column-layout__column" style="width: 67%">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-8"></div>
+        </div>
+        <div class="fd-flexible-column-layout__separator">
+            <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
+                    aria-label="Expand">
+                <i class="sap-icon--slim-arrow-left" role="presentation"></i>
+            </button>
+        </div>
+        <div class="fd-flexible-column-layout__column fd-flexible-column-layout__column--hidden" style="width: 0">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-7"></div>
+        </div>
     </div>
 </div>`;
 
 export const oneColumnsLayout = () => `
-<div class="fd-flexible-column-layout">
-    <div class="fd-flexible-column-layout__column" style="width: 100%">
-        <div style="height: 600px; background: green;"></div>
+<div style="max-width: 600px">
+    <div class="fd-flexible-column-layout">
+        <div class="fd-flexible-column-layout__column fd-flexible-column-layout__column--hidden" style="width: 0">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-9"></div>
+        </div>
+        <div class="fd-flexible-column-layout__separator">
+            <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
+                    aria-label="Expand">
+                <i class="sap-icon--slim-arrow-right" role="presentation"></i>
+            </button>
+        </div>
+        <div class="fd-flexible-column-layout__column" style="width: 100%">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-8"></div>
+        </div>
+        <div class="fd-flexible-column-layout__separator">
+            <button class="fd-button fd-button--compact fd-button--transparent fd-flexible-column-layout__button" 
+                    aria-label="Expand">
+                <i class="sap-icon--slim-arrow-left" role="presentation"></i>
+            </button>
+        </div>
+        <div class="fd-flexible-column-layout__column fd-flexible-column-layout__column--hidden" style="width: 0">
+            <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-7"></div>
+        </div>
     </div>
 </div>`;
 
