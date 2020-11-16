cb533a1b185e749a72a0c6a62f91b8509aec23ad
Jedrzej <jedrzej.markowski@sap.com>
2020-11-16 11:45:42 +0100

    fix documentation, add background color

--- stories/flexible-column-layout/flexible-column-layout.stories.js
+++ stories/flexible-column-layout/flexible-column-layout.stories.js
@@ -1,7 +1,17 @@
 export default {
     title: 'Layouts/Flexible Column',
     parameters: {
-        description: `asdasd`,
+        description: `
+The flexible column layout is a layout control that displays multiple floorplans on a single page. 
+This allows faster and more fluid navigation between multiple floorplans than the usual page-by-page navigation. 
+The flexible column layout offers different layouts with up to three columns. 
+Users can expand the column they want to focus on, switch between different layouts, and view the rightmost column in full screen mode.
+
+### Background Designs
+- <code>fd-flexible-column-layout</code>: solid (default)
+- <code>fd-flexible-column-layout--translucent</code>: translucent 
+- <code>fd-flexible-column-layout--transparent</code>: transparent 
+`,
         tags: ['f3', 'theme', 'development'],
         components: ['button', 'flexible-column-layout', 'icon']
     }
@@ -31,8 +41,16 @@ export const threeColumnsLayout = () => `
             <div class="docs-layout-grid-bg docs-layout-grid-bg--tall docs-layout-grid-bg--color-9"></div>
     </div>
 </div>`;
-
-
+threeColumnsLayout.storyName = 'Three Columns Layout';
+threeColumnsLayout.parameters = {
+    docs: {
+        storyDescription: `
+- Minimum width of the device: <code>1281px</code>
+- Wide column width: <code>50%</code>
+- Narrow columns width: <code>25%</code>
+`
+    }
+};
 
 export const twoColumnsLayout = () => `
 <div style="max-width: 1200px">
@@ -60,6 +78,16 @@ export const twoColumnsLayout = () => `
         </div>
     </div>
 </div>`;
+twoColumnsLayout.storyName = 'Two Columns Layout';
+twoColumnsLayout.parameters = {
+    docs: {
+        storyDescription: `
+- Width of the device: Max: <code>1280px</code>, Min: 961px
+- Wide column width: <code>67%</code>
+- Narrow column width: <code>33%</code>
+`
+    }
+};
 
 export const oneColumnsLayout = () => `
 <div style="max-width: 600px">
@@ -87,5 +115,14 @@ export const oneColumnsLayout = () => `
         </div>
     </div>
 </div>`;
+oneColumnsLayout.storyName = 'One Columns Layout';
+oneColumnsLayout.parameters = {
+    docs: {
+        storyDescription: `
+- Max width of the device: <code>960px</code>
+- Column width: <code>100%</code>
+`
+    }
+};
 
 
