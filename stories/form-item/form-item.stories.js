15f3be792197475630234725c121d7c6a950ded0
Jessie <57453945+jessmetivier@users.noreply.github.com>
2020-12-17 14:51:42 -0500

    updated-docs (#1984)

--- stories/form-item/form-item.stories.js
+++ stories/form-item/form-item.stories.js
@@ -1,6 +1,9 @@
 export default {
     title: 'Components/Forms/Form Item',
     parameters: {
+        description: `
+A form item is essentially a label positioned above an input field. When the user places their cursor in the input field, the field is highlighted in blue to indicate that it’s active.     
+        `,
         tags: ['f3', 'a11y', 'theme'],
         components: ['form-item', 'form-label', 'input']
     }
@@ -13,6 +16,12 @@ export const primary = () => `<div class="fd-form-item">
 `;
 
 primary.storyName = 'Default';
+primary.parameters = {
+    docs: {
+        storyDescription: `The default form item displays a label placed above an empty input field with placeholder text that can be replaced with regular text.
+        `
+    }
+};
 
 export const compact = () => `<div class="fd-form-item">
         <label class="fd-form-label" for="input-1b">Compact Input:</label>
@@ -20,8 +29,24 @@ export const compact = () => `<div class="fd-form-item">
     </div>
 `;
 
+compact.storyName = 'Compact';
+compact.parameters = {
+    docs: {
+        storyDescription: `Form items can be displayed in compact mode by adding the \`--compact\` modifier class to the \`fd-input\` element.
+        `
+    }
+};
+
 export const required = () => `<div class="fd-form-item">
         <label class="fd-form-label fd-form-label--required" for="input-1c">Required Input:</label>
         <input class="fd-input" type="text" id="input-1c" placeholder="Field placeholder text">
     </div>
 `;
+
+required.storyName = 'Required';
+required.parameters = {
+    docs: {
+        storyDescription: `To indicate to the user that it’s required to fill out an input field, add the \`--required\` modifier class to the \`fd-form-label\` element.
+        `
+    }
+};
