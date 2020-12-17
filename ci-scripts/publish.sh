9d0bc0ab6c4ecbd932870765426b1c0334b6e26a
stefanoScalzo <50607147+stefanoScalzo@users.noreply.github.com>
2020-12-17 05:46:19 -0500

    chore: change master to main and update travis.yml (#1957)
    * chore: change master to main
    
    * Allow main to trigger deploy scripts
    
    Co-authored-by: Jedrzej <jedrzej.markowski@sap.com>

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -20,7 +20,7 @@ fi
 echo "$std_ver"
 echo "$release_tag"
 
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" main > /dev/null 2>&1;
 
 # build dist and component folders
 npm run build:prod
@@ -28,7 +28,7 @@ npm run build:prod
 npm publish
 
 # run this after publish to make sure GitHub finishes updating from the push
-npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
+npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch main
 
 npm run storybook:static
 
