977223646c53a1b2d6ca8b5c4f862fa05ac18905
Jessie <57453945+jessmetivier@users.noreply.github.com>
2020-12-17 14:59:28 -0500

    updated-docs (#1985)

--- stories/form-label/form-label.stories.js
+++ stories/form-label/form-label.stories.js
@@ -2,7 +2,8 @@ export default {
     title: 'Components/Forms/Form Label',
     parameters: {
         description: `
-A form-label is used to identify form elements such as form-input, checkbox and radio. Best practice is to use this element as a child of form-item.`,
+Form labels are used to identify form elements such as input fields, checkboxes and radio buttons. This component is best used as a child element of the **Form Item** component.
+        `,
         tags: ['f3', 'a11y', 'theme'],
         components: ['form-item', 'form-label', 'input']
     }
@@ -15,12 +16,28 @@ export const primary = () => `<div class="fd-form-item">
 `;
 
 primary.storyName = 'Default';
+primary.parameters = {
+    docs: {
+        storyDescription: `
+The default form label is displayed in grey text above an input field. To display a form label, add the \`fd-form-label\` class as a child element of the \`fd-form-item\` element.
+        `
+    }
+};
 
 export const required = () => `<div class="fd-form-item">
         <label class="fd-form-label fd-form-label--required" for="input-1c">Required Input</label>
         <input class="fd-input" type="text" id="input-1c" placeholder="Field placeholder text">
     </div>
 `;
+
+required.storyName = 'Required';
+required.parameters = {
+    docs: {
+        storyDescription: `
+To indicate to the user that it’s required to fill out an input field, add the \`--required\` modifier class to the \`fd-form-label\` element.
+        `
+    }
+};
 
 export const colon = () => `<div class="fd-form-item">
         <label class="fd-form-label fd-form-label--colon" for="input-1d">Input With Colon</label>
@@ -29,18 +46,25 @@ export const colon = () => `<div class="fd-form-item">
 `;
 
 colon.storyName = 'Colon';
-
 colon.parameters = {
     docs: {
         storyDescription: `
-To achieve overflow proof colon icon there should be \`fd-form-label--colon\` included. The ":" character will be added at the end of a label as pseudo element. 
+A form label can be displayed with a colon that does not disappear when truncated. To achieve this, add the \`--colon\` modifier class to the \`fd-form-label\` element.
         `
     }
 };
-
 
 export const disabled = () => `<div class="fd-form-item">
         <label class="fd-form-label" for="input-05">Disabled Input</label>
         <input class="fd-input" type="text" id="input-05" disabled placeholder="Field placeholder text">
     </div>
 `;
+
+disabled.storyName = 'Disabled';
+disabled.parameters = {
+    docs: {
+        storyDescription: `
+A form label can be displayed as disabled to indicate to the users that it cannot be interacted with. To display a disabled form label, add the \`is-disabled\` class to the \`fd-input\` element.
+        `
+    }
+};
