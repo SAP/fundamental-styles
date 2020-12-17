8d7aa67322dc77bdd4ce8bfee4a1b377fcb779d9
jessmetivier <jessiemetivier@gmail.com>
2020-12-17 14:24:02 -0500

    updated-docs

--- stories/form-header/form-header.stories.js
+++ stories/form-header/form-header.stories.js
@@ -1,6 +1,9 @@
 export default {
     title: 'Components/Forms/Form Header',
     parameters: {
+        description: `
+Form headers are essentially titles that provide users with more context about a group of input fields. For instance, a form header titled with “Personal information” would categorize input fields with labels: “Name, Address, etc.”
+`,
         tags: ['f3', 'theme'],
         components: ['form-header']
     }
@@ -11,4 +14,11 @@ export const primary = () => `<div class="fd-form-header">
     </div>
 `;
 
-primary.storyName = 'Form Header without Toolbar';
+primary.storyName = 'Default';
+primary.parameters = {
+    docs: {
+        storyDescription: `
+Default form headers are displayed in text that does not wrap but truncates into an ellipsis.
+`
+    }
+};
