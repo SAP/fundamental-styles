44a926fc2be5f6eb522b9054be225f0746db64a5
deno <mladen.droshev@sap.com>
2021-01-14 17:22:57 -0500

    chore: reduce chromatic theming tests (#2059)

--- scripts/build-visual-stories.js
+++ scripts/build-visual-stories.js
@@ -85,8 +85,8 @@ rimraf('**/*.visual.js', (rimRafError) => {
 
                     const themes = [
                         { value: 'sap_fiori_3', title: 'Quartz Light' },
-                        { value: 'sap_fiori_3_dark', title: 'Quartz Dark' },
-                        { value: 'sap_fiori_3_hcw', title: 'High Contrast White' },
+                        // { value: 'sap_fiori_3_dark', title: 'Quartz Dark' },
+                        // { value: 'sap_fiori_3_hcw', title: 'High Contrast White' },
                         { value: 'sap_fiori_3_hcb', title: 'High Contrast Black' }
                     ];
 
