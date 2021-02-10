a7ffe2ac2b858374ab82f2058aafae7ba1891c3e
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2021-02-10 16:03:06 -0700

    tristate checkboxes indeterminate on load

--- .storybook/static/preview-head.js
+++ .storybook/static/preview-head.js
@@ -1,13 +1,13 @@
 (function() {
     document.addEventListener('DOMContentLoaded', function () {
         //set the indeterminate state of checkbox
-        var triStateCheckboxes = ['Ai4ez613', 'Ai4ez613i1', 'Ai4ez613i2', 'Ai4ez613i3', 'Ai4ez613i4', 'Ai4ez613i5', 'Ai4ez613i6', 'Ai4ez613i7', 'Ai4ez643', 'Ai4ez613c', 'Ai4ez643c'];
-        for (var i = 0; i < triStateCheckboxes.length; i++) {
-            const triStateCheckbox = document.getElementById(triStateCheckboxes[i]);
+        const triStateCheckboxes = ['Ai4ez613', 'Ai4ez613i1', 'Ai4ez613i2', 'Ai4ez613i3', 'Ai4ez613i4', 'Ai4ez613i5', 'Ai4ez613i6', 'Ai4ez613i7', 'Ai4ez643', 'Ai4ez613c', 'Ai4ez643c'];
+        triStateCheckboxes.forEach((_id) => {
+            const triStateCheckbox = document.getElementById(_id);
             if (triStateCheckbox) {
                 triStateCheckbox.indeterminate = true;
             }
-        };
+        });
 
         //climb up DOM to get block element
         function getBlock(control) {
