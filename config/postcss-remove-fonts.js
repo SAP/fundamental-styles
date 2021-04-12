e7e273504617c320e854b5ca339491859500b278
InnaAtanasova <i.atanasova@sap.com>
2021-04-12 17:38:07 -0400

    chore: migrate add fallback plugin and remove font variables to postcss version 8

--- config/postcss-remove-fonts.js
+++ config/postcss-remove-fonts.js
@@ -1,12 +1,15 @@
-const postcss = require('postcss');
-
-module.exports = postcss.plugin('remove font variables', function() {
-    return function(root) {
-        root.walkDecls(decl => {
-            const regex = /--sapFontUrl_([a-zA-Z-_0-9])+/g;
-            if (decl.prop.match(regex)) {
-                decl.remove();
-            }
-        });
+module.exports = () => {
+    return {
+        postcssPlugin: 'remove font variables',
+        Once(root) {
+            root.walkDecls(decl => {
+                const regex = /--sapFontUrl_([a-zA-Z-_0-9])+/g;
+                if (decl.prop.match(regex)) {
+                    decl.remove();
+                }
+            });
+        }
     };
-});
+};
+
+module.exports.postcss = true;
