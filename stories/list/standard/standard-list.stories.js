e9328dfaa4ae890bb1dea2f782b2e5d8e2c29696
DeepakSap14 <deepak.kumar14@sap.com>
2021-05-19 23:10:54 +0530

    fix: removed duplicate classes, verified in Ngx [ci visual]

--- stories/list/standard/standard-list.stories.js
+++ stories/list/standard/standard-list.stories.js
@@ -588,120 +588,52 @@ inactive.parameters = {
     }
 };
 
-export const noSeprator = () => `<ul class="fd-list" role="list">
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 1</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 2</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 3</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 4</span>
-  </li>
-</ul>
-`;
-
-noSeprator.storyName = 'No-Seprator';
-
-noSeprator.parameters = {
-    docs: {
-        iframeHeight: 370,
-        storyDescription: `Border of list items can be removed using no-seprator class. This can be passed at list item level.
-`
-    }
-};
-
-export const growing = () => `<ul class="fd-list" role="list">
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 1</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 2</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 3</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item fd-list__item--no-seprator">
-    <span class="fd-list__title">List item 4</span>
-  </li>
-  <li role="listitem" tabindex="0" class ="fd-list__item fd-list__item--action">
-    <button class="fd-list__title fd-list__title--growing">
-      <div class="fd-busy-indicator--m" aria-hidden="false" aria-label="Loading">
-        <div class="fd-busy-indicator--circle-0"></div>
-        <div class="fd-busy-indicator--circle-1"></div>
-        <div class="fd-busy-indicator--circle-2"></div>
-      </div>
-      Show More
-    </button>
-  </li>
-</ul>
-`;
-
-growing.storyName = 'Growing';
-
-growing.parameters = {
-    docs: {
-        iframeHeight: 370,
-        storyDescription: `Applies styles for button, which start loading more list items on click and shown busy indicator
-`
-    }
-};
-
-export const unReadTitle = () => `<ul class="fd-list" role="list">
-  <li role="listitem" tabindex="0" class="fd-list__item">
-    <span class="fd-list__title fd-list__title--unread">List item 1</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item">
-    <span class="fd-list__title">List item 2</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item">
-    <span class="fd-list__title fd-list__title--unread">List item 3</span>
-  </li>
-  <li role="listitem" tabindex="0" class="fd-list__item">
-    <span class="fd-list__title">List item 4</span>
-  </li>
-</ul>
-`;
-
-unReadTitle.storyName = 'Un-read title';
-
-unReadTitle.parameters = {
-    docs: {
-        iframeHeight: 370,
-        storyDescription: `Un-read list item also can be created using fd-list-title__unread on list item title level
-`
-    }
-};
-
-export const counter = () => `<ul class="fd-list" role="list">
+export const scroll = () => `<ul class="fd-list fd-list__infinite-scroll fddocs-list-height" role="list">
   <li role="listitem" tabindex="0" class="fd-list__item">
     <span class="fd-list__title">List item 1</span>
-    <span class="fd-list__secondary fd-list__counter">10</span>
   </li>
   <li role="listitem" tabindex="0" class="fd-list__item">
     <span class="fd-list__title">List item 2</span>
-    <span class="fd-list__secondary fd-list__counter">15</span>
   </li>
   <li role="listitem" tabindex="0" class="fd-list__item">
     <span class="fd-list__title">List item 3</span>
-    <span class="fd-list__secondary fd-list__counter">8</span>
   </li>
   <li role="listitem" tabindex="0" class="fd-list__item">
     <span class="fd-list__title">List item 4</span>
-    <span class="fd-list__secondary fd-list__counter">12</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 5</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 6</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 7</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 8</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 9</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 10</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 11</span>
+  </li>
+  <li role="listitem" tabindex="0" class="fd-list__item">
+    <span class="fd-list__title">List item 12</span>
   </li>
 </ul>
 `;
 
-counter.storyName = 'Counter';
+scroll.storyName = 'Infinite scroll';
 
-counter.parameters = {
+scroll.parameters = {
     docs: {
         iframeHeight: 370,
-        storyDescription: `List item can have counter value
+        storyDescription: `If height for list is fixed and there are more list otem to show, then we should be able to scroll to next list items.
 `
     }
 };
