f4fe698b2b6c8f21adcac73a141d680673dfa41b
Yurii Betrozov <betrozov91@gmail.com>
2021-06-01 19:19:57 +0300

    chore: update commitlint, changelog (#2408)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,8 @@ const Configuration = {
     'extends': ['@commitlint/config-conventional'],
     rules: {
         'body-max-line-length': [2, 'always', 200],
-        'footer-max-line-length': [2, 'always', 200]
+        'footer-max-line-length': [2, 'always', 200],
+        'header-max-length': [2, 'always', 200]
     }
 };
 
