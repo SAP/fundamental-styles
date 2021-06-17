b161f8b2c740582e410daa871c7fafd3371dba3e
deno <mladen.droshev@sap.com>
2021-06-17 15:28:05 -0400

    fix: bring charset at the beginning of the css files [ci visual] (#2459)

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -3,6 +3,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssClean = require('postcss-clean');
 const postcssBanner = require('postcss-banner');
+const postcssNormalizeCharset = require('postcss-normalize-charset');
 const postcssAddFallback = require('./postcss-add-fallback.js');
 const postcssCustomProperties = require('postcss-custom-properties'); //ie11 fallbacks
 const postcssImport = require('postcss-import');
@@ -29,20 +30,21 @@ module.exports = {
         autoprefixer({
             cascade: true
         }),
-        postcssClean({
-            format: 'beautify',
-            level: 1
-        }),
         postcssCustomProperties({
             preserve: true
         }),
         postcssRemoveFonts(), // remove fonts from @sap-theming/theming-base-content
-        minify,
         postcssBanner({
             banner: `Fundamental Library Styles v${packageVersion}
 Copyright (c) ${year} SAP SE or an SAP affiliate company.
 Licensed under Apache License 2.0 (https://github.com/SAP/fundamental-styles/blob/main/LICENSE)`,
-            important: true })
+            important: true }),
+        postcssNormalizeCharset(),
+        postcssClean({
+            format: 'beautify',
+            level: 1
+        }),
+        minify
 
     ]
 };
