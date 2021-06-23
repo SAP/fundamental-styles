63bdae84c0d1bc3fd0bebccaf2fdb844442100f6
DeepakSap14 <deepak.kumar14@sap.com>
2021-06-23 22:20:40 +0530

    fix: export component and move to dev folder [ci visual]

--- stories/off-screen/off-screen.stories.js
+++ stories/off-screen/off-screen.stories.js
@@ -1,17 +1,18 @@
 export default {
-    title: 'Components/Off Screen',
+    title: 'Dev/Off Screen',
     parameters: {
         description: `For aria-live to read any change in element, we need to place element but it should not be visible to user.
             same time, it can not be made hidden or display: none, as screen reader will not be able to pick this element.
             This component will make sure that element is not visible on screen but it can be read by screen reader.`,
         tags: ['a11y'],
-        components: ['off-screen']
+        components: ['off-screen', 'text']
     }
 };
 
 export const primary = () => `<div class="fd-off-screen" aria-live="polite">
     This will be read whenever this element changes.
 </div>
+<div class="fd-text">Text with aria-live is kept far left of the screen. so it will not be visible, but will be read by screen reader.</div>
 `;
 primary.parameters = {
     docs: {
