edb2672d8f75a35d12f179d3138a81a019839de4
deno <mladen.droshev@sap.com>
2021-07-16 20:16:51 -0400

    docs: update textarea validation messages [ci visual] (#2581)

--- stories/textarea/textarea.stories.js
+++ stories/textarea/textarea.stories.js
@@ -70,7 +70,7 @@ export const states = () => `
 <div class="fd-form-item">
     <label class="fd-form-label" for="textarea-3">Success text area:</label>
     <div class="fd-popover fd-popover--input-message-group">
-        <div class="fd-popover__control" aria-controls="popoverT51" aria-expanded="false" aria-haspopup="true">
+        <div class="fd-popover__control" aria-controls="popoverT51" aria-expanded="false" aria-haspopup="true" onfocusin="onPopoverClick('popoverT51')" onfocusout="onPopoverClick('popoverT51')">
             <textarea class="fd-textarea is-success" id="textarea-3" placeholder="Write something here"></textarea>
         </div>
         <div class="fd-popover__body fd-popover__body--no-arrow"  aria-hidden="true" id="popoverT51">
@@ -83,7 +83,7 @@ export const states = () => `
 <div class="fd-form-item">
     <label class="fd-form-label" for="textarea-4">Error text area:</label>
     <div class="fd-popover fd-popover--input-message-group">
-        <div class="fd-popover__control" aria-controls="popoverT52" aria-expanded="false" aria-haspopup="true">
+        <div class="fd-popover__control" aria-controls="popoverT52" aria-expanded="false" aria-haspopup="true" onfocusin="onPopoverClick('popoverT52')" onfocusout="onPopoverClick('popoverT52')">
             <textarea class="fd-textarea is-error" id="textarea-4" placeholder="Write something here"></textarea>
         </div>
         <div class="fd-popover__body fd-popover__body--no-arrow"  aria-hidden="true" id="popoverT52">
@@ -95,7 +95,7 @@ export const states = () => `
 <div class="fd-form-item">
     <label class="fd-form-label" for="textarea-5">Warning text area:</label>
     <div class="fd-popover fd-popover--input-message-group">
-        <div class="fd-popover__control" aria-controls="popoverT53" aria-expanded="false" aria-haspopup="true">
+        <div class="fd-popover__control" aria-controls="popoverT53" aria-expanded="false" aria-haspopup="true" onfocusin="onPopoverClick('popoverT53')" onfocusout="onPopoverClick('popoverT53')">
             <textarea class="fd-textarea is-warning" id="textarea-5" placeholder="Write something here"></textarea>
         </div>
         <div class="fd-popover__body fd-popover__body--no-arrow"  aria-hidden="true" id="popoverT53">
@@ -107,7 +107,7 @@ export const states = () => `
 <div class="fd-form-item">
     <label class="fd-form-label" for="textarea-6">Information text area:</label>
     <div class="fd-popover fd-popover--input-message-group">
-        <div class="fd-popover__control" aria-controls="popoverT54" aria-expanded="false" aria-haspopup="true">
+        <div class="fd-popover__control" aria-controls="popoverT54" aria-expanded="false" aria-haspopup="true" onfocusin="onPopoverClick('popoverT54')" onfocusout="onPopoverClick('popoverT54')">
             <textarea class="fd-textarea is-information" id="textarea-6" placeholder="Write something here"></textarea>
         </div>
         <div class="fd-popover__body fd-popover__body--no-arrow"  aria-hidden="true" id="popoverT54">
