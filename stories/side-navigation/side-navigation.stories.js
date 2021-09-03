bc24e45d4f84e3913fa1d5bb3de455096ea42c7b
DeepakSap14 <deepak.kumar14@sap.com>
2021-09-03 22:00:42 +0530

    fix: compact mode handle, remove text-only class from popover [ci visual]

--- stories/side-navigation/side-navigation.stories.js
+++ stories/side-navigation/side-navigation.stories.js
@@ -1267,9 +1267,9 @@ export const nestedListPopover = () => `<div class="fddocs-container" style="mar
             </ul>
         </div>
         <div class="fd-popover__popper" aria-hidden="false" id="popoverA11">
-            <div class="fd-nested-list fd-nested-list--text-only fd-nested-list--popover">
+            <div class="fd-nested-list fd-nested-list--popover fd-nested-list--compact">
                 <div class="fd-nested-list__popover-header">
-                    <span class="fd-nested-list__title">Popover Header</span>
+                    Popover Header
                 </div>
             </div>
 
@@ -1298,7 +1298,7 @@ export const nestedListPopover = () => `<div class="fddocs-container" style="mar
 
     <div class="fd-popover fd-popover--right">
         <div class="fd-popover__control">
-            <ul class="fd-nested-list fd-nested-list--compact">
+            <ul class="fd-nested-list">
                 <li class="fd-nested-list__item">
                     <a class="fd-nested-list__link" href="#/">
                         <i role="presentation" class="fd-nested-list__icon sap-icon--home"></i>
@@ -1307,12 +1307,12 @@ export const nestedListPopover = () => `<div class="fddocs-container" style="mar
             </ul>
         </div>
         <div class="fd-popover__popper" aria-hidden="false" id="popoverA12">
-            <div class="fd-nested-list fd-nested-list--text-only fd-nested-list--popover">
+            <div class="fd-nested-list fd-nested-list--popover">
                 <div class="fd-nested-list__popover-header">
-                    <span class="fd-nested-list__title">Popover Header</span>
+                    Popover Header
                 </div>
             </div>
-            <ul class="fd-nested-list fd-nested-list--popover fd-nested-list--compact">
+            <ul class="fd-nested-list fd-nested-list--popover">
                 <li class="fd-nested-list__group-header">Group Header</li>
                 <li class="fd-nested-list__item fd-nested-list__item--popover">
                     <a class="fd-nested-list__link" href="#/">
