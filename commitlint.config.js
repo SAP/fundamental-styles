b98c566637e7f9b43345304ce10ac7ff8813bd16
deno <mladen.droshev@sap.com>
2021-10-18 21:47:14 -0400

    chore(styles,fn): introduce scope for styles and fn (#2831)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
 const Configuration = {
     'extends': ['@commitlint/config-conventional'],
     rules: {
+        'scope-enum': [
+            2,
+            'always',
+            ['styles', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog', 'ci', 'fn']
+        ],
         'body-max-line-length': [2, 'always', 200],
         'footer-max-line-length': [2, 'always', 200],
         'header-max-length': [2, 'always', 200]
