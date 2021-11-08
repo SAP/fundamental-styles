e221d235fb90436349899d99329f3e41ef95d764
Platon Rov <platon.rov@gmail.com>
2021-11-08 12:40:47 +0200

    docs: themeable scrollbar [ci visual]

--- stories/scrollbar/scrollbar.stories.js
+++ stories/scrollbar/scrollbar.stories.js
@@ -1,7 +1,12 @@
 export default {
     title: 'Components/Scrollbar',
     parameters: {
-        description: 'Themeable scrollbar',
+        description: `
+Themeable scrollbar is the scrollbar component made accordingly to the Fiori 3 design guidelines. 
+Using the themeable scrollbar is preferred over the browser's default in the components that support having scrollbars and made accordingly to the Fiori 3 design guidelines.
+Themeable scrollbar works in Chrome, Safari, and other WebKit-based browsers and partially works in Firefox (no hover effects).
+Component not working in the IE, default scrollbar is shown instead.
+            `,
         docs: { iframeHeight: 400 },
         tags: ['f3', 'theme', 'development'],
         components: ['scrollbar']
