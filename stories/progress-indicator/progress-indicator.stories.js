2d49572d218bacfccb9b8662fce9f040ab8ca9f1
InnaAtanasova <i.atanasova@sap.com>
2021-11-10 17:58:36 -0500

    feat(styles): add final code for progress indicator [ci visual]

--- stories/progress-indicator/progress-indicator.stories.js
+++ stories/progress-indicator/progress-indicator.stories.js
@@ -17,51 +17,65 @@ export default {
 - The progress is indeterminate. Use a busy indicator instead.
 - You want to display a single value in the form of a fillable shape or a group of shapes that describe their context. Use the status indicator instead.
 
+##Modifier classes
+State | Modifier class
+:------------- | :-----------------
+Mobile | \`fd-progress-indicator--mobile\`
+Display | \`fd-progress-indicator--display\`
+Informative &nbsp;&nbsp; &nbsp;&nbsp; | \`fd-progress-indicator--informative\`
+Positive | \`fd-progress-indicator--positive\`
+Critical | \`fd-progress-indicator--critical\`
+Negative | \`fd-progress-indicator--negative\`
+
         `,
         components: ['progress-indicator', 'menu', 'popover', 'icon'],
-        tags: ['f3', 'a11y', 'theme']
+        tags: ['f3', 'a11y', 'theme', 'development']
     }
 };
 
 export const layout = () => `
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="0" aria-valuemax="100" aria-valuetext="0%" aria-label="0%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__remain-area">
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 0%; width: 0%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">0%</span>
-            </div>  
+            </div>
         </div>
     </div>
     
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area">
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">42%</span>
-            </div>  
-        </div>
-    </div>
-    
-    <div class="fd-progress-indicator">
-        <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar" style="flex-basis: 69%">
-                <span class="fd-progress-indicator__label fd-progress-indicator__label--left">69%</span>
             </div>
         </div>
     </div>
     
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="69" aria-valuemax="100" aria-valuetext="69%" aria-label="69%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar" style="flex-basis: 100%">
-                <span class="fd-progress-indicator__label fd-progress-indicator__label--left">100%</span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 69%; width: 69%;">
+                <span class="fd-progress-indicator__label">69%</span>
             </div>
+            <div class="fd-progress-indicator__remaining"></div>
         </div>
     </div>
     
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="100" aria-valuemax="100" aria-valuetext="100%" aria-label="100%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar" style="flex-basis: 80%">
-                <span class="fd-progress-indicator__label fd-progress-indicator__label--left">80 of 100 MB</span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 100%; width: 100%">
+                <span class="fd-progress-indicator__label">100%</span>
             </div>
+            <div class="fd-progress-indicator__remaining"></div>
+        </div>
+    </div>
+    
+    <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="80" aria-valuemax="100" aria-valuetext="80 of 100 MB" aria-label="80 of 100 MB">
+        <div class="fd-progress-indicator__container">
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 80%; width: 80%">
+                <span class="fd-progress-indicator__label">80 of 100 MB</span>
+            </div>
+            <div class="fd-progress-indicator__remaining"></div>
         </div>
     </div>
 `;
@@ -74,85 +88,78 @@ layout.parameters = {
             Show the current progress as a percentage value between 0% and 100%.
             Alternatively, you can show the current progress as text in addition to the bar. 
             In this case, the text is shown on the right of the bar if the progress is 50% or less. In all other cases, the progress is shown right-aligned on the bar itself
-            You also have the option of showing any application-specific text instead of a percentage
+            You also have the option of showing any application-specific text instead of a percentage.
         `
     }
 };
 
 export const valueStates = () => `
-     <div class="fd-progress-indicator">
+     <div class="fd-progress-indicator" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area"><span class="fd-progress-indicator__label">42%</span></div>  
-        </div>
-    </div>
-   
-    <div class="fd-progress-indicator">
-        <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar fd-progress-indicator__progress-bar--informative" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area">
-                <span class="fd-progress-indicator__icon fd-progress-indicator__icon--informative"></span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">42%</span>
             </div>
         </div>
     </div>
    
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator fd-progress-indicator--informative" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar fd-progress-indicator__progress-bar--positive" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area">
-                <span class="fd-progress-indicator__icon fd-progress-indicator__icon--positive"></span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
+                <span class="fd-progress-indicator__label">42%</span>
+            </div>
+        </div>
+    </div>
+   
+    <div class="fd-progress-indicator fd-progress-indicator--positive" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
+        <div class="fd-progress-indicator__container">
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">42%</span>
             </div>
         </div>
     </div>
     
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator fd-progress-indicator--critical" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar fd-progress-indicator__progress-bar--critical" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area">
-                <span class="fd-progress-indicator__icon fd-progress-indicator__icon--critical"></span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">42%</span>
-            </div>  
+            </div>
         </div>
     </div>
    
-    <div class="fd-progress-indicator">
+    <div class="fd-progress-indicator fd-progress-indicator--negative" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="42" aria-valuemax="100" aria-valuetext="42%" aria-label="42%">
         <div class="fd-progress-indicator__container">
-            <div class="fd-progress-indicator__progress-bar fd-progress-indicator__progress-bar--negative" style="flex-basis: 42%"></div>
-            <div class="fd-progress-indicator__remain-area">
-                <span class="fd-progress-indicator__icon fd-progress-indicator__icon--negative"></span>
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 42%; width: 42%;"></div>
+            <div class="fd-progress-indicator__remaining">
                 <span class="fd-progress-indicator__label">42%</span>
             </div>
         </div>
     </div>
 `;
 
-valueStates.storyName = 'Value States';
+valueStates.storyName = 'States';
 valueStates.parameters = {
     docs: {
         iframeHeight: 600,
-        storyDescription: 'The progress indicator can visualize different value states that are represented by various theme-dependent semantic colors. The states are: normal, success, warning, error, and information. The icons showing depends on theme value'
+        storyDescription: 'The progress indicator can visualize different value states that are represented by various theme-dependent semantic colors. The states are: normal, success, warning, error, and information.'
     }
 };
 
-export const truncation = () => `
-    <div class="example-container" style="margin-bottom: 50px">
-        <div class="fd-popover" style="width: 60%">
-            <div class="fd-popover__control">
-                <div class="fd-progress-indicator">
-                    <div class="fd-progress-indicator__container">
-                        <div class="fd-progress-indicator__progress-bar" style="flex-basis: 69%" onclick="onPopoverClick('popoverF0')">
-                            <span class="fd-progress-indicator__label fd-progress-indicator__label--left">69% Reduce container width until this text is truncated, then press the ProgressIndicator ProgressIndicator ProgressIndicator</span>
-                        </div>
-                    </div>
-                </div>
+export const truncation = () => `<div style="width: 400px; margin: auto;">
+    <div class="fd-progress-indicator fd-popover" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="70" aria-valuemax="100" aria-valuetext="In cases where the label is being truncated a small popover appears on click." aria-label="Progress Indicator">
+        <div class="fd-progress-indicator__container">
+            <div class="fd-progress-indicator__progress-bar fd-popover__control" style="min-width: 70%; width: 70%;" onclick="onPopoverClick('popoverF0')">
+                <span class="fd-progress-indicator__label">In cases where the label is being truncated a small popover appears on click.</span>
             </div>
+            <div class="fd-progress-indicator__remaining"></div>
             <div class="fd-popover__body" aria-hidden="false" id="popoverF0">
                 <div class="fd-popover__wrapper">
-                    <div class="fd-progress-indicator__truncate-popover">
-                        <span>69% Reduce container width until this text is truncated, then press the ProgressIndicator</span>
-                        <button class="close-popover">
+                    <div class="fd-progress-indicator__overflow">
+                        <span>In cases where the label is being truncated a small popover appears on click.</span>
+                        <span class="fd-progress-indicator__overflow-close">
                             <i class="sap-icon sap-icon--decline"></i>
                         </span>
                     </div>
@@ -160,35 +167,37 @@ export const truncation = () => `
             </div>
         </div>
     </div>
-   
-   <div class="example-container" style="margin-bottom: 50px">
-        <div class="fd-popover" style="width: 60%">
-            <div class="fd-popover__control">
-                <div class="fd-progress-indicator">
-                    <div class="fd-progress-indicator__container" onclick="onPopoverClick('popoverF1')">
-                        <div class="fd-progress-indicator__progress-bar" style="flex-basis: 45%"></div>
-                        <span class="fd-progress-indicator__label">45% Reduce container width until this text is truncated, then press the ProgressIndicator</span>
-                    </div>
-                </div>
+
+    <div style="margin-bottom: 5rem"></div>
+
+    <div class="fd-progress-indicator fd-popover" tabindex="-1" role="progressbar" aria-valuemin="0" aria-valuenow="70" aria-valuemax="100" aria-valuetext="In cases where the label is being truncated a small popover appears on click." aria-label="Progress Indicator">
+        <div class="fd-progress-indicator__container">
+            <div class="fd-progress-indicator__progress-bar" style="min-width: 30%; width: 30%;">
+            </div>
+            <div class="fd-progress-indicator__remaining fd-popover__control" onclick="onPopoverClick('popoverF1')">
+                <span class="fd-progress-indicator__label">In cases where the label is being truncated a small popover appears on click.</span>
             </div>
             <div class="fd-popover__body fd-popover__body--right" aria-hidden="false" id="popoverF1">
                 <div class="fd-popover__wrapper">
-                    <div class="fd-progress-indicator__truncate-popover">
-                        <span>Reduce container width until this text is truncated, then press the ProgressIndicator</span>
-                        <button class="close-popover">
+                    <div class="fd-progress-indicator__overflow">
+                        <span>In cases where the label is being truncated a small popover appears on click.</span>
+                        <span class="fd-progress-indicator__overflow-close">
                             <i class="sap-icon sap-icon--decline"></i>
-                        </button>
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div style="margin-bottom: 5rem"></div>
+</div>
 `;
 
-truncation.storyName = 'Text Truncation';
+truncation.storyName = 'Truncation Behaviour';
 truncation.parameters = {
     docs: {
         iframeHeight: 600,
-        storyDescription: 'If the length of the text exceeds the available space in the progress indicator, the text truncates. In this case, clicking the progress indicator displays an information popover with the full text. The close popover button should be focused when it is opened'
+        storyDescription: 'If the length of the text exceeds the available space in the progress indicator, the text truncates. In this case, clicking the progress indicator displays an information popover with the full text.'
     }
 };
