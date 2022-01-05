8821782965b5ccdf4a499ae2d6bc71afd941adba
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-01-05 15:05:27 -0500

    feat(fn): new changes for card

--- stories/fn-card/fn-card.stories.js
+++ stories/fn-card/fn-card.stories.js
@@ -1,18 +1,24 @@
 export default {
     title: 'Experimental/Card',
     parameters: {
-        components: ['fn-card', 'fn-avatar', 'icon', 'fn-info-label', 'fn-title']
+        components: ['fn-card', 'fn-avatar', 'icon', 'fn-info-label', 'fn-title', 'fn-button'],
+        description: `
+There are two types of cards - "container" cards (default), and "data" cards which are interactive / hoverable / focusable / selectable. Typically container cards will contain complex
+controls within the card whereas the data cards are themselves what the user will be interacting with.
+<br><br>
+By default, cards only have a minimum width, so the developer should set width rules or keep the card in a container of appropriate width.
+`
     }
 };
 
-export const card = () => `
-    <div class="fn-card">
+export const Card = () => `
+    <div class="fn-card" style="max-width: 40rem">
         <span class="fn-info-label fn-info-label--yellow">
             <span class="fn-info-label__circle"></span>
             <span class="fn-info-label__text">In Progress</span>
         </span>
-        <h1 class="fn-card__title">Card</h1>
-        <span class="fn-card__description">Control Description</span>
+        <h1 class="fn-card__title--large">Card - Large Title</h1>
+        <span class="fn-card__description">Card Description</span>
         <div class="fn-card__container">
             <span class="fn-card__container-title">Types</span>
             <div class="fn-card__container-item">
@@ -56,8 +62,45 @@ export const card = () => `
     </div>
 `;
 
-card.parameters = {
+Card.parameters = {
     docs: {
-        storyDescription: 'This is a card component with an info label, title, description, and some card containers containing additional information.'
+        storyDescription: 'This is a container (default) card component with an info label, title, description, and some card containers containing additional information.'
+    }
+};
+
+export const DataCard = () => `
+    <div class="fn-card fn-card--data" style="max-width: 40rem">
+        <h1 class="fn-card__title">Data Card Title</h1>
+        <span class="fn-card__subtitle">Card Subtitle</span>
+    </div>
+    <br/><br/>
+    <div class="fn-card fn-card--data is-focus" style="max-width: 40rem">
+        <div class="fn-card__title-icon-button-container">
+            <h1 class="fn-card__title">Focused Data Card Title</h1>
+            <button class="fn-button fn-button--ghost fn-card__title-button">
+                <span class="fn-button__text">Button</span>
+            </button>
+        </div>
+    </div>
+    <br/><br/>
+    <div class="fn-card fn-card--data is-selected" aria-selected="true" style="max-width: 40rem">
+        <div class="fn-card__title-icon-button-container">
+            <div class="fn-avatar fn-avatar--xs fn-avatar--circle fn-card__title-icon">
+                <div class="fn-avatar__icon">
+                    <span class="sap-icon sap-icon--accept"></span>
+                </div>
+            </div>
+            <div class="fn-card__title-container">
+                <h1 class="fn-card__title">Selected Data Card Title</h1>
+                <span class="fn-card__subtitle">Card Subtitle</span>
+            </div>
+        </div>
+    </div>
+    <br/><br/>  
+`;
+
+DataCard.parameters = {
+    docs: {
+        storyDescription: 'This is a data card with hover, focus, and selected states.'
     }
 };
