82f409bbdb02e1961695cafce352bb09b2eb7d25
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2022-01-07 18:23:52 -0500

    feat(fn): introduce Mutli-Input and Combobox [ci visual] (#3059)

--- stories/fn-step-input/fn-step-input.stories.js
+++ stories/fn-step-input/fn-step-input.stories.js
@@ -26,12 +26,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-1">normal:</label></div>
     <div class="fn-input fn-step-input" tabindex="0">
-        <button class="fn-nested-button" aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field" type="number" id="field-1" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
@@ -40,12 +40,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-2">hover:</label></div>
     <div class="fn-input fn-step-input" tabindex="0">
-        <button class="fn-nested-button" aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field is-hover" type="number" id="field-2" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
@@ -54,12 +54,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-3">active:</label></div>
     <div class="fn-input fn-step-input" tabindex="0">
-        <button class="fn-nested-button" aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field is-active" type="number" id="field-3" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
@@ -68,12 +68,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-4">focus:</label></div>
     <div class="fn-input fn-step-input" tabindex="0">
-        <button class="fn-nested-button" aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field is-focus" type="number" id="field-4" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
@@ -82,12 +82,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-5">disabled:</label></div>
     <div class="fn-input fn-step-input" tabindex="-1">
-        <button class="fn-nested-button" disabled aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" disabled aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field" disabled type="number" id="field-5" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" disabled aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" disabled aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
@@ -97,12 +97,12 @@ export const StepInput = () => `${localStyles}
 <div class="docs-fn-container">
     <div><label for="field-6">display:</label></div>
     <div class="fn-input fn-step-input is-display" tabindex="-1">
-        <button class="fn-nested-button" aria-label="subtract button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-left" aria-label="subtract button" tabindex="-1">
             <span class="sap-icon sap-icon--less"></span>
         </button>
         <input class="fn-input__text-field fn-step-input__text-field is-display" type="number" id="field-6" value="42" tabindex="-1" />
         <div class="fn-input__border"></div>
-        <button class="fn-nested-button" aria-label="add button" tabindex="-1">
+        <button class="fn-nested-button fn-nested-button--absolute-right" aria-label="add button" tabindex="-1">
             <span class="sap-icon sap-icon--add"></span>
         </button>
     </div>
