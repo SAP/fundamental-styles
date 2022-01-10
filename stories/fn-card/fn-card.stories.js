9921b41a8c056f706c2006a5bb33ff019afdf702
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-01-10 09:14:19 -0700

    feat(fn): remove non-card things

--- stories/fn-card/fn-card.stories.js
+++ stories/fn-card/fn-card.stories.js
@@ -1,7 +1,7 @@
 export default {
     title: 'Experimental/Card',
     parameters: {
-        components: ['fn-card', 'fn-avatar', 'icon', 'fn-info-label', 'fn-title', 'fn-button'],
+        components: ['fn-card', 'icon', 'fn-button'],
         description: `
 There are two types of cards - "container" cards (default), and "data" cards which are interactive / hoverable / focusable / selectable. Typically container cards will contain complex
 controls within the card whereas the data cards are themselves what the user will be interacting with.
@@ -13,58 +13,14 @@ By default, cards only have a minimum width, so the developer should set width r
 
 export const Card = () => `
     <div class="fn-card" style="max-width: 40rem">
-        <span class="fn-info-label fn-info-label--yellow">
-            <span class="fn-info-label__circle"></span>
-            <span class="fn-info-label__text">In Progress</span>
-        </span>
-        <h1 class="fn-card__title--large">Card - Large Title</h1>
-        <span class="fn-card__description">Card Description</span>
-        <div class="fn-card__container">
-            <span class="fn-card__container-title">Types</span>
-            <div class="fn-card__container-item">
-                <div class="fn-avatar fn-avatar--xs fn-avatar--lime">
-                    <div class="fn-avatar__icon">
-                        <span class="sap-icon sap-icon--accept"></span>
-                    </div>
-                </div>
-                <span>Standard</span>
-            </div>
-            <div class="fn-card__container-item">
-                <div class="fn-avatar fn-avatar--xs fn-avatar--crimson">
-                    <div class="fn-avatar__icon">
-                        <span class="sap-icon sap-icon--cancel"></span>
-                    </div>
-                </div>
-                <span>Catalog</span>
-            </div>
-        </div>
-        <div class="fn-card__container">
-            <span class="fn-card__container-title">Search Metadata</span>
-            <div class="fn-card__container-item">
-                <span>keywords, comma, separated</span>
-            </div>
-        </div>
-        <div class="fn-card__container">
-            <span class="fn-card__container-title">Contributors</span>
-            <div class="fn-card__container-item">
-                <div class="fn-avatar fn-avatar--circle fn-avatar--xs fn-avatar--indigo">
-                    <div class="fn-avatar__initials">J</div>
-                </div>
-                <span>Jane Doe</span>
-            </div>
-            <div class="fn-card__container-item">
-                <div class="fn-avatar fn-avatar--circle fn-avatar--xs fn-avatar--thumbnail">
-                    <div class=" fn-avatar__thumbnail" role="img" aria-label="John Doe" style="background-image: url('/assets/images/backgrounds/city.jpg')"></div>
-                </div>
-                <span>John Doe</span>
-            </div>
-        </div>
+        <h1 class="fn-card__title">Card Title</h1>
+        <span class="fn-card__subtitle">Card Subtitle</span>
     </div>
 `;
 
 Card.parameters = {
     docs: {
-        storyDescription: 'This is a container (default) card component with an info label, title, description, and some card containers containing additional information.'
+        storyDescription: 'This is a container (default) card component.'
     }
 };
 
