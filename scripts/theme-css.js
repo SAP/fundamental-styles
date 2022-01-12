1a017e8268a3ec62ce80bfb6e0d8ebb0631a1430
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-01-12 22:59:20 +0400

    refactor(deps): swapped node-sass with dart-sass

--- scripts/theme-css.js
+++ scripts/theme-css.js
@@ -3,9 +3,14 @@ const path = require('path');
 const supportedThemes = require('../config/constants');
 
 fs.readdirSync('dist').forEach(file => {
-    const fileName = file.replace(/\.[^.]+$/, '');
-
-    if (file.indexOf('.css') === -1) return;
+    let originalExtension;
+    const extensionRegex = /\.[^.]+$/;
+    const extensionRegexMatch = file.match(extensionRegex);
+    const fileName = file.replace(extensionRegex, '');
+    if (extensionRegexMatch) {
+        originalExtension = extensionRegexMatch[0].replace('.', '');
+    }
+    if (originalExtension !== 'css') return;
 
     supportedThemes.forEach(theme => {
         fs.copyFile(path.resolve(__dirname, `../dist/${file}`), `dist/${fileName}-${theme}.css`, (err) => {
