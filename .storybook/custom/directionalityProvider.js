33e007f6b595a97932cc361d6ff48ef28e9049f7
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-01-26 02:50:59 +0400

    fix(deps): fixed directionality for visual components

--- .storybook/custom/directionalityProvider.js
+++ .storybook/custom/directionalityProvider.js
@@ -4,7 +4,15 @@ export const withDirectionality = makeDecorator({
     name: 'directionalityProvider',
     parameterName: 'directionality',
     wrapper: (storyFn, context) => {
-        return `<div dir='${context?.globals?.directionality || 'ltr'}'>${storyFn(context)}</div>`;
+        const directionality = context?.globals?.directionality || 'ltr';
+        const story = storyFn(context);
+        if (typeof story === 'string') {
+            // visual stories return HTMLElements, whereas ordinary stories return html strings
+            return `<div dir='${directionality}'>${storyFn(context)}</div>`;
+        }
+        if (story instanceof HTMLElement) {
+            story.setAttribute('dir', directionality);
+        }
+        return story;
     }
 });
-
