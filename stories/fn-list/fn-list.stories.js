07c39af30816e8e95be6081c150af7114bbef103
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-01-31 14:34:49 -0700

    feat(fn): implement fn-list

--- stories/fn-list/fn-list.stories.js
+++ stories/fn-list/fn-list.stories.js
@@ -1,39 +1,58 @@
 export default {
     title: 'Experimental/List',
     parameters: {
-        description: `
-        
-**Modifier classes for list style:**
-
-| Style&nbsp;&nbsp;&nbsp;&nbsp;     | Modifier class           |
-| --------------------------------- | ------------------------ |
-| default &nbsp;&nbsp;&nbsp;&nbsp;        | \`none\`                  |
-| emphasized &nbsp;&nbsp;&nbsp;&nbsp;     | \`fn-list--emphasized\` |
-
-        `,
-        components: ['fn-list', 'icon']
+        components: ['fn-list', 'icon', 'fn-button', 'fn-checkbox', 'fn-object-status', 'fn-avatar']
     }
 };
 
-const localStyles = `
+export const Primary = () => `
     <ul class="fn-list">
         <li class="fn-list__item" tabindex="0">
-            <span>List Item 1</span>
+            <span class="fn-list__text">List Item</span>
         </li>
         <li class="fn-list__item" tabindex="0">
-            <span>List Item 2</span>
+            <span class="fn-list__text">List Item With Buttons</span>
+            <div class="fn-list__end-container">
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--ghost">
+                    <span class="sap-icon sap-icon--edit"></span>
+                </button>
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--ghost">
+                    <span class="sap-icon sap-icon--decline"></span>
+                </button>
+            </div>
         </li>
         <li class="fn-list__item" tabindex="0">
-            <span>List Item 3</span>
+            <span class="fn-list__text">List Item With Navigation Arrow</span>
+            <div class="fn-list__end-container">
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--transparent">
+                    <span class="sap-icon sap-icon--navigation-right-arrow"></span>
+                </button>
+            </div>
+        </li>
+        <li class="fn-list__item is-selected" tabindex="0">
+            <span>Selected List Item</span>
         </li>
         <li class="fn-list__item" tabindex="0">
-            <span>List Item 4</span>
+            <span class="fn-list__text">List Item with Text at the End</span>
+            <div class="fn-list__end-container">
+                <span>42</span>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <span class="fn-list__icon sap-icon sap-icon--person-placeholder"></span>
+            <span class="fn-list__text">List Item with Icon</span>
+        </li>
+        <li class="fn-list__item is-disabled" aria-disabled="true" tabindex="0">
+            <span>Disabled List Item</span>
+        </li>
+        <li class="fn-list__item fn-list__item--has-checkbox" tabindex="0">
+                <div class="fn-checkbox" tabindex="0">
+                    <input class="fn-checkbox__input" id="checkbox-1" name="checkbox-1" type="checkbox" tabindex="-1" aria-label="checkbox">
+                    <span class="fn-checkbox__checkmark"></span>
+                </div>
+            <span class="fn-list__text">List Item with Checkbox</span>
         </li>
     </ul>
-`;
-
-export const Primary = () => `${localStyles}
-
 `;
 
 Primary.storyName = 'List';
@@ -41,7 +60,109 @@ Primary.storyName = 'List';
 Primary.parameters = {
     docs: {
         description: {
-            story: 'List'
+            story: 'This is the standard list where information is presented on 1 line.'
+        }
+    }
+};
+
+export const Byline = () => `
+    <ul class="fn-list fn-list--byline">
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-avatar fn-list__avatar fn-avatar--circle fn-avatar--m fn-avatar--indigo">
+                <div class="fn-avatar__initials">M</div>
+            </div>
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item with Avatar Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item with Status Title</span>
+                <div class="fn-object-status fn-object-status--byline fn-object-status--critical">
+                    <span class="sap-icon sap-icon--status-critical"></span>
+                    <div class="fn-object-status__text">Critical</div>
+                </div>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item With Buttons Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+            <div class="fn-list__end-container">
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--ghost">
+                    <span class="sap-icon sap-icon--edit"></span>
+                </button>
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--ghost">
+                    <span class="sap-icon sap-icon--decline"></span>
+                </button>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item with Navigation Arrow Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+            <div class="fn-list__end-container">
+                <button class="fn-button fn-list__button fn-button--icon-only fn-button--transparent">
+                    <span class="sap-icon sap-icon--navigation-right-arrow"></span>
+                </button>
+            </div>
+        </li>
+        <li class="fn-list__item is-selected" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline Selected List Item Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item with Text at the End Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+            <div class="fn-list__end-container">
+                <span>42</span>
+            </div>
+        </li>
+        <li class="fn-list__item" tabindex="0">
+            <span class="fn-list__icon sap-icon sap-icon--person-placeholder"></span>
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Byline List Item with Icon Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+        </li>
+        <li class="fn-list__item is-disabled" aria-disabled="true" tabindex="0">
+            <div class="fn-list__start-container">
+                <span class="fn-list__title">Disabled Byline List Item Title</span>
+                <span class="fn-list__byline">List Item Byline</span>
+            </div>
+        </li>
+        <li class="fn-list__item fn-list__item--has-checkbox" tabindex="0">
+                <div class="fn-checkbox" tabindex="0">
+                    <input class="fn-checkbox__input" id="checkbox-1" name="checkbox-1" type="checkbox" tabindex="-1" aria-label="checkbox">
+                    <span class="fn-checkbox__checkmark"></span>
+                </div>
+                <div class="fn-list__start-container">
+                    <span class="fn-list__title">Byline List Item with Checkbox Title</span>
+                    <span class="fn-list__byline">List Item Byline</span>
+                </div>
+        </li>
+    </ul>
+`;
+
+Byline.storyName = 'Byline List';
+
+Byline.parameters = {
+    docs: {
+        description: {
+            story: 'This is the byline list, where the list items are taller than the default list and information can be conveyed on two lines.'
         }
     }
 };
