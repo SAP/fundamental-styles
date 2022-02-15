b4e613aca14431abf147b3f5aab1d4d5243b0cb4
droshev <mladen.droshev@sap.com>
2022-02-14 19:21:54 -0500

    fix(styles): remove the colors for info label texts [ci visual]

--- stories/info-label/info-label.stories.js
+++ stories/info-label/info-label.stories.js
@@ -20,78 +20,78 @@ Use the Info Label base class with following modifiers:
  * Options include numbers from 1 to 10.
  */
 
-export const Colors = () => `<div class="fd-info-label fd-info-label--accent-color-1">
+export const Colors = () => `<span class="fd-info-label fd-info-label--accent-color-1">
     <span class="fd-info-label__text">color 1</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-2">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-2">
     <span class="fd-info-label__text">color 2</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-3">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-3">
     <span class="fd-info-label__text">color 3</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-4">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-4">
     <span class="fd-info-label__text">color 4</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-5">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-5">
     <span class="fd-info-label__text">color 5</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-6">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-6">
     <span class="fd-info-label__text">color 6</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-7">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-7">
     <span class="fd-info-label__text">color 7</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-8">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-8">
     <span class="fd-info-label__text">color 8</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-9">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-9">
     <span class="fd-info-label__text">color 9</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-10">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-10">
     <span class="fd-info-label__text">color 10</span>
-</div>
+</span>
 `;
 Colors.storyName = 'Color Flavors';
 
 /** Use the `fd-info-label--icon` modifier class and icon type to create Info Label with Icon. */
 
-export const Icons = () => `<div class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon">
+export const Icons = () => `<span class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon">
     <i role="presentation" class="fd-info-label__icon sap-icon--future"></i>
     <span class="fd-info-label__text">Info Label</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon">
     <i role="presentation" class="fd-info-label__icon sap-icon--upload-to-cloud"></i>
-</div>
+</span>
 `;
 Icons.storyName = 'Info Label with Icon';
 
 /** For Numeric Info Label use the `fd-info-label--numeric` modifier class. */
 
-export const Numeric = () => `<div class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-1">
+export const Numeric = () => `<span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-1">
     <span class="fd-info-label__text">6</span>
-</div>
-<div class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-2">
+</span>
+<span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-2">
     <span class="fd-info-label__text">6.2</span>
-</div>
-<div class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-3">
+</span>
+<span class="fd-info-label fd-info-label--numeric fd-info-label--accent-color-3">
     <span class="fd-info-label__text">42k</span>
-</div>
+</span>
 `;
 Numeric.storyName = 'Numeric Info Label';
 
 /** For Display Only Info Label use the `fd-info-label--display` modifier class */
 
-export const DisplayOnlyInfoLabel = () => `<div class="fd-info-label fd-info-label--display">
+export const DisplayOnlyInfoLabel = () => `<span class="fd-info-label fd-info-label--display">
     <span class="fd-info-label__text">system state</span>
-</div>
-<div class="fd-info-label fd-info-label--display fd-info-label--numeric">
+</span>
+<span class="fd-info-label fd-info-label--display fd-info-label--numeric">
     <span class="fd-info-label__text">42</span>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon fd-info-label--display ">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-2 fd-info-label--icon fd-info-label--display ">
     <i role="presentation" class="fd-info-label__icon sap-icon--upload-to-cloud"></i>
-</div>
-<div class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon fd-info-label--display">
+</span>
+<span class="fd-info-label fd-info-label--accent-color-1 fd-info-label--icon fd-info-label--display">
     <i role="presentation" class="fd-info-label__icon sap-icon--future"></i>
     <span class="fd-info-label__text">Info Label</span>
-</div>
+</span>
 `;
