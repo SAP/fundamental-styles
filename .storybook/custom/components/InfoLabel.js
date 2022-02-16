f47c297cf98650b5c329e644f1737d87bbe80cc1
deno <mladen.droshev@sap.com>
2022-02-16 17:22:36 -0500

    feat(styles): info label display mode, update design [ci visual] (#3189)
    BREAKING CHANGE:
    update release notes

--- .storybook/custom/components/InfoLabel.js
+++ .storybook/custom/components/InfoLabel.js
@@ -3,7 +3,7 @@ import tagData from './tags.json';
 
 const InfoLabel = ({ tag }) => {
     return (
-        tagData[tag] && <span title={tagData[tag]?.title} className={`fddocs fd-info-label fd-info-label--accent-color-${tagData[tag]?.color}`}>{tagData[tag]?.name}</span>
+        tagData[tag] && <span title={tagData[tag]?.title} className={`fddocs fd-info-label fd-info-label--accent-color-${tagData[tag]?.color}`}><span class="fd-info-label__text">{tagData[tag]?.name}</span></span>
     )
 }
 
