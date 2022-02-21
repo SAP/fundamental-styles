85485d820c18b51c56c7656124ea02abddcc9a3a
deno <mladen.droshev@sap.com>
2022-02-21 17:49:34 -0500

    feat(styles): bring horizon themes [ci visual] (#3201)

--- .storybook/custom/constants/availableThemes.js
+++ .storybook/custom/constants/availableThemes.js
@@ -4,5 +4,8 @@ export default [
     { value: 'sap_fiori_3_light_dark', title: 'Light Dark' },
     { value: 'sap_fiori_3_hcw', title: 'High Contrast White' },
     { value: 'sap_fiori_3_hcb', title: 'High Contrast Black' },
-    { value: 'sap_horizon', title: 'Horizon' }
+    { value: 'sap_horizon', title: 'Morning Horizon' },
+    { value: 'sap_horizon_hcb', title: 'High Contrast Black Horizon' },
+    { value: 'sap_horizon_hcw', title: 'High Contrast White Horizon' },
+    { value: 'sap_horizon_dark', title: 'Evening Horizon' }
 ];
