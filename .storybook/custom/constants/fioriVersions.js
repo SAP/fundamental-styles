d806f6b6e18a0ec0c82fedb8b8a804e4a4edae17
deno <mladen.droshev@sap.com>
2022-03-16 12:48:53 -0400

    test(fn): update btp design label (#3245)

--- .storybook/custom/constants/fioriVersions.js
+++ .storybook/custom/constants/fioriVersions.js
@@ -1,5 +1,5 @@
 export default [
     {value: 'all', title: 'All'},
     { value: 'fiori', title: 'SAP Fiori' },
-    { value: 'fioriNext', title: 'BTP Fiori Next' }
+    { value: 'fioriNext', title: 'BTP Experimental Design' }
 ];
