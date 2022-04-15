60d7a7518df9d5374b5dc6e0a81c86a3c05e33a9
deno <mladen.droshev@sap.com>
2022-04-15 14:42:01 -0400

    chore(ci): fix rc script (#3332)

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -3,7 +3,7 @@
 set -e
 
 PACKAGE_PREFIX=@fundamental-styles
-PACKAGES=( (theming-preview dist-theming) (common-css dist-common-css) (fn dist-fn) (fn-icons dist-fn-icons) )
+PACKAGES=(("theming-preview" "dist-theming") ("common-css" "dist-common-css") ("fn" "dist-fn") ("fn-icons" "dist-fn-icons"))
 
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
