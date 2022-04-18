bf840b80cc7e9d6aca9e772a1f0f43b43ab2f465
droshev <mladen.droshev@sap.com>
2022-04-18 17:43:04 -0400

    chore(ci): fix rc publishing script

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -3,7 +3,8 @@
 set -e
 
 PACKAGE_PREFIX=@fundamental-styles
-PACKAGES=(("theming-preview" "dist-theming") ("common-css" "dist-common-css") ("fn" "dist-fn") ("fn-icons" "dist-fn-icons"))
+PACKAGES=("theming-preview" "common-css" "fn" "fn-icons")
+DISTFOLDERS=( "dist-theming" "dist-common-css" "dist-fn" "dist-fn-icons")
 
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
@@ -17,13 +18,13 @@ npm run storybook:static
 
 npm publish --tag prerelease
 
-for p in "${!PACKAGES[@]}"
+for p in "${!DISTFOLDERS[@]}"
 do
   :
-      echo "publish ${PACKAGE_PREFIX}/${PACKAGES[p][0]}"
-      echo "directory: ${PACKAGES[p][1]}"
+      echo "publish ${PACKAGE_PREFIX}/${PACKAGES[p]}"
+      echo "directory: ${DISTFOLDERS[p]}"
 
-      cd ${PACKAGES[p][1]}
+      cd ${DISTFOLDERS[p]}
       npm publish --tag prerelease
       cd ..
 done
