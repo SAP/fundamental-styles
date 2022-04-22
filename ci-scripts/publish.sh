0dff4a0f9b6b3f9d1e1c5c944a27a979b62a1f4b
deno <mladen.droshev@sap.com>
2022-04-22 10:12:19 -0400

    chore(ci): optimize release and hotfix scripts (#3349)

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -4,16 +4,9 @@ set -e
 
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
-
-PACKAGE_THEMING_PREVIEW=theming-preview
 PACKAGE_PREFIX=@fundamental-styles
-DIST_THEMING_PREVIEW=dist-theming
-DIST_COMMON_CSS=dist-common-css
-PACKAGE_FN=fn
-DIST_FN=dist-fn
-PACKAGE_FN_ICONS=fn-icons
-PACKAGE_COMMON_CSS=common-css
-DIST_FN_ICONS=dist-fn-icons
+PACKAGES=("theming-preview" "common-css" "fn" "fn-icons")
+DISTFOLDERS=( "dist-theming" "dist-common-css" "dist-fn" "dist-fn-icons")
 
 # delete temp branch
 git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH" > /dev/null 2>&1;
@@ -38,35 +31,16 @@ npm run build:prod
 
 npm publish
 
-# publish fn package
-echo publish "${PACKAGE_PREFIX}/${PACKAGE_FN}"
+for p in "${!DISTFOLDERS[@]}"
+do
+  :
+      echo "publish ${PACKAGE_PREFIX}/${PACKAGES[p]}"
+      echo "directory: ${DISTFOLDERS[p]}"
 
-cd ${DIST_FN}
-npm publish
-cd ..
-
-# publish fn-icons package
-echo publish "${PACKAGE_PREFIX}/${PACKAGE_FN_ICONS}"
-
-cd ${DIST_FN_ICONS}
-npm publish
-cd ..
-
-# publish common-css package
-echo publish "${PACKAGE_PREFIX}/${PACKAGE_COMMON_CSS}"
-
-cd ${DIST_COMMON_CSS}
-npm publish
-cd ..
-
-# build dist-theming package
-npm run build:theming-preview
-npm run sync-versions
-
-echo publish "${PACKAGE_PREFIX}/${PACKAGE_THEMING_PREVIEW}"
-cd ${DIST_THEMING_PREVIEW}
-npm publish
-cd ..
+      cd ${DISTFOLDERS[p]}
+      npm publish
+      cd ..
+done
 
 # run this after publish to make sure GitHub finishes updating from the push
 npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch main
