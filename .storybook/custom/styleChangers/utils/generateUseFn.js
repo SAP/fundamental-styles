fe713dbe1d411e0a23f55f68419c56806515d911
N1XUS <denis.severin@valor-software.com>
2022-04-27 17:06:44 +0300

    fix(styles): load styles once [ci visual] (#3362)

--- .storybook/custom/styleChangers/utils/generateUseFn.js
+++ .storybook/custom/styleChangers/utils/generateUseFn.js
@@ -1,5 +1,13 @@
-export default (linkTag) => {
+export default (linkTag, uniqueIdentifier) => {
     return () => {
+        if (uniqueIdentifier) {
+            const uniqueValue = linkTag.getAttribute(uniqueIdentifier);
+            const tagName = linkTag.tagName;
+            // If such element with such unique attribute value already loaded on the page, no need to duplicate it once again.
+            if (document.querySelector(`${tagName}[${uniqueIdentifier}="${uniqueValue}"]`) !== null) {
+                return;
+            }
+        }
         document.head.appendChild(linkTag);
     };
 }
