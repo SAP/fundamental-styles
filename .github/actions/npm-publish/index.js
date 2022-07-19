e6bd9980c96db888790e9d604901410e8aa28411
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-20 02:14:18 +0400

    feat(ci): upgraded ci workflows

---
+++ .github/actions/npm-publish/index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const npmPublish = require('@jsdevtools/npm-publish');
+
+const packagePaths = core.getInput('packagePaths').split(',').map(path => `./${path}/package.json`);
+const isPrerelease = core.getInput('isPrerelease') !== 'false';
+const isHotfix = core.getInput('isHotfix') !== 'false';
+const npmToken = core.getInput('token');
+
+let tag = 'latest';
+
+if (isPrerelease) {
+    tag = 'prerelease';
+}
+if (isHotfix) {
+    tag = 'archive';
+}
+
+const run = async () => {
+    for (const packagePath of packagePaths) {
+        const result = await npmPublish({
+            package: packagePath,
+            token: npmToken,
+            tag,
+            dryRun: true
+        });
+        core.info(`Published ${result.package}@${result.version}`);
+    }
+};
+
+run();
