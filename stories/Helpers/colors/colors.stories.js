9165ed4ff722e6eb9071403cf61420fb9ad01683
Inna Atanasova <i.atanasova@sap.com>
2022-10-12 16:20:43 -0400

    feat(styles): address PR reveiw comments

--- stories/Helpers/colors/colors.stories.js
+++ stories/Helpers/colors/colors.stories.js
@@ -17,30 +17,30 @@ const localStyles = `
 </style>
 `;
 export const TextColor = () => `${localStyles}
-<h3>sapBrandColor</h3>
-<div class="sap-color-sapBrandColor">TEXT</div>
+<h3>brandColor (--sapBrandColor)</h3>
+<div class="sap-color-brandColor">TEXT</div>
 <br>
-<h3>sapPositiveTextColor</h3>
-<div class="sap-color-sapPositiveTextColor">TEXT</div>
+<h3>positiveTextColor (--sapPositiveTextColor)</h3>
+<div class="sap-color-positiveTextColor">TEXT</div>
 <br>
-<h3>sapCriticalTextColor</h3>
-<div class="sap-color-sapCriticalTextColor">TEXT</div>
+<h3>criticalTextColor (--sapCriticalTextColor)</h3>
+<div class="sap-color-criticalTextColor">TEXT</div>
 <br>
-<h3>sapNegativeTextColor</h3>
-<div class="sap-color-sapNegativeTextColor">TEXT</div>
+<h3>negativeTextColor (--sapNegativeTextColor)</h3>
+<div class="sap-color-negativeTextColor">TEXT</div>
 <br>
-<h3>sapInformativeTextColor</h3>
-<div class="sap-color-sapInformativeTextColor">TEXT</div>
+<h3>informativeTextColor (--sapInformativeTextColor)</h3>
+<div class="sap-color-informativeTextColor">TEXT</div>
 <br>
-<h3>sapNeutralTextColor</h3>
-<div class="sap-color-sapNeutralTextColor">TEXT</div>
+<h3>neutralTextColor (--sapNeutralTextColor)</h3>
+<div class="sap-color-neutralTextColor">TEXT</div>
 `;
 TextColor.storyName = 'Text Color';
 TextColor.parameters = {
     docs: {
         iframeHeight: 150,
         description: {
-            story: `To set the text color of an element use the class <code>.sap-color-<i style="color: red;">colorName</i></code>, where <i style="color: red;">colorName</i> is the name of the css variable from the SAP-theming package that represent the desired color. <br><br>
+            story: `To set the text color of an element use the class <code>.sap-color-<i style="color: red;">colorName</i></code>, where <i style="color: red;">colorName</i> is the base of the css variable from the SAP-theming package that represent the desired color. <br><br>
             <b>SCSS Mixin:</b> 
             <code>@include  sap-color(colorName)</code>
             `
@@ -49,32 +49,32 @@ TextColor.parameters = {
 };
 
 export const BackgroundColor = () => `${localStyles}
-<h3>sapBrandColor</h3>
-<div class="demo sap-background-color-sapBrandColor"></div>
+<h3>brandColor (--sapBrandColor)</h3>
+<div class="demo sap-bg-color-brandColor"></div>
 <br>
-<h3>sapErrorBackground</h3>
-<div class="demo sap-background-color-sapErrorBackground"></div>
+<h3>errorBackground (--sapErrorBackground)</h3>
+<div class="demo sap-bg-color-errorBackground"></div>
 <br>
-<h3>sapWarningBackground</h3>
-<div class="demo sap-background-color-sapWarningBackground"></div>
+<h3>warningBackground (--sapWarningBackground)</h3>
+<div class="demo sap-bg-color-warningBackground"></div>
 <br>
-<h3>sapSuccessBackground</h3>
-<div class="demo sap-background-color-sapSuccessBackground"></div>
+<h3>successBackground (--sapSuccessBackground)</h3>
+<div class="demo sap-bg-color-successBackground"></div>
 <br>
-<h3>sapInformationBackground</h3>
-<div class="demo sap-background-color-sapInformationBackground"></div>
+<h3>informationBackground (--sapInformationBackground)</h3>
+<div class="demo sap-bg-color-informationBackground"></div>
 <br>
-<h3>sapNeutralBackground</h3>
-<div class="demo sap-background-color-sapNeutralBackground"></div>
+<h3>neutralBackground (--sapNeutralBackground)</h3>
+<div class="demo sap-bg-color-neutralBackground"></div>
 `;
 BackgroundColor.storyName = 'Background Color';
 BackgroundColor.parameters = {
     docs: {
         iframeHeight: 150,
         description: {
-            story: `To set the background color of an element use the class <code>.sap-background-color-<i style="color: red;">colorName</i></code>, where <i style="color: red;">colorName</i> is the name of the css variable from the SAP-theming package that represent the desired color. <br><br>
+            story: `To set the background color of an element use the class <code>.sap-bg-color-<i style="color: red;">colorName</i></code>, where <i style="color: red;">colorName</i> is the base of the css variable from the SAP-theming package that represent the desired color. <br><br>
             <b>SCSS Mixin:</b> 
-            <code>@include  sap-color(colorName, true)</code>
+            <code>@include  sap-bg-color(colorName)</code>
             `
         }
     }
