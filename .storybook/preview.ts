c782f5bc5d3e879d4174d42ef0257171a41389f5
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-10-29 00:52:56 +0400

    chore: added theming support to the common-css package

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -94,6 +94,7 @@ export const globalTypes = {
     },
     ['styles-theme']: {},
     ['fn-theme']: {},
+    ['common-css-theme']: {},
     directionality: {
         name: 'Directionality',
         description: 'Directionality of components',
