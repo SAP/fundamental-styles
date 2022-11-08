7475da77b34ba1083a573033c5151e6e522daf0d
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2022-11-08 16:44:05 +0400

    chore(cx): added themes to the CX package (#4011)
    * chore(cx): added themes to the CX package
    
    * chore: added cx to the commitlint scopes

--- packages/cx/package-config.ts
+++ packages/cx/package-config.ts
@@ -1,8 +1,48 @@
-import { PackageConfigInterface } from 'fundamental-styles/storybook';
+import {PackageConfigInterface} from "fundamental-styles/storybook";
 
-import { default as projects } from '../../projects';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+// @ts-ignore
+import sap_horizon from '../styles/theming/sap_horizon.scss?inline';
+// @ts-ignore
+import sap_horizon_dark from '../styles/theming/sap_horizon_dark.scss?inline';
+// @ts-ignore
+import sap_horizon_hcb from '../styles/theming/sap_horizon_hcb.scss?inline';
+// @ts-ignore
+import sap_horizon_hcw from '../styles/theming/sap_horizon_hcw.scss?inline';
+// @ts-ignore
+import sap_fiori_3 from '../styles/theming/sap_fiori_3.scss?inline';
+// @ts-ignore
+import sap_fiori_3_dark from '../styles/theming/sap_fiori_3_dark.scss?inline';
+// @ts-ignore
+import sap_fiori_3_light_dark from '../styles/theming/sap_fiori_3_light_dark.scss?inline';
+// @ts-ignore
+import sap_fiori_3_hcw from '../styles/theming/sap_fiori_3_hcw.scss?inline';
+// @ts-ignore
+import sap_fiori_3_hcb from '../styles/theming/sap_fiori_3_hcb.scss?inline';
+
+import {default as projects} from '../../projects';
+
+const themesVariables = {
+    sap_horizon,
+    sap_horizon_dark,
+    sap_horizon_hcb,
+    sap_horizon_hcw,
+    sap_fiori_3,
+    sap_fiori_3_dark,
+    sap_fiori_3_light_dark,
+    sap_fiori_3_hcw,
+    sap_fiori_3_hcb
+};
 
 export const packageConfig: PackageConfigInterface = {
-    ...projects['cx'],
-    value: 'cx'
-};
+    ...projects["cx"],
+    value: 'cx',
+    themes: projects["cx"].themes.map((theme) => {
+        return {
+            id: theme.value,
+            name: theme.title,
+            variables: themesVariables[theme.value]
+        }
+    })
+}
--- projects.js
+++ projects.js
@@ -1,46 +1,49 @@
 import { projects } from './workspace.json';
+const stylesThemes = {
+    defaultTheme: 'sap_horizon',
+    themes: [
+        {
+            value: 'sap_horizon',
+            title: 'Morning Horizon (Light)'
+        },
+        {
+            value: 'sap_horizon_dark',
+            title: 'Evening Horizon (Dark)'
+        },
+        {
+            value: 'sap_horizon_hcb',
+            title: 'Horizon High Contrast Black'
+        },
+        {
+            value: 'sap_horizon_hcw',
+            title: 'Horizon High Contrast White'
+        },
+        {
+            value: 'sap_fiori_3',
+            title: 'Quartz Light'
+        },
+        {
+            value: 'sap_fiori_3_dark',
+            title: 'Quartz Dark'
+        },
+        {
+            value: 'sap_fiori_3_light_dark',
+            title: 'Quartz Auto (Depending on the OS Settings)'
+        },
+        {
+            value: 'sap_fiori_3_hcw',
+            title: 'Quartz High Contrast White'
+        },
+        {
+            value: 'sap_fiori_3_hcb',
+            title: 'Quartz High Contrast Black'
+        }
+    ]
+};
+
 export const themes = {
-    styles: {
-        defaultTheme: 'sap_horizon',
-        themes: [
-            {
-                value: 'sap_horizon',
-                title: 'Morning Horizon (Light)'
-            },
-            {
-                value: 'sap_horizon_dark',
-                title: 'Evening Horizon (Dark)'
-            },
-            {
-                value: 'sap_horizon_hcb',
-                title: 'Horizon High Contrast Black'
-            },
-            {
-                value: 'sap_horizon_hcw',
-                title: 'Horizon High Contrast White'
-            },
-            {
-                value: 'sap_fiori_3',
-                title: 'Quartz Light'
-            },
-            {
-                value: 'sap_fiori_3_dark',
-                title: 'Quartz Dark'
-            },
-            {
-                value: 'sap_fiori_3_light_dark',
-                title: 'Quartz Auto (Depending on the OS Settings)'
-            },
-            {
-                value: 'sap_fiori_3_hcw',
-                title: 'Quartz High Contrast White'
-            },
-            {
-                value: 'sap_fiori_3_hcb',
-                title: 'Quartz High Contrast Black'
-            }
-        ]
-    },
+    styles: stylesThemes,
+    cx: stylesThemes,
     ['common-css']: {
         defaultTheme: 'sap_horizon',
         themes: [
