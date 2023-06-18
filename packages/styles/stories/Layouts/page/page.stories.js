d2931f74ad4ab85de4f8943d187ba2c508b9decd
droshev <mladen.droshev@sap.com>
2023-06-18 11:08:04 -0400

    docs(styles): update page docs [ci visual]

--- packages/styles/stories/Layouts/page/page.stories.js
+++ packages/styles/stories/Layouts/page/page.stories.js
@@ -4,7 +4,12 @@ import basicPageExampleHtml from "./basic-page.example.html?raw";
 import '../../../src/page.scss';
 import '../../../src/bar.scss';
 import '../../../src/button.scss';
+import '../../../src/form-label.scss';
+import '../../../src/form-item.scss';
+import '../../../src/button.scss';
+import '../../../src/button.scss';
 import '../../../src/icon.scss';
+import '../../../src/input.scss';
 export default {
   title: 'Layouts/Page',
   parameters: {
@@ -19,22 +24,22 @@ export default {
 
  The elements used to construct a page are:
 
- - fd-page: The page-level wrapper
- - Header: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--header\` modifier classes
- - Header with SubHeader: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--header-with-subheader\` modifier classes
- - SubHeader: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--subheader\` modifier classes
- - Content: The page content is wrapped in \`fd-page__content container\`
- - Footer: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--footer\` modifier classes
+ - \`fd-page\`: The page-level wrapper
+ - **Header**: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--header\` modifier classes
+ - **Header with SubHeader**: Uses a Bar component with \`fd-bar--page\` and \`fd-bar--header-with-subheader\` modifier classes
+ - **SubHeader**: _(optional)_ Uses a Bar component with \`fd-bar--page\` and \`fd-bar--subheader\` modifier classes
+ - **Content**: The page content is wrapped in \`fd-page__content container\`
+ - **Footer**: _(optional)_ Uses a Bar component with \`fd-bar--page\` and \`fd-bar--footer\` modifier classes
 
 
  ### PAGE RESPONSIVE BEHAVIOUR
 
  The left and right spacings of the Bar and the Content could be adjusted according to the container width.
 
- - default: Bar component with \`fd-bar--page modifier\` class and \`fd-page__content\` class for the content
- - Size S: Bar component with additional \`fd-bar--page-s\` modifier class and \`fd-page__content--s\` modifier class for the content
- - Size M & L: Bar component with additional \`fd-bar--page-m_l\` modifier class and \`fd-page__content--m_l\` modifier class for the content
- - Size XL: Bar component with additional \`fd-bar--page-xl\` modifier class and \`fd-page__content--xl\` modifier class for the content
+ - **default**: Bar component with \`fd-bar--page modifier\` class and \`fd-page__content\` class for the content
+ - **Size S**: Bar component with additional \`fd-bar--page-s\` modifier class and \`fd-page__content--s\` modifier class for the content
+ - **Size M & L**: Bar component with additional \`fd-bar--page-m_l\` modifier class and \`fd-page__content--m_l\` modifier class for the content
+ - **Size XL**: Bar component with additional \`fd-bar--page-xl\` modifier class and \`fd-page__content--xl\` modifier class for the content
 `,
     tags: ['f3', 'a11y', 'theme']
   }
