579d1de75507e1ad54a124a28cbc48bf42adefc7
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2023-08-02 14:10:27 -0400

    feat(styles): introduce BTP Search field component [ci visual] (#4729)
    * feat(styles): introduce BTP Search field component [ci visual]
    
    * feat(styles): address PR review comments [ci visual]

---
+++ packages/styles/stories/Components/search-field/search-field.stories.js
@@ -0,0 +1,23 @@
+import defaultExampleHtml from "./default.example.html?raw";
+import '../../../src/icon.scss';
+import '../../../src/popover.scss';
+import '../../../src/list.scss';
+import '../../../src/form-label.scss';
+import '../../../src/form-message.scss';
+import '../../../src/select.scss';
+import '../../../src/dialog.scss';
+import '../../../src/toolbar.scss';
+import '../../../src/button.scss';
+import '../../../src/icon.scss';
+import '../../../src/layout-grid.scss';
+import '../../../src/form-item.scss';
+import '../../../src/search-field.scss';
+
+export default {
+  title: 'BTP/Search Field',
+  parameters: {
+    description: `The Search Field component is intended to be used in BTP Tool Header. `,
+    tags: ['btp']
+  }
+};
+export const Default = () => defaultExampleHtml;
