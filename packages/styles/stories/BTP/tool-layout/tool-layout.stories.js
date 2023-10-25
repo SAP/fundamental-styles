6457b4e17405277398983a3cceae26f63e167cb9
Inna Atanasova <i.atanasova@sap.com>
2023-10-25 12:39:19 -0400

    fix(styles): position the header on the top for Tool Layout [ci visual]

--- packages/styles/stories/BTP/tool-layout/tool-layout.stories.js
+++ packages/styles/stories/BTP/tool-layout/tool-layout.stories.js
@@ -1,3 +1,4 @@
+import stickyExampleHtml from './sticky.example.html?raw';
 import desktopExampleHtml from './desktop.example.html?raw';
 import tabletExampleHtml from './tablet.example.html?raw';
 import mobileExampleHtml from './mobile.example.html?raw';
@@ -17,10 +18,11 @@ export default {
 - <code>.fd-tool-layout</code>
   - <code>.fd-tool-layout--tablet</code>&nbsp; - modifier class for tablets
   - <code>.fd-tool-layout--phone</code>&nbsp; - modifier class for phones
+  - <code>.fd-tool-layout--sticky</code>&nbsp; - modifier class for positioning the header on the top
+  - <code>.fd-tool-layout--horizontal-navigation</code>&nbsp; - modifier class for layout with Horizontal Navigation
 - <code>.fd-tool-layout__container</code>
 - <code>.fd-tool-layout__header-container</code>&nbsp; - wraping element around the Tool Header
 - <code>.fd-tool-layout__navigation-container</code>&nbsp; - wraping element around the Navigation (all its variants)
-  - <code>.fd-tool-layout__navigation-container--popup</code>&nbsp; - modifier class for when the navigation is a Popup
   - <code>.fd-tool-layout__navigation-container--horizontal</code>&nbsp; - modifier class for when the navigation is horizontal
 - <code>.fd-tool-layout__content-container</code>&nbsp; - wraping element around the page content. By default, the background of the content area is <code>var(--sapBackgroundColor)</code>
   - <code>.fd-tool-layout__content-container--transparent</code>&nbsp; - modifier class to apply transparent background to the content area.
@@ -29,15 +31,24 @@ export default {
         tags: ['btp', 'development', 'horizon-only']
     }
 };
+
 export const Desktop = () => desktopExampleHtml;
 Desktop.parameters = {
-    docs: {
-      description: {
-        story: ``
-      }
+  docs: {
+    description: {
+      story: ``
     }
-  };
+  }
+};
 
+export const Sticky = () => stickyExampleHtml;
+Sticky.parameters = {
+  docs: {
+    description: {
+      story: `The <code>fd-tool-layout--sticky</code> modifier class will position the whole layout absolutely with the header container always on top. The navigation and content containers receive the scroll overflow.`
+    }
+  }
+};
 
 export const Tablet = () => tabletExampleHtml;
 Tablet.parameters = {
