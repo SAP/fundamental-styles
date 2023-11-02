40b939cea4e9e7b41e911d44dc8c401cb5a6fd4f
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2023-11-02 04:15:23 -0400

    feat(styles): add square Nested Button to BTP (#4953)

--- packages/styles/stories/BTP/button/button.stories.js
+++ packages/styles/stories/BTP/button/button.stories.js
@@ -1,5 +1,6 @@
 import toolHeaderButtonExampleHtml from './tool-header.example.html?raw';
 import nestedButtonExampleHtml from './nested.example.html?raw';
+import nestedSquareButtonExampleHtml from './nested-square.example.html?raw';
 import description from './button.md?raw';
 
 import '../../../src/icon.scss';
@@ -28,6 +29,16 @@ NestedButton.parameters = {
     docs: {
         description: {
             story: `The Nested button is also intended to be used within the controls and patterns for the <b>BTP</b> area. Use the <code>.fd-button--nested</code> modifier class with the <code>.fd-button</code> base class for this type of button.
+`
+        }
+    }
+};
+
+export const NestedSquareButton = () => nestedSquareButtonExampleHtml;
+NestedSquareButton.parameters = {
+    docs: {
+        description: {
+            story: `Use the <code>.fd-button--nested-square</code> modifier class with the <code>.fd-button</code> base class for aquare nested button.
 `
         }
     }
