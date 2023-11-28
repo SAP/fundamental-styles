299b847a5e5392d4ac43371c6a767f3b60802393
Inna Atanasova <i.atanasova@sap.com>
2023-11-28 15:04:59 -0500

    fix(styles): update Variant Management [ci visual]

--- packages/styles/stories/Components/variant-management/variant-management.stories.js
+++ packages/styles/stories/Components/variant-management/variant-management.stories.js
@@ -1,7 +1,9 @@
+import sizesExampleHtml from "./sizes.example.html?raw";
 import dirtyLabelExampleHtml from "./dirty-label.example.html?raw";
 import '../../../src/variant-management.scss';
 import "../../../src/button.scss";
 import "../../../src/title.scss";
+import "../../../src/icon.scss";
 export default {
   title: 'Components/Variant Management',
   parameters: {
@@ -13,36 +15,18 @@ export default {
     tags: ['f3', 'theme', 'development']
   }
 };
-export const HeaderSizes = () => {
-  const headers = Array.from({
-    length: 6
-  }, (_, i) => i + 1);
-  return headers.map(header => `
-  <div class="fd-variant-management">
-    <div class="fd-variant-management__header" tabindex="0">
-      <h4 class="fd-title fd-variant-management__title fd-title--h${header}">
-        Variant Title H${header}
-      </h4>
-      <button aria-disabled="false" tabindex="0" aria-haspopup="true" class="fd-button fd-button--transparent fd-button--menu" type="button">
-        <i role="presentation" class="sap-icon--slim-arrow-down"></i>
-      </button>
-    </div>
-  </div>
-  `).join('\n');
-};
-HeaderSizes.storyName = 'Header sizes';
-HeaderSizes.parameters = {
+
+export const Sizes = () => sizesExampleHtml;
+Sizes.parameters = {
   docs: {
     description: {
-      story: `
-<p>By using the title control the variant management control can be set to any header level size.
-This is important when setting the variant management into an object or Dynamic page header area.</p>
-<p><strong>Variant management should by default have the Title control with fd-title--h3 class in dynamic page header
-  and fd-title--h4 if used in table header/toolbar above tables.</strong></p>
-          `
+      story: `- <b>For matching the dynamic / object page header style:</b> add the modifier class <code>.fd-variant-management__title--object-header</code> to the <code>.fd-variant-management__title</code> base class.
+- <b>For matching the dynamic / object page header style when the header is snapped or scrolled:</b> add the modifier class <code>.fd-variant-management__title--snapped-object-header</code> to the <code>.fd-variant-management__title</code> base class.
+- <b>For matching the size of Headers within Tables:</b> add the modifier class <code>.fd-variant-management__title--title</code> to the <code>.fd-variant-management__title</code> base class.`
     }
   }
 };
+
 export const DirtyLabel = () => dirtyLabelExampleHtml;
 DirtyLabel.storyName = 'Dirty state';
 DirtyLabel.parameters = {
@@ -50,8 +34,7 @@ DirtyLabel.parameters = {
     description: {
       story: `
 <p>Variant management supports dirty indicator in cases when selected variant has been modified but not saved.</p>
-<p>It can be either an asterisk sign, or a human-readable text.</p>
-          `
+<p>It can be either an asterisk sign, or a human-readable text.</p>`
     }
   }
 };
