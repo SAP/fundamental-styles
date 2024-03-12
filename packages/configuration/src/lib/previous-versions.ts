afc5b23f91885613df78116205a79416c232e351
droshev <mladen.droshev@sap.com>
2024-03-12 14:49:50 -0400

    docs: add link to 0.33 to the docs

--- packages/configuration/src/lib/previous-versions.ts
+++ packages/configuration/src/lib/previous-versions.ts
@@ -4,4 +4,5 @@ export const previousVersions = [
     {url: 'https://docs-0-30--fundamental-styles.netlify.app/', version: '0.30'},
     {url: 'https://docs-0-31--fundamental-styles.netlify.app/', version: '0.31'},
     {url: 'https://docs-0-32--fundamental-styles.netlify.app/', version: '0.32'},
+    {url: 'https://docs-0-33--fundamental-styles.netlify.app/', version: '0.33'},
 ]
