0d6589357cec5a659821e4294be7f1e0604e9011
Inna Atanasova <i.atanasova@sap.com>
2024-03-22 16:22:19 -0400

    feat(styles): initial commit for Card header updates

--- packages/styles/stories/Components/card/card.stories.js
+++ packages/styles/stories/Components/card/card.stories.js
@@ -3,6 +3,9 @@ import tableCardExampleHtml from "./table-card.example.html?raw";
 import listCardExampleHtml from "./list-card.example.html?raw";
 import analyticalCardExampleHtml from "./analytical-card.example.html?raw";
 import cardAnatomyExampleHtml from "./card-anatomy.example.html?raw";
+import extendedHeaderExampleHtml from "./extended-header.example.html?raw";
+import numericHeaderExampleHtml from "./numeric-header.example.html?raw";
+
 import '../../../src/button.scss';
 import '../../../src/avatar.scss';
 import '../../../src/badge.scss';
@@ -14,10 +17,13 @@ import '../../../src/checkbox.scss';
 import '../../../src/list.scss';
 import '../../../src/link.scss';
 import '../../../src/icon.scss';
+import '../../../src/form-label.scss';
+import '../../../src/rating-indicator.scss';
 export default {
   title: 'Components/Card',
   parameters: {
-    description: `A card is content container that represents a task or a topic on the overview page of an application. It is essentially a smart component that uses UI annotations to render its content. Cards can display read-only content from different sources side by side – without requiring the user to switch screens.
+    description: `The Card is a container that consists of a <b>header</b>, a <b>content</b> and a <b>footer</b> area. 
+    It takes the width of its parent. The <b>header</b> could be placed either above or below the <b>content</b> and is not separated with a line. The <b>footer</b> area is optional.
 
 **Cards can display different types of content, such as:**
 
@@ -31,32 +37,63 @@ export default {
 Card sizes vary depending on the layout, and they are not editable. A card can focus on either a single object or topic, or on a group of objects. Several cards can reference the same application, but the information should be distinct from one card to another.
 
 ##Card anatomy
+- <b>header</b> (optional): the card header is a flexible combination of 3 block areas: main header, numeric header and extended header. The header displays a mandatory title, indicating what the card is about and functions as a navigation control that directs the user to the parent app.
+  - <b>main header</b>: the main card header block shows the basic information of the card. It can contain <b>avatar</b> (optional), <b>title</b>, <b>subtitle</b> (optional), <b>counter</b> (optional) and/or <b>button</b> (optional).
+  - <b>extended header</b>: the extended header is a flexible block to hold various other components, like time stamp, rating, tags, label/value, long text, numeric values, etc. The components can be arranged in left column or right column, each column is suggested to have maximum 3 lines for placing the components.
+  - <b>numeric header</b>: the numeric header block is designed for displaying numeric information. It consumes Numeric Content (Horizon) and can show additional qualifying information and side indicators, if required. 
 
-A card is a container that consists of two main components: a selectable header area (with a title) and a content area. The width and the height of a card depends on its parent, and its header can be placed either above or below the content.
+- <b>content</b> (main): the content area is reserved for application content.
+    
+- <b>footer</b> (optional): the footer displays a list of actions that can be performed on the card. When link is too long, or there is no more place for actions, overflow button should appear.
 
-Components | Description
-:--------- |:-----------
-Header (optional) | The header displays a mandatory title, indicating what the card is about and functions as a navigation control that directs the user to the parent app.
-Content (main) | The content area is reserved for application content.
-Title | A title is mandatory to explain what content is being displayed to the user.
-Avatar (optional) | An avatar can be displayed in a size S (3rem).
-Subtitle (optional) | The subtitle provides additional context to the title or displays a status. Its usage depends on the card type. Subtitles that exceed one line are truncated with an ellipsis.
-Counter (optional) | The counter indicates how many items are showing on the card in relation to the total number of relevant items. If all the relevant items are visible on the card, no counter is shown. There is also no counter if there is an issue loading a card, or if no items are found in the filter criteria. The counter is right-aligned and is never truncated.
-Footer (optional) | The footer displays a list of actions that can be performed on the card. When link is too long, or there is no more place for actions, overflow button should appear.
 `,
     tags: ['f3', 'a11y', 'theme', 'development']
   }
 };
+
+
 export const CardAnatomy = () => cardAnatomyExampleHtml;
-CardAnatomy.storyName = 'Standard card';
+CardAnatomy.storyName = 'Standard card | Main Header';
 CardAnatomy.parameters = {
   docs: {
    description: {
-      story: `The standard card displays a header area with a title and a content area, as well as any other components mentioned in the card anatomy section.
+      story: `The main card header block comes in two interactivity variants: non-interactive and interactive. It shows the basic information of the card. 
+
+  - <b>title</b>: title is mandatory to explain what content is being displayed to the user. It wraps 3 lines.
+  - <b>avatar</b> (optional): avatar should be displayed in a size S (3rem).
+  - <b>subtitle</b> (optional): the subtitle provides additional context to the title or displays a status. Its usage depends on the card type. Subtitles that exceed one line are truncated with an ellipsis (it wraps 2 lines).
+  - <b>counter</b> (optional): the counter indicates how many items are showing on the card in relation to the total number of relevant items. If all the relevant items are visible on the card, no counter is shown. There is also no counter if there is an issue loading a card, or if no items are found in the filter criteria. The counter is right-aligned and is never truncated.
+  - <b>interactive element</b> (optional): like an icon button.
+
+For non-interactive main header add the <code>.fd-card__header-main--non-interactive</code> modifier class to the <code>.fd-card__header-main</code> base class.
 `
     }
   }
 };
+
+export const ExtendedHeader = () => extendedHeaderExampleHtml;
+ExtendedHeader.parameters = {
+  docs: {
+   description: {
+      story: `The Extended header is a flexible block to hold various other components, like time stamp, rating, tags, label/value, long text, numeric values, etc. The components can be arranged in left column or right column, each column is suggested to have maximum 3 lines for placing the components.
+
+- <code>.fd-card__header-extended--top-aligned</code> and <code>.fd-card__header-extended--bottom-aligned</code> modifier classes applied with the <code>.fd-card__header-extended</code> base class will align the content of the columns vertically to the top or the bottom (default is center). 
+- <code>.fd-card__header-column--right-aligned</code> modifier class for <code>.fd-card__header-column</code> will align the content of the column to the right.
+`
+    }
+  }
+};
+
+export const NumericHeader = () => numericHeaderExampleHtml;
+NumericHeader.parameters = {
+  docs: {
+   description: {
+      story: `The numeric header block is designed for displaying numeric information.
+`
+    }
+  }
+};
+
 export const AnalyticalCard = () => analyticalCardExampleHtml;
 AnalyticalCard.storyName = 'Analytical card';
 AnalyticalCard.parameters = {
