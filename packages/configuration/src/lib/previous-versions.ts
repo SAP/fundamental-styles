df685fef2b7aec7dda4602f70eb9c8504ce5690f
deno <mladen.droshev@sap.com>
2024-03-26 15:54:20 -0400

    docs: add doc 0.34 (#5262)

--- packages/configuration/src/lib/previous-versions.ts
+++ packages/configuration/src/lib/previous-versions.ts
@@ -5,4 +5,5 @@ export const previousVersions = [
     {url: 'https://docs-0-31--fundamental-styles.netlify.app/', version: '0.31'},
     {url: 'https://docs-0-32--fundamental-styles.netlify.app/', version: '0.32'},
     {url: 'https://docs-0-33--fundamental-styles.netlify.app/', version: '0.33'},
+    {url: 'https://docs-0-34--fundamental-styles.netlify.app/', version: '0.34'},
 ]
