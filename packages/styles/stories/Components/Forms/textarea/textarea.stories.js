3919885bd4aa962b7945dde3e2c4b0aa6f85a206
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2024-08-06 09:32:35 -0400

    feat(styles): add ai text highlight [ci visual] (#5543)

--- packages/styles/stories/Components/Forms/textarea/textarea.stories.js
+++ packages/styles/stories/Components/Forms/textarea/textarea.stories.js
@@ -2,6 +2,7 @@ import statesExampleHtml from "./states.example.html?raw";
 import disabledAndReadOnlyExampleHtml from "./disabled-and-read-only.example.html?raw";
 import counterExampleHtml from "./counter.example.html?raw";
 import primaryExampleHtml from "./primary.example.html?raw";
+import aiExampleHtml from "./ai.example.html?raw";
 import '../../../../src/textarea.scss';
 import '../../../../src/fieldset.scss';
 import '../../../../src/form-label.scss';
@@ -9,6 +10,9 @@ import '../../../../src/form-message.scss';
 import '../../../../src/form-item.scss';
 import '../../../../src/form-group.scss';
 import '../../../../src/popover.scss';
+import '../../../../src/menu.scss';
+import '../../../../src/button.scss';
+import '../../../../src/icon.scss';
 export default {
   title: 'Components/Forms/Textarea',
   parameters: {
@@ -50,6 +54,16 @@ States.parameters = {
   docs: {
     description: {
       story: 'As with any other input component, you can validate the fields and show the result as a value state of the component. Possible value states are error, warning, success, information, or neutral (none).'
+    }
+  }
+};
+
+export const AI = () => aiExampleHtml;
+AI.storyName = 'AI Text Highlight Pattern';
+AI.parameters = {
+  docs: {
+    description: {
+      story: 'The AI Text Highlight pattern helps users differentiate between AI-generated and human-authored text. This clear distinction is vital for maintaining transparency and trust, especially as AI becomes more prevalent in applications. By visually distinguishing the two types of text, users can easily identify the source of the information, aiding in understanding the context and any potential biases. This pattern is especially useful in scenarios where both AI and human inputs are present.'
     }
   }
 };
