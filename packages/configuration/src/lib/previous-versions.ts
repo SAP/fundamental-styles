e6ac8679a6096c7db339ccaee6d6c2d83e5f091c
droshev <mladen.droshev@sap.com>
2025-03-07 10:33:48 -0500

    docs: bump docs

--- packages/configuration/src/lib/previous-versions.ts
+++ packages/configuration/src/lib/previous-versions.ts
@@ -10,4 +10,5 @@ export const previousVersions = [
     {url: 'https://docs-0-36--fundamental-styles.netlify.app/', version: '0.36'},
     {url: 'https://docs-0-37--fundamental-styles.netlify.app/', version: '0.37'},
     {url: 'https://docs-0-38--fundamental-styles.netlify.app/', version: '0.38'},
+    {url: 'https://docs-0-39--fundamental-styles.netlify.app/', version: '0.39'},
 ]
