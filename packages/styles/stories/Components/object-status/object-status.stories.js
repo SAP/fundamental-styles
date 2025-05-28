87581d31b5d2b0733b91c8fab5407e90115c9e96
Inna Atanasova <i.atanasova@sap.com>
2025-05-28 14:52:30 -0400

    fix(styles): add a11y updates for Object Status [ci visual]

--- packages/styles/stories/Components/object-status/object-status.stories.js
+++ packages/styles/stories/Components/object-status/object-status.stories.js
@@ -18,7 +18,6 @@ export default {
     description: `Object Status is a short text that represents the semantic status of an object. It has a semantic
 color and an optional icon. Typically, the object status is used in the dynamic page header and as a status
 attribute of a line item in a table. `,
-    tags: ['f3', 'a11y', 'theme']
   }
 };
 export const Primary = () => primaryExampleHtml;
