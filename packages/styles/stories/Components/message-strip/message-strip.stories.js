361fb4ed5fc01d3d5217efc98fcee2976a942aca
Inna Atanasova <i.atanasova@sap.com>
2025-05-28 11:28:06 -0400

    fix(styles): update custom Message strip colors [ci visual]

--- packages/styles/stories/Components/message-strip/message-strip.stories.js
+++ packages/styles/stories/Components/message-strip/message-strip.stories.js
@@ -129,7 +129,11 @@ MessageStripWithAccentColors.storyName = 'Custom Message Strip';
 MessageStripWithAccentColors.parameters = {
   docs: {
     description: {
-      story: `If the application needs a custom Message Strip, other than the semantic variations, then the colours from Inverted Object Status/Tag control should be used. Use the modifier classes \`.fd-message-strip--indication-color-*\`, where \`*\` is a number from 1 to 10 for the first set, and 1b to 10b for the second set. `
+      story: `If the application needs a custom Message Strip, other than the semantic variations, then the colours from Inverted Object Status/Tag control should be used. Use the modifier classes \`.fd-message-strip--indication-color-*\`, where \`*\` is a number from 1 to 10 for the first set, and 1b to 10b for the second set. <br />
+      <b>Disclaimer</b> <br />
+Please note that accessibility features, including contrast ratios, are verified only for centrally defined standard values in Light, Dark, HCB, and HCW themes. Customization of components – whether internal or external – may impact compliance, and accessibility cannot be guaranteed.
+
+`
     }
   }
 };
