4087ff4b5e4bcb21a82ece6441f4e0c230d6dc7c
Inna Atanasova <i.atanasova@sap.com>
2025-06-03 14:44:30 -0400

    fix(styles): add design and a11y updates for Object Number [ci visual]

--- packages/styles/stories/Components/object-number/object-number.stories.js
+++ packages/styles/stories/Components/object-number/object-number.stories.js
@@ -1,6 +1,10 @@
 import boldExampleHtml from "./bold.example.html?raw";
 import largeExampleHtml from "./large.example.html?raw";
 import primaryExampleHtml from "./primary.example.html?raw";
+import interactiveExampleHtml from "./interactive.example.html?raw";
+import invertedExampleHtml from "./inverted.example.html?raw";
+import invertedInteractiveExampleHtml from "./inverted-interactive.example.html?raw";
+
 import '../../../src/object-number.scss';
 export default {
   title: 'Components/Object Number',
@@ -27,23 +31,24 @@ Make sure that the object number is properly described and semantically understa
 - You want to display system messages.
 - They are for decorative purposes only.
         `,
-    tags: ['f3', 'theme']
+    tags: []
   }
 };
-export const Primary = () => primaryExampleHtml;
-Primary.parameters = {
+export const SemanticStatus = () => primaryExampleHtml;
+SemanticStatus.parameters = {
   docs: {
     description: {
       story: `
  There are different semantic statuses that can be applied to the object number by adding a modifier class.
- 
- | **Status** | **Modifier class**              |
- | ----------: | :------------------------------- |
- | Positive    | \`fd-object-number--positive\`     |
- | Negative    | \`fd-object-number--negative\`     |
- | Critical    | \`fd-object-number--critical\`     |
- | Informative | \`fd-object-number--informative\`  |
- | None        | \`fd-object-number__text\` (class) |
+
+| **Status** | **Modifier class**                  | 
+| ----------: | :-------------------------------   |
+| Neutral     |   none (default)                     |
+| Positive    | \`fd-object-number--positive\`     |
+| Negative    | \`fd-object-number--negative\`     |
+| Critical    | \`fd-object-number--critical\`     |
+| Informative | \`fd-object-number--informative\`  |
+
  `
     }
   }
@@ -52,7 +57,7 @@ export const Large = () => largeExampleHtml;
 Large.parameters = {
   docs: {
     description: {
-      story: `In some cases, it can be useful to enlarge the text for the object number. To display the object number in large text, add the \`--large\` modifier class to the element.`
+      story: `In some cases, it can be useful to enlarge the text for the object number. To display the object number in large text, add the \`fd-object-number--large\` modifier class to the \`fd-object-number\` base class.`
     }
   }
 };
@@ -60,7 +65,35 @@ export const Bold = () => boldExampleHtml;
 Bold.parameters = {
   docs: {
     description: {
-      story: `When there is a key numeric attribute on the page, it should be emphasized in bold text. To display the object number in bold text, add the \`--bold\` modifier class to the element.`
+      story: `When there is a key numeric attribute on the page, it should be emphasized in bold text. To display the object number in bold text, add the \`fd-object-number__text--bold\` modifier class to the text element with class \`fd-object-number__text\`.`
+    }
+  }
+};
+
+export const Interactive = () => interactiveExampleHtml;
+Interactive.parameters = {
+  docs: {
+    description: {
+      story: `To make an object number interactive, add the \`fd-object-number__interactive\` modifier class, \`role="button"\`, and \`tabindex="0"\` to its container element.`
+    }
+  }
+};
+
+
+export const Inverted = () => invertedExampleHtml;
+Inverted.parameters = {
+  docs: {
+    description: {
+      story: `To display the object number with an inverted style, add the \`fd-object-number--inverted\` modifier class.`
+    }
+  }
+};
+
+export const InvertedInteractive = () => invertedInteractiveExampleHtml;
+InvertedInteractive.parameters = {
+  docs: {
+    description: {
+      story: `To make an object number interactive, add the \`fd-object-number__interactive\` modifier class, along with \`role="button"\` and \`tabindex="0"\`, to its container element. To display the object number with an inverted style, add the \`fd-object-number--inverted\` modifier class.`
     }
   }
 };
