86079d7e5ee4172513678de395c697d9f930bf50
Inna Atanasova <i.atanasova@sap.com>
2025-06-19 11:26:20 -0400

    fix(styles): add improvements for Field Set, Form Grid, Group, Header, Item and Label [ci visual]

--- packages/styles/stories/Components/Forms/form-header/form-header.stories.js
+++ packages/styles/stories/Components/Forms/form-header/form-header.stories.js
@@ -6,7 +6,7 @@ export default {
     description: `
 Form headers are essentially titles that provide users with more context about a group of input fields. For instance, a form header titled with “Personal information” would categorize input fields with labels: “Name, Address, etc.”
 `,
-    tags: ['f3', 'theme']
+    tags: []
   }
 };
 export const Primary = () => primaryExampleHtml;
