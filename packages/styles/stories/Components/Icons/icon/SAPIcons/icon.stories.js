a467a50679d419e14f6d04944fcacf0021df82a6
Inna Atanasova <i.atanasova@sap.com>
2025-07-03 14:23:34 -0400

    docs(styles): update icons a11y [ci visual]

--- packages/styles/stories/Components/Icons/icon/SAPIcons/icon.stories.js
+++ packages/styles/stories/Components/Icons/icon/SAPIcons/icon.stories.js
@@ -79,7 +79,7 @@ export const AvailableIcons = () => {
   const div = document.createElement('div');
   div.className = 'fddocs-container';
   div.innerHTML = data.icons.map(icon => {
-    return '<div class="fddocs-container--icon">' + `<span class="sap-icon sap-icon--${icon}" style="font-size:3rem"></span>` + `<div>.sap-icon--${icon}</div>` + '</div><br />';
+    return '<div class="fddocs-container--icon">' + `<span class="sap-icon sap-icon--${icon}" role="presentation" aria-hidden="true" aria-label="${icon}" style="font-size:3rem"></span>` + `<div>.sap-icon--${icon}</div>` + '</div><br />';
   }).join('');
   return div.outerHTML;
 };
