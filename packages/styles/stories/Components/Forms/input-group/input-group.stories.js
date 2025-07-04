e14ef7d3a789b550808f2bb79a387e194b7da722
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-07-04 10:02:22 -0400

    fix(styles): a11y updates for Input Group [ci visual] (#6113)
    BREAKING CHANGES:
    added a new visually hidden element for screen readers with class fd-input-group__sr-only

--- packages/styles/stories/Components/Forms/input-group/input-group.stories.js
+++ packages/styles/stories/Components/Forms/input-group/input-group.stories.js
@@ -1,9 +1,3 @@
-import testSixExampleHtml from "./test-six.example.html?raw";
-import testFiveExampleHtml from "./test-five.example.html?raw";
-import testFourExampleHtml from "./test-four.example.html?raw";
-import testThreeExampleHtml from "./test-three.example.html?raw";
-import testTwoExampleHtml from "./test-two.example.html?raw";
-import testOneExampleHtml from "./test-one.example.html?raw";
 import focusExampleHtml from "./focus.example.html?raw";
 import statesExampleHtml from "./states.example.html?raw";
 import inputWithActionsExampleHtml from "./input-with-actions.example.html?raw";
@@ -20,19 +14,23 @@ import '../../../../src/input-group.scss';
 export default {
   title: 'Components/Forms/Input Group',
   parameters: {
-    description: 'The input group includes form inputs with add-ons that allow the user to better understand the information being entered.',
-    tags: ['f3', 'a11y', 'theme']
+    description: 'The Input Group includes form inputs with add-ons that allow the user to better understand the information being entered.',
+    tags: []
   }
 };
 export const Default = () => defaultExampleHtml;
 Default.parameters = {
-  docs: {}
+  docs: {
+    description: {
+      story: 'Basic Input Group with no additional elements or value state modifiers.'
+    }
+  }
 };
 export const TextAddOn = () => textAddOnExampleHtml;
 TextAddOn.parameters = {
   docs: {
    description: {
-      story: 'The Input Group with text add-on component is typically used to specify the type of the data being entered, such as currency or unit of measure. This add-on can be placed at the left or right (or both) of the input element.'
+      story: 'An Input Group with a text add-on (e.g., currency or unit). The add-on can be positioned before, after, or on both sides of the input field to clarify the expected value format.'
     }
   }
 };
@@ -40,7 +38,7 @@ export const InputWithIcons = () => inputWithIconsExampleHtml;
 InputWithIcons.parameters = {
   docs: {
    description: {
-      story: 'The Input with add-on supports icons.'
+      story: 'Demonstrates how to use an icon as an add-on. Typically used for visual cues like visibility toggles, status, or type indication.'
     }
   }
 };
@@ -48,53 +46,24 @@ export const InputWithActions = () => inputWithActionsExampleHtml;
 InputWithActions.parameters = {
   docs: {
    description: {
-      story: 'The Input with add-on supports actions. Actions can be shown with a text label or icon.'
+      story: 'An Input Group with a button add-on. Supports buttons with either text or icons, typically used for actions like submit, search, or trigger.'
     }
   }
 };
 export const States = () => statesExampleHtml;
-States.parameters = {};
+States.parameters = {
+  docs: {
+    description: {
+       story: 'Input Groups can visually represent different validation or feedback states such as success, error, warning, and information. Use modifier classes like `.is-success`, `.is-error`, `.is-warning`, or `.is-information` on the `.fd-input-group` container.'
+     }
+   }
+};
+
 export const Focus = () => focusExampleHtml;
 Focus.parameters = {
   docs: {
    description: {
-      story: 'The Input Group supports <b>focus</b> state, it can be added by putting `.is-focus` class to component.'
+      story: 'The focussed state of Input Group is achieved by adding the `.is-focus` class to the parent element with class `.fd-input-group`.'
     }
-  }
-};
-export const TestOne = () => testOneExampleHtml;
-TestOne.parameters = {
-  docs: {
-    disable: true
-  }
-};
-export const TestTwo = () => testTwoExampleHtml;
-TestTwo.parameters = {
-  docs: {
-    disable: true
-  }
-};
-export const TestThree = () => testThreeExampleHtml;
-TestThree.parameters = {
-  docs: {
-    disable: true
-  }
-};
-export const TestFour = () => testFourExampleHtml;
-TestFour.parameters = {
-  docs: {
-    disable: true
-  }
-};
-export const TestFive = () => testFiveExampleHtml;
-TestFive.parameters = {
-  docs: {
-    disable: true
-  }
-};
-export const TestSix = () => testSixExampleHtml;
-TestSix.parameters = {
-  docs: {
-    disable: true
   }
 };
