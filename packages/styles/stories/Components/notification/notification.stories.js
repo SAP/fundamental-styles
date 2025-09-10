571c8cd43936630d2111682b1f599b0aa5c5055f
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-09-10 09:12:47 -0400

    fix(styles): add a11y and design updates for Notifications [ci visual] (#6164)
    BREAKING CHANGES:
    Introduced new visually hidden elements (.fd-notification__sr-only) for conveying title status to assistive technologies. Added accessibility attributes (ARIA roles, labels, and relationships) to improve screen reader support

--- packages/styles/stories/Components/notification/notification.stories.js
+++ packages/styles/stories/Components/notification/notification.stories.js
@@ -37,15 +37,15 @@ Primary.parameters = {
   docs: {
     description: {
       story: `Notification items display a container with:
-      <ul>
-        <li><b>Avatar</b> (optional): uses Avatar component, size XS</li>
-        <li><b>Title</b> (required): the title wraps up to two lines and then truncates. For unread title (renders with bold font-weight) use <code>fd-notification__title--unread</code> modifier class.</li>
-        <li><b>Description</b> (optional): the description wraps up to two lines and then truncates.</li>
-        <li><b>Meta</b> (optional): items separated by a span element with class <code>fd-notification__separator</code>. Items truncate if there's not enough space. In case Title or Description truncates in an ellipsis, the meta is followed by the text <b>Expansion Trigger</b> ("More" / "Less"). </li>
-        <li><b>Priority Indicator</b> (optional): the priority is displayed using Message and Status Icons.  </li>
-        <li><b>Object Status</b> (optional): uses Inverted ObjectStatus / Tag (Horizon) - Inverted Object Status with icon, indication color: Second Color Set - Indication 2 - Red</li>
-        <li><b>Actions</b> (optional): include a "Close" and "Overflow" button (Transparent Button) actions.</li>
-      </ul>
+<ul>
+  <li><b>Avatar</b> (optional): uses Avatar component, size XS</li>
+  <li><b>Title</b> (required): the title wraps up to two lines and then truncates. For unread title (renders with bold font-weight) use <code>fd-notification__title--unread</code> modifier class.</li>
+  <li><b>Description</b> (optional): the description wraps up to two lines and then truncates.</li>
+  <li><b>Meta</b> (optional): items separated by a span element with class <code>fd-notification__separator</code>. Items truncate if there's not enough space. In case Title or Description truncates in an ellipsis, the meta is followed by the text <b>Expansion Trigger</b> ("More" / "Less"). </li>
+  <li><b>Priority Indicator</b> (optional): the priority is displayed using Message and Status Icons.  </li>
+  <li><b>Object Status</b> (optional): uses Inverted ObjectStatus / Tag (Horizon) - Inverted Object Status with icon, indication color: Second Color Set - Indication 2 - Red</li>
+  <li><b>Actions</b> (optional): include a "Close" and "Overflow" button (Transparent Button) actions.</li>
+</ul>
 `
     }
   }
