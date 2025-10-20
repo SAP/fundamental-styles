7ca21ad5cf07cfcab512dc1f65fd0622fad0f80f
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-10-20 09:15:56 -0400

    fix(styles): update Shellbar after design review [ci visual] (#6177)

--- packages/styles/stories/BTP/tool-header/tool-header.stories.js
+++ packages/styles/stories/BTP/tool-header/tool-header.stories.js
@@ -6,11 +6,18 @@ import '../../../src/button.scss';
 import '../../../src/icon.scss';
 import '../../../src/search-field.scss';
 import '../../../src/tool-header.scss';
+import '../../../src/message-strip.scss';
 
 export default {
     title: 'BTP/Tool Header',
     parameters: {
-        description: `The Tool Header exists on all BTP tools. It is the uppermost section of the tool. It is always visible to give users access to various functions. <br><b>This component is deprecated. Please use the <a href="../?path=/docs/sap-fiori-components-shellbar--docs" target="_blank">SHELLBAR</a> component instead.</b>
+        description: `The Tool Header exists on all BTP tools. It is the uppermost section of the tool. It is always visible to give users access to various functions. <br>
+<div class="fd-message-strip fd-message-strip--warning fd-message-strip--link">
+  <div class="fd-message-strip__icon-container" aria-hidden="true">
+    <span class="sap-icon sap-icon--message-warning" role="presentation" aria-hidden="true"></span>
+  </div><p class="fd-message-strip__text" id="message-strip-text-4">
+    This component is deprecated. Please use the <a class="fd-link fd-link--emphasized" href="../?path=/docs/sap-fiori-components-shellbar--docs" target="_blank">SHELLBAR</a> component instead.
+  </p></div>
 
 ## Structure
 - <code>fd-tool-header</code> -&nbsp;<b>(required)</b> Tool Header Container element. Holds all Tool Header UI elements
