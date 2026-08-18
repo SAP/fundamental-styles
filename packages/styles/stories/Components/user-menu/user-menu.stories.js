0d35041466d2eb40db64404b355cf59d2f2773e5
MariaIDineva <maria.dineva@sap.com>
2026-08-18 15:28:20 +0300

    feat(styles): add --wrap modfier classes for user menu; update examples [ci visual]

--- packages/styles/stories/Components/user-menu/user-menu.stories.js
+++ packages/styles/stories/Components/user-menu/user-menu.stories.js
@@ -60,10 +60,10 @@ Represents the current user and includes:
 <ul>
     <li>Popover: Contains the entire User Menu content.</li>
     <li>Avatar: A key visual element displaying the user's profile picture. If no profile picture is available, it shows the user's initials. If the user is not signed in, an icon is displayed.</li>
-    <li>Title: A required UI element showing the user's First and Last names. To truncate long titles use the <code>.fd-user-menu\\_\\_user-name--truncate</code> modifier class.</li>
-    <li>Subtitle 1 (subline): An optional secondary identifier, such as user ID or email.</li>
-    <li>Subtitle 2 (subline): An optional identifier, such as role, position, organization, or unit.</li>
-    <li>Subtitle 3 (subline): An optional identifier, such as role, position, organization, or unit. To truncate long subtitles use the <code>.fd-user-menu\\_\\_subline--truncate</code> modifier class.</li>
+    <li>Title: A required UI element showing the user's First and Last names. By default, long titles are clamped to two lines. To allow the title to wrap across multiple lines, use the <code>.fd-user-menu\\_\\_user-name--wrap</code> modifier class.</li>
+    <li>Subtitle 1 (subline): An optional secondary identifier, such as user ID or email. By default, long subtitles are truncated. To allow a subtitle to wrap across multiple lines, use the <code>.fd-user-menu\\_\\_subline--wrap</code> modifier class.</li>
+    <li>Subtitle 2 (subline): An optional identifier, such as role, position, organization, or unit. To allow it to wrap across multiple lines, use the <code>.fd-user-menu\\_\\_subline--wrap</code> modifier class.</li>
+    <li>Subtitle 3 (subline): An optional identifier, such as role, position, organization, or unit. By default, long subtitles are truncated. To allow it to wrap across multiple lines, use the <code>.fd-user-menu\\_\\_subline--wrap</code> modifier class.</li>
     <li>Manage Account Button: Links to account settings.</li>
 </ul>
 
